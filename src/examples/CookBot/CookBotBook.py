d890dfbe3c51e314a7ea2ccb8a6fd2df990dcf46
petrvanblokland <buro@petr.com>
2016-11-11 19:48:46 -0500

    Image position
    Former-commit-id: 52b0f72fd5268b20ce3059adab7d7a1353c5c20e
    Former-commit-id: ad6ac8c441f5db64fd37d2cf41acff95630f384d
    Former-commit-id: c93b1fed997a80f58d8594f296e4442e54f89219

--- src/examples/CookBot/CookBotBook.py
+++ src/examples/CookBot/CookBotBook.py
@@ -156,7 +156,7 @@ def makeDocument(rs):
     # Show baseline grid if rs.showBaselineGrid is True
     template1.baselineGrid(rs)
     # Create empty image place holders. To be filled by running content on the page.
-    template1.cContainer(2, -0.6, 5, 4, rs)  # Empty image element, cx, cy, cw, ch
+    template1.cContainer(2, -0.7, 5, 4, rs)  # Empty image element, cx, cy, cw, ch
     template1.cContainer(0, 5, 2, 3, rs)
     # Create linked text boxes. Note the "nextPage" to keep on the same page or to next.
     template1.cTextBox(FS, 0, 0, 2, 5, rs, flowId1, nextBox=flowId2, nextPage=0, fill=BOX_COLOR)
