ebb461f39f42ff4dd606a2a14a45bcd2f7f5020e
petrvanblokland <buro@petr.com>
2016-11-14 08:19:59 -0500

    Testing font metrics direct from TTFont
    Former-commit-id: 249ad1927aa723b7b601fe84a1baac3c552f1d51
    Former-commit-id: c8a758c59170a6a554acbc5d93dab2ba8ddd6730
    Former-commit-id: 13e26863247d95ddd7e54ea4ca830839160237a6

--- src/teststuff/testFontMetrics.py
+++ src/teststuff/testFontMetrics.py
@@ -1,15 +1,35 @@
+import pagebot.fonttoolbox.fontmetrics  
+reload(pagebot.fonttoolbox.fontmetrics )
+from pagebot.fonttoolbox.fontmetrics import getFontMetrics
+
+fontName = 'PromisePageBot-Bold'
+fontPath = '../fonts/PromisePageBot-GX.ttf'
+#if not fontName in installedFonts():
+installFont(fontPath)
+metrics = getFontMetrics(fontPath)
+font(fontName)
+spacer = FormattedString('-----\n ', lineHeight=1, font=fontName, fontSize=12)
+fontSize(400)
+
+s = FormattedString('Hlxg', font=fontName, fill=(1, 0, 0),
+    fontSize=300, lineHeight=320, baselineShift=200)
+print metrics
+
 M = 20
 fill(0.9)
-rect(M, M, 1000-2*M, 1000-2*M)
+rect(M, M, 1000-2*M, 800-2*M)
 
-fs = FormattedString('Hlxg', fontSize=400, font='Georgia')
+
 Y = 1000 - M
-BASE = Y - 367
-YS = (BASE, BASE+fs.fontXHeight(), BASE+fs.fontCapHeight(), BASE+fs.fontAscender(), BASE+fs.fontDescender())
+BASE = metrics['ascender'] + metrics['descender'] + metrics['lineGap']
+print BASE
+YS = (BASE,)
 for y in YS:
     stroke(0)
     strokeWidth(0.5)
     line((0, y), (1000, y))
 
-textBox(fs, (M, M, 1000-2*M, 1000-2*M))
+fill(0)
+stroke(None)
+textBox(s, (M, M, 1000-2*M, 800-2*M))
 
