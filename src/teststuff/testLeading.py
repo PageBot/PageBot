09b8b234fe55884b1317c5ff871daac386cf7a1d
petrvanblokland <buro@petr.com>
2016-11-16 07:47:47 +0100

    Adding tests.

--- src/teststuff/testLeading.py
+++ src/teststuff/testLeading.py
@@ -6,27 +6,27 @@ FONT_PATH = '../fonts/'
 FONT_LOCATIONS = {
     #'Promise-BoldCondensed': {"wght": 750, "wdth": 500, },
     #'Promise-LightCondensed': {"wght": 0, "wdth": 500},
-    'Promise-Light': {"wght": 0, "wdth": 1000},
-    'Promise-Book': {"wght": 250, "wdth": 1000},
-    'Promise-Regular': {"wght": 400, "wdth": 1000},    
-    'Promise-Medium': {"wght": 600, "wdth": 1000},    
-    'Promise-Semibold': {"wght": 750, "wdth": 1000},    
-    'Promise-Bold': {"wght": 1000, "wdth": 1000},
+    'PromisePageBot-Light': {"wght": 0, "wdth": 1000},
+    'PromisePageBot-Book': {"wght": 250, "wdth": 1000},
+    'PromisePageBot-Regular': {"wght": 400, "wdth": 1000},    
+    'PromisePageBot-Medium': {"wght": 600, "wdth": 1000},    
+    'PromisePageBot-Semibold': {"wght": 750, "wdth": 1000},    
+    'PromisePageBot-Bold': {"wght": 1000, "wdth": 1000},
 }
 FONTS = {}
 # Install the test V-font
 if not 'Promise-Bold' in installedFonts():
-    installFont(FONT_PATH + 'Promise-GX.ttf')
+    installFont(FONT_PATH + 'PromisePageBot-GX.ttf')
 for name, location in FONT_LOCATIONS.items():
-    fontName, fontPath = generateInstance(FONT_PATH + 'Promise-GX.ttf', 
+    fontName, fontPath = generateInstance(FONT_PATH + 'PromisePageBot-GX.ttf', 
     location, targetDirectory=FONT_PATH + 'instances')
     FONTS[name] = fontName#fontPath # Instead of fontName, no need to uninstall.
-LIGHT = FONTS['Promise-Light']
-BOOK = FONTS['Promise-Book']
-BOOK_ITALIC = FONTS['Promise-Book']
-MEDIUM = FONTS['Promise-Medium']
-SEMIBOLD = FONTS['Promise-Semibold']
-BOLD = FONTS['Promise-Bold']
+LIGHT = FONTS['PromisePageBot-Light']
+BOOK = FONTS['PromisePageBot-Book']
+BOOK_ITALIC = FONTS['PromisePageBot-Book']
+MEDIUM = FONTS['PromisePageBot-Medium']
+SEMIBOLD = FONTS['PromisePageBot-Semibold']
+BOLD = FONTS['PromisePageBot-Bold']
 
 tt = """Waar in de traditie van werken met opmaakprogrammatuur zoals Quark XPress en InDesign altijd een menselijke beslissing de definitieve opmaak van een pagina bepaalt, zijn er steeds meer situaties waarin dat geen optie is. Doordat steeds meer pagina’s worden gegenereerd met inhoud die uit een database komt – of van een online source – en waar de selectie van de informatie direct wordt bepaald door eigenschappen van de lezer, van de pagina’s automatisch worden berekend."""
 
@@ -54,6 +54,7 @@ print '$$$$$', fs.fontAscender(), fs.fontDescender(), fs.fontAscender()-fs.fontD
 textBox(fs, (500, 20, 400, 900))
 
 stroke(0)
+strokeWidth(0.5)
 fill(None)
 for n in range(20, 920, 28):
-    line((0, n), (600, n))
+    line((0, n), (900, n))
