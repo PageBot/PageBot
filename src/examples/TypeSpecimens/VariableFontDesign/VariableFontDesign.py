689e7c90848f9b74d2a54cfd3ead12c482985a38
petrvanblokland <buro@petr.com>
2016-11-24 11:59:11 +0100

    Adding newlines to thesis styles.
    Former-commit-id: 45398ec17dcb3702dda0b6cc703db06ed8fec8be [formerly 73c84156d5ef05e252c28e5050e38660c37cf1b9]
    Former-commit-id: 1ae166f36beceb6f2d74d56bebbe1dc3e5194e2f
    Former-commit-id: 9a5a0e389a5a2078e31260cc437ccc544223e06a
    Former-commit-id: 71989b73e8f312687bb658518f04e36a10d37909

--- src/examples/TypeSpecimens/VariableFontDesign/VariableFontDesign.py
+++ src/examples/TypeSpecimens/VariableFontDesign/VariableFontDesign.py
@@ -101,7 +101,7 @@ def makeSpecimen(rs):
     template1.grid(rs) 
     # Show baseline grid if rs.showBaselineGrid is True
     template1.baselineGrid(rs)
-    vCube = VariationCube(w=500, h=500, s='a', fontSize=13, dimensions=dict(wght=8, wdth=8))
+    vCube = VariationCube(w=500, h=500, s='a', fontSize=36, dimensions=dict(wght=3, wdth=3))
     template1.place(vCube, 50, 160)
    
     # Create new document with (w,h) and fixed amount of pages.
