a6b7bbb438de1ec6aa3659183c566cdd9684976d
Michiel Kauw-A-Tjoe <michiel@concretejungle.nl>
2016-12-14 16:39:12 +0100

    Adding application basics.
    Former-commit-id: f9aadf17c5750e268c44e9ad72c597d65ca3eb56 [formerly 090bc44b9c8054a458143cf2e6608e3ad32b39d5]
    Former-commit-id: 6426aea421b399424fe6ea7e3975ae1d0ec9aa1a
    Former-commit-id: 6914253056393f4feb8da12ec941eb8380e92fef
    Former-commit-id: 5ec53051f4cf44acc114d6180e29619c28449dc2

--- src/pagebot/pagebotapp.py
+++ src/pagebot/pagebotapp.py
@@ -2,15 +2,18 @@
 # -----------------------------------------------------------------------------
 #
 #     P A G E B O T
-#
-#     Copyright (c) 2016+ Type Network, www.typenetwork.com, www.pagebot.io
+# #     Copyright (c) 2016+ Type Network, www.typenetwork.com, www.pagebot.io
 #     Licensed under MIT conditions
 #     Made for usage in DrawBot, www.drawbot.com
 # -----------------------------------------------------------------------------
 #
 #     pagebotapp.py
 #
+import drawBot
+
 class PageBotApp(object):
-    u"""Wrapper class to bundle all document page typesetter and composition functions, genating 
-    export document."""
-                    
+    u"""Wrapper class to bundle all document page typesetter and composition
+    functions, generating export document."""
+
+    def __init__(self, window):
+        self.window = window
