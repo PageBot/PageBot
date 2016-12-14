5e939481c47caf2c1e46e8f7cd9965763c4cbbfc
Michiel Kauw-A-Tjoe <michiel@concretejungle.nl>
2016-12-14 16:39:12 +0100

    Adding application basics.

---
+++ src/pagebot/delegate.py
@@ -0,0 +1,46 @@
+# -*- coding: UTF-8 -*-
+# -----------------------------------------------------------------------------
+#    PageBot appication.
+#    Copyright (c) 2016+ Type Network
+#
+#
+# -----------------------------------------------------------------------------
+#
+#    delegate.py
+#
+
+# General Python libraries.
+
+import os, os.path
+import string
+import datetime
+
+# Objective-C.
+from vanilla import Window
+import objc
+from AppKit import NSObject
+from PyObjCTools import AppHelper
+
+from pagebotapp import PageBotApp
+
+class AppDelegate(NSObject):
+    u"""
+    Main delegate for PageBot application.
+    """
+
+    windowSize = (800, 600)
+
+    @objc.IBAction
+    def applicationDidFinishLaunching_(self, notification):
+        u"""
+        """
+        window = Window(self.windowSize, minSize=(1, 1), closable=True)
+        self.pagebotapp = PageBotApp(window)
+        self.pagebotapp.initialize()
+        window.open()
+
+    def applicationShouldTerminate_(self, sender):
+        self.pagebotapp.terminate()
+        return True
+
+AppHelper.runEventLoop()
