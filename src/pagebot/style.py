306d6787fd3767f639e7737a2244923411e2073a
petrvanblokland <buro@petr.com>
2016-12-15 15:00:36 +0100

    Adding TODO about firstColumnIndent
    Former-commit-id: 814ff4d0e3157635f9ee1199e0a52e5936098527 [formerly 35eb1d34e997bbeef73aae6f93d704f52fd1a01b]
    Former-commit-id: 8d65013c1a8d0202d37e8ef7c9b4fc14ce7e43f7
    Former-commit-id: 1022dd817168f82db9a8f053fc0048cd27190e32
    Former-commit-id: 9c1db10fb2df30683b7001bc28a012341a7cdab7

--- src/pagebot/style.py
+++ src/pagebot/style.py
@@ -200,6 +200,8 @@ def getRootStyle(u=U, showGrid=SHOW_GRID, showGridColumns=SHOW_GRID_COLUMNS,
         rFirstLineIndent = 0, # First line indent as factor if font size.
         firstParagraphIndent = 0, # Indent of first line of first paragraph in a text tag.
         rFirstParagraphIndent = 0, # Indent of first line of first paragraph, relative to font size.
+        firstColumnIndent = 0, # Indent of first line in a column, after start of new column (e.g. by overflow)
+        rFirstColumnIndent = 0, # Indent of first line in a colum, after start of new column, relative to font size.
         indent = 0, # Left indent (for left-right based scripts)
         rIndent = 0, # Left indent as factor of font size.
         tailIndent = 0, # Tail/right indent (for left-right based scripts)
