5e866a8496ce819876df25aa84041805fa209ad8
Michiel Kauw-A-Tjoe <michiel@concretejungle.nl>
2017-01-10 15:57:48 +0100

    Moving vanilla code to pagebot application object.
    Former-commit-id: 27cd5d70e1ba53bcb8b70d986f6e5bad4e92cdb2 [formerly 40c3f8bb33fb6b6d9bf9fdacb451ac35afbf54b8]
    Former-commit-id: 87da56eaa504d8777c41a4de5970d768070c67ee
    Former-commit-id: 0b5d5e1b08a1eb36e7b8a4460199743281d2d869
    Former-commit-id: 48ca5aa6322c53892fa497fb5e07ea8ce725f1ce

--- src/pagebotapp/delegate.py
+++ src/pagebotapp/delegate.py
@@ -16,12 +16,9 @@ import string
 import datetime
 
 # Objective-C.
-from vanilla import Window
 import objc
 from AppKit import NSObject
 from PyObjCTools import AppHelper
-from drawBot.ui.drawView import DrawView
-from drawBot.ui.codeEditor import OutPutEditor
 
 from pagebotapp import PageBotApp
 
@@ -30,20 +27,11 @@ class AppDelegate(NSObject):
     Main delegate for PageBot application.
     """
 
-    windowSize = (800, 600)
-
     @objc.IBAction
     def applicationDidFinishLaunching_(self, notification):
         u"""
         """
-        outputWindow = Window((400, 300), minSize=(1, 1), closable=True)
-        outputWindow.outputView = OutPutEditor((0, 0, -0, -0), readOnly=True)
-        window = Window(self.windowSize, minSize=(1, 1), closable=True)
-        window.drawView = DrawView((0, 32, -0, -0))
-        self.pagebotapp = PageBotApp(window, outputWindow)
-        self.pagebotapp.initialize()
-        window.open()
-        outputWindow.open()
+        self.pagebotapp = PageBotApp()
 
     def applicationShouldTerminate_(self, sender):
         self.pagebotapp.terminate()
