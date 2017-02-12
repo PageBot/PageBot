4c61c04420916c0463e50c8c50285919803c3ea0
petrvanblokland <buro@petr.com>
2017-02-12 16:52:02 +0100

    Add pagebot.getFontPath() to answer the standard fonts paths.
    Comment debug lines.
    Start of specimen page for Decovar (will include standard method to
    visualize variation fonts).
    
    
    Former-commit-id: 3f7f0be93604951d3d01f27f260ec2c2eeae4caa [formerly 95a2dc40154cb9d64cce74097ecd56ab46d36024]
    Former-commit-id: b56f43f5be7266296746a2b0474c58e57d197ea0
    Former-commit-id: a9b03b04b8daeef7ea4de2eaf74c979af830153b
    Former-commit-id: bf5da40f115acfc52cd575fa829deda4410a02c0

---
+++ src/examples/DecoVarSpecimen.py
@@ -0,0 +1,78 @@
+# -----------------------------------------------------------------------------
+#     Copyright (c) 2016+ Type Network, www.typenetwork.com, www.pagebot.io
+#
+#     P A G E B O T
+#
+#     Licensed under MIT conditions
+#     Made for usage in DrawBot, www.drawbot.com
+# -----------------------------------------------------------------------------
+#
+#     SystemFontSpecimen.py
+#
+import pagebot
+from pagebot import getFormattedString
+from pagebot.page import Template
+from pagebot.fonttoolbox.style import Style
+
+import pagebot.publications.typespecimen
+reload(pagebot.publications.typespecimen)
+from pagebot.publications.typespecimen import TypeSpecimen
+
+DEBUG = False # Make True to see grid and element frames.
+
+FONT_PATH = pagebot.getFontPath()
+DecovarPath = FONT_PATH + 'fontbureau/Decovar-VF.ttf'
+
+decovarName = installFont(DecovarPath)
+s = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz 0123456789'
+
+class VariationTypeSpecimen(TypeSpecimen):
+
+    def makeTemplate(self, rs):
+        hyphenation(False)
+        # Template for the main page.
+        template = Template(rs) # Create second template. This is for the main pages.
+        # Show grid columns and margins if rootStyle.showGrid or 
+        # rootStyle.showGridColumns are True.
+        # The grid is just a regular element, like all others on the page. Same parameters apply.
+        template.grid(rs)  
+        # Add named text box to template for main specimen text.
+        template.cTextBox('', 0, 0, 6, 1, eId=self.titleBoxId, style=rs)       
+        template.cTextBox('', 1, 1, 5, 6, eId=self.specimenBoxId, style=rs)       
+        #template.cTextBox('', 0, 1, 2, 6, eId=self.infoBoxId, style=rs)
+        # Some lines, positioned by vertical and horizontal column index.
+        template.cLine(0, 0, 6, 0, style=rs, stroke=0, strokeWidth=0.25)       
+        template.cLine(0, 1, 6, 0, style=rs, stroke=0, strokeWidth=0.25)       
+        template.cLine(0, 7, 6, 0, style=rs, stroke=0, strokeWidth=0.25)       
+        return template
+ 
+    def buildVariationPage(self, page):
+        style = Style(DecovarPath)
+        
+        title = page.getElement(self.titleBoxId) 
+        fs = getFormattedString(style.info.fullName, dict(fontSize=48, font=style.path))
+        title.append(fs)
+ 
+        column = page.getElement(self.specimenBoxId) # Find the specimen column element on the current page.
+        # Create the formatted string with the style names shown in their own style.
+        # The first one in the list is also used to show the family Name.
+        for fontSize in (9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24):
+            fs = getFormattedString('%dpt %s' % (fontSize, s), 
+                style=dict(font=decovarName, fontSize=fontSize, hyphenation=False))
+            column.append(fs)
+        
+    def buildPages(self, doc):
+        # Build the pages, showing axes, samples, etc.
+        # Using the first page as cover (to be filled...)
+        coverPage = doc[1]
+        # Fill cover here.
+        
+        page = doc.newPage()    
+        self.buildVariationPage(page)
+    
+# Create a new specimen publications and add the list of system fonts.
+typeSpecimen = VariationTypeSpecimen([decovarName], showGrid=DEBUG) 
+# Build the pages of the publication, interpreting the font list.
+typeSpecimen.build()
+# Export the document of the publication to PDF.
+typeSpecimen.export('SystemFontSpecimen.pdf')
