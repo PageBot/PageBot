2d660a3a88e5d994f85e68c5ee2758fad3e15faa
petrvanblokland <buro@petr.com>
2017-02-22 09:57:08 +0100

    Adding functions to Font and Glyph.
    Kerning reading from OTF, available as lazy font.kerning.

--- src/pagebot/fonttoolbox/objects/font.py
+++ src/pagebot/fonttoolbox/objects/font.py
@@ -11,13 +11,17 @@
 #
 #     Implements a PabeBot font style to get info from a TTFont.
 #     The Style instance is a convenience caching storage, similar to RoboFont Font.
+#     Using the Family/Font/Glyph classes, allows page layout in PageBot to access
+#     all information in a font purpose of typography and layout.
 #   
 #     We'll call this class "Font" instead of "Style" (as in other TypeNetwerk tool code),
 #     to avoid confusion with the PageBot style dictionary, which hold style parameters.
 #
 from AppKit import NSFont
 from fontTools.ttLib import TTFont, TTLibError
+from pagebot.fonttoolbox.objects.glyph import Glyph
 from pagebot.fonttoolbox.objects.fontinfo import FontInfo
+from pagebot.contributions.adobe.getKerningPairsFromOTF import OTFKernReader
 
 def getFontPathOfFont(fontName):
     font = NSFont.fontWithName_size_(fontName, 25)
@@ -27,23 +31,27 @@ def getFontPathOfFont(fontName):
         return url.path()
     return None
 
-class Glyph(object):
-    def __init__(self, ttGlyph):
-        self.ttGlyph = ttGlyph
-        
 class Font(object):
     # Storage of font information while composing the pages.
+    GLYPH_CLASS = Glyph
+
     def __init__(self, path, name=None):
         self.path = path # File path of the font file.
         try: 
             self.ttFont = TTFont(path, lazy=True)
             self.info = FontInfo(self.ttFont) # TTFont is available as lazy style.info.font
             self.name = name # Keep original DrawBot name. Otherwise use from FontInfo
+            self.path = path
+            self._kerning = None # Lazy reading.
+            self._groups = None # Lazy reading.
         except TTLibError:
             raise OSError('Cannot open font file "%s"' % path)
- 
+
+    def __repr__(self):
+        return self.path or self.name
+
     def __getitem__(self, glyphName):
-        return Glyph(self.ttFont[glyphName])
+        return self.GLYPH_CLASS(self, glyphName)
 
     def _get_axes(self): # Answer dictionary of axes
         try:
@@ -53,3 +61,16 @@ class Font(object):
         return axes
     axes = property(_get_axes)
 
+    def _get_kerning(self):
+        if self._kerning is None: # Lazy read.
+            self._kerning = OTFKernReader(self.path).kerningPairs
+        return self._kerning
+    kerning =  property(_get_kerning)
+
+    def _get_groups(self):
+        return self._groups
+    groups = property(_get_groups)
+
+    def save(self, path=None):
+        u"""Save the font to optional path or to self.path."""
+        self.ttFont.save(path or self.path)
