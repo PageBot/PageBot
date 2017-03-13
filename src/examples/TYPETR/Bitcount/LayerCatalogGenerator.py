5e3d7a770215f17e0b9c2ce8519f4cfb1d1141b3
petrvanblokland <buro@petr.com>
2017-03-13 23:35:21 +0100

    Working on automatic catalog generator for Bitcount and Bitpath
    Start of TnBlogBuilder class.

--- src/examples/TYPETR/Bitcount/LayerCatalogGenerator.py
+++ src/examples/TYPETR/Bitcount/LayerCatalogGenerator.py
@@ -27,93 +27,52 @@ Use_BitPath = True
 W = 500 # Width of the sample image. Heights is calculated from string.
  # Height of the sample image
 M = 32 # Margin between text and image side.
-Sample_Text = u'Typëtr' # Initial sample string
+Sample_Text = u'Typetr' # Initial sample string
 monoSpaced = True #random()<0.5
 Background_Color = NSColor.blackColor()
 Italics = False
 Layers = 3
-Actions = 'QQQ'
-color = True
+Color = True
 Layer_Offset_X = -2
 Layer_Offset_Y = 2
 
-Layer_Color_0 = None
-Layer_Color_1 = None
-Layer_Color_2 = None
-Layer_Color_3 = None
-Layer_Color_4 = None
-Layer_Color_5 = None
+familyName = 'Bitcount'
+if monoSpaced:
+    searchName = familyName + 'Mono'
+else:
+    # Only works in layers, if also Single/Double is selected.
+    searchName = familyName + 'Prep'
 
-def clearFonts():
-    myglobals.layerFonts = {0: None, 1:None, 2:None, 3:None, 4:None, 5:None} # 
- 
-# Kinda hack, storing in empty module, to prevent globals to re-initialized, if variables are changed.
-import myglobals
-
-if not hasattr(myglobals, 'initialized'):
-    myglobals.initialized = True
-    # Store Italics flag, so we can test if it changed.
-    myglobals.italics = Italics
-    # Store Use_BitPath flag, so we can test if it changed.
-    myglobals.use_BitPath = Use_BitPath
-    # Currently (random) selected fonts per layer
-    clearFonts() # Initialize font choice, so new random fonts will be selected.
-    myglobals.layerColors = {0:NSColor.grayColor(), 1:NSColor.grayColor(), 2:NSColor.grayColor(), 
-        3:NSColor.grayColor(), 4:NSColor.grayColor(), 5:NSColor.grayColor()} # 
-    # Collections of avaiable fonts, filtered by weight and stem-pixel 
-    myglobals.fontNamePaths = {}
-    myglobals.lightPaths = {} # Not Bold or Black
-    myglobals.boldPaths = {}
-    myglobals.singleLightPaths = {} # Not Bold or Black
-    myglobals.singleBoldPaths = {}
-    myglobals.doubleLightPaths = {} # Not Bold or Black
-    myglobals.doubleBoldPaths = {}
+lightPaths = {}
+boldPaths = {}
+singleLightPaths = {}
+singleBoldPaths = {}
+doubleLightPaths = {}
+doubleBoldPaths = {}
 
 typetrStoreUrl = 'https://store.typenetwork.com/foundry/typetr'
 EXPORT_DIR = '_export/BitcountLayerCatalog/'
-EXPORT_IMAGES = EXPORT_DIR + 'images'
+EXPORT_IMAGES = EXPORT_DIR + 'images/'
+EXPORT_PATH = EXPORT_IMAGES + 'LayerCatalogImage%04d.png'
+
 # Make sure the export directory exists
 if not os.path.exists(EXPORT_IMAGES):
     os.makedirs(EXPORT_IMAGES)
 
-
-if Italics != myglobals.italics: # Did it change?
-    myglobals.italics = Italics
-    # Force new random selection of fonts.
-    clearFonts()
-
-if Use_BitPath != myglobals.use_BitPath: # Did it changed?
-    myglobals.use_BitPath = Use_BitPath
-    # Force new random selection of fonts.
-    clearFonts()
-
-# Compare stored colors
-for layerIndex in range(Layers):
-    if globals()['Layer_Color_%d' % layerIndex] is None:
-        globals()['Layer_Color_%d' % layerIndex] = myglobals.layerColors[layerIndex]
-    elif myglobals.layerColors[layerIndex] != globals()['Layer_Color_%d' % layerIndex]:
-        myglobals.layerColors[layerIndex] = globals()['Layer_Color_%d' % layerIndex]
-          
-def collectFonts():
+def collectFonts(searchName):
     # Bitcount is made from 3 spacing sets: 
     #    - Monospaced (Grid, Mono)
     #    - Proportional (Prop)
     # Define the family name of fonts that we want to use, 
     # including the type of spacing.
-    if myglobals.fontNamePaths: # Already installed, skip.
-        return
         
-    if monoSpaced:
-        familyName = 'BitcountMono'
-    else:
-        familyName = 'BitcountProp' # Only works in layers, if also Single/Double is selected.
     
     # Just get paths of the fonts, not the Font objects. 
     # We want quick interactive response.
     # Call imported method, to find all installed Bitcount fonts.
-    myglobals.fontNamePaths = getFamilyFontPaths(familyName) 
+    fontNamePaths = getFamilyFontPaths(searchName) 
         
-    for fontName in myglobals.fontNamePaths:
+    for fontName in fontNamePaths:
         if 'Prop' in fontName:
             print fontName
 
@@ -126,33 +85,34 @@ def collectFonts():
 
         bitpathPaths = getFamilyFontPaths(familyName)
         for fontName, fontPath in bitpathPaths.items(): # Merge the dictionaries
-            myglobals.fontNamePaths[fontName] = fontPath
+            fontNamePaths[fontName] = fontPath
 
     # Remove all italic or roman, depending on setting of [x] Italics checkbox.
     if not Italics:
-        for fontName in myglobals.fontNamePaths.keys():
+        for fontName in fontNamePaths.keys():
             if 'Italic' in fontName:
-                del myglobals.fontNamePaths[fontName]
+                del fontNamePaths[fontName]
     elif Italics:
-        for fontName in myglobals.fontNamePaths.keys():
+        for fontName in fontNamePaths.keys():
             if not 'Italic' in fontName:
-                del myglobals.fontNamePaths[fontName] # Remove non-italics
+                del fontNamePaths[fontName] # Remove non-italics
 
     # Filter types of usage
-    for fontName, fontPath in myglobals.fontNamePaths.items():
+    for fontName, fontPath in fontNamePaths.items():
         if 'Bold' in fontName or 'Black' in fontName:
-            myglobals.boldPaths[fontName] = fontPath
+            boldPaths[fontName] = fontPath
             if 'Single' in fontName:
-                myglobals.singleBoldPaths[fontName] = fontPath
+                singleBoldPaths[fontName] = fontPath
             else:
-                myglobals.doubleBoldPaths[fontName] = fontPath
+                doubleBoldPaths[fontName] = fontPath
         else:
-            myglobals.lightPaths[fontName] = fontPath
+            lightPaths[fontName] = fontPath
             if 'Single' in fontName:
-                myglobals.singleLightPaths[fontName] = fontPath
+                singleLightPaths[fontName] = fontPath
             else:
-                myglobals.doubleLightPaths[fontName] = fontPath
-
+                doubleLightPaths[fontName] = fontPath
+    return fontNamePaths
+    
 def getColor(index):
     g = globals()
     label = 'Layer_Color_%d' %  index
@@ -161,14 +121,10 @@ def getColor(index):
     return g[label]
     
 def getFontName(index):
-    if myglobals.layerFonts.get(index) is None:
-        if index < Layers/2: # First half of layers, use bold/black/shadow as background.
-            fontName = choice(myglobals.boldPaths.keys())
-        else:
-            fontName = choice(myglobals.lightPaths.keys())
-        myglobals.layerFonts[index] = fontName
+    if index < Layers/2: # First half of layers, use bold/black/shadow as background.
+        fontName = choice(boldPaths.keys())
     else:
-        fontName = myglobals.layerFonts[index]   
+        fontName = choice(lightPaths.keys())
     return fontName
        
 # Define method to show a random sample
@@ -185,24 +141,14 @@ def getFittingString(t, fontName, layerIndex, fontSize=None):
     # Make formatted string of large type. 
     # Then see if it fits and calculate the fitting size.
     # First guess, to see if constructed formatted string fits.
-    initialFontSize = 500 
-    glb = globals()
-    colorLabel = 'Layer_Color_%d' % layerIndex
-    layerColor = getColor(layerIndex)
-    if glb.get(colorLabel) is None:
-        glb[colorLabel] = NSColor.whiteColor()
-        
-    if layerColor is not None:
-        r, g, b, opacity = layerColor.getRed_green_blue_alpha_(None, None, None, None)
-    else:
+    initialFontSize = 500         
+    if Color:
         r = random()
         g = random()
         b = random()
-        opacity = 0.4+0.6*random() # Not totally opaque.
-        glb[colorLabel] = NSColor.colorWithCalibratedRed_green_blue_alpha_(r, g, b, opacity)
-
-    if not color:
+    else:
         r = g = b = random()*0.8
+    opacity = 0.4+0.6*random() # Not totally opaque.
     
     if fontSize is None:
         # Calculate the size for the given string for the selected font/spacing.
@@ -218,39 +164,28 @@ def getFittingString(t, fontName, layerIndex, fontSize=None):
                
 def drawLayers(fss, fontSize):
     # Draw this layer in a couple of frame
-    offsetX = 0
-    offsetY = 0
+    x = M
+    y = M
     _, h = fss[0].size()
-    h += M
+    h += M/2
     newPage(W, h)
     fill(Background_Color)
     rect(0, 0, W, h)
     for fs in fss:
-        text(fs, (M+offsetX, M+offsetY))
-        offsetX += Layer_Offset_X
-        offsetY += Layer_Offset_Y
-
-UI = [
-    dict(name='Sample_Text', ui='EditText', args=dict(text=u'Typetr')),
-    dict(name='Italics', ui='CheckBox'),
-    dict(name='Use_BitPath', ui='CheckBox'), # Optional usage mixture with Bitpath.
-    dict(name='Background_Color', ui='ColorWell'),
-    #dict(name='Layers', ui='PopUpButton', args=dict(items=('0','1','2','3','4','5','6'))),
-    dict(name="Layer_Offset_X", ui="Slider", args=dict(value=0, minValue=-16, maxValue=16)),
-    dict(name="Layer_Offset_Y", ui="Slider", args=dict(value=0, minValue=-16, maxValue=16)),
-]
-for layerIndex in range(Layers):
-    UI.append(dict(name='Layer_Color_%d' % layerIndex, ui='ColorWell'))
-    
-UI.append(dict(name="Actions", ui="PopUpButton", args=dict(items=('New Random Fonts', 'New Random Color', 'Save'))))
-    
-Variable(UI, globals())
-       
+        text(fs, (x, y))
+        x += Layer_Offset_X
+        y += Layer_Offset_Y
         
 # If no Bitcount fonts could be found, open the browser on the TypeNetwork shop page and stop this script.
-collectFonts() # Collect available fonts, filter into characteristics, as weight, italic, etc.
-if not myglobals.fontNamePaths:
-    os.system('open %s/fonts/%s' % (typetrStoreUrl, 'productus')) #familyName.lower())
+fontNamePaths = collectFonts(searchName) # Collect available fonts, filter into characteristics, as weight, italic, etc.
+if not fontNamePaths:
+    os.system('open %s/fonts/%s' % (typetrStoreUrl, familyName.lower()))
 else:
     drawSample()
-    #saveImage(EXPORT_PATH) # Save the sample as file or animated gif.
+    # Find non-existing name
+    for n in range(10000):
+        filePath = EXPORT_PATH % n
+        if not os.path.exists(filePath):
+            saveImage(filePath) # Save the sample as png.
+            break # Make sure to break, or else 10000 copies are created.
+            
