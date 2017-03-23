a515fb8bf47f11b86e55b52c147172f58ec32288
petrvanblokland <buro@petr.com>
2017-03-23 19:47:10 +0100

    Start of Condition and Transformation class
    Former-commit-id: 202026caf03bc712d3f0ec1eea0acd7a43842b9c [formerly e5f29b763ab2f4c9c45e7cff30862400cffb5595]
    Former-commit-id: f5bab682b5de8b056c031812a39b8ad90f0f1ca6
    Former-commit-id: f3abd0bd31e1def51396f23d58778e02a6710805
    Former-commit-id: 6a5341c501ec555b74db6c29dc1bce510e3415e7

---
+++ src/pagebot/transformation.py
@@ -0,0 +1,26 @@
+# -*- coding: UTF-8 -*-
+# -----------------------------------------------------------------------------
+#
+#     P A G E B O T
+#
+#     Copyright (c) 2016+ Type Network, www.typenetwork.com, www.pagebot.io
+#     Licensed under MIT conditions
+#     Made for usage in DrawBot, www.drawbot.com
+# -----------------------------------------------------------------------------
+#
+#     transformation.py
+#
+#     (For now) experimental approach. 
+#     Transformation instances can be added to elements to define their intentioal
+#     behavior when rendering pages.
+#     The Composer can try to apply transformations to elements in order to improve
+#     the composition of a page, if there are Conditions related to elements that
+#     don't fully meet the requirements.
+#
+class Condition(objectx):
+    def __init__(self):
+        pass
+
+class Transformation(object):
+    def __init__(self):
+        pass
