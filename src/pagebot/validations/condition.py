de404f8f04256cfaf436e461d0670002c69302b6
petrvanblokland <buro@petr.com>
2017-03-24 23:16:34 +0100

    Starting with experimental validator/conditions for Composer to work with.
    This could replace manual page layout as well as templates and grid.
    Thrilling to see how far we can extend this and scale up, without
    breaking into infinite permutations.

---
+++ src/pagebot/validations/condition.py
@@ -0,0 +1,15 @@
+# -*- coding: UTF-8 -*-
+# -----------------------------------------------------------------------------
+#
+#     P A G E B O T
+#
+#     Copyright (c) 2016+ Type Network, www.typenetwork.com, www.pagebot.io
+#     Licensed under MIT conditions
+#     Made for usage in DrawBot, www.drawbot.com
+# -----------------------------------------------------------------------------
+#
+#     condition.py
+#
+class Condition(object):
+    def __init__(self, transformations=None):
+        self.transformations = transformations or []
---
+++ src/pagebot/validations/validator.py
@@ -0,0 +1,30 @@
+# -*- coding: UTF-8 -*-
+# -----------------------------------------------------------------------------
+#
+#     P A G E B O T
+#
+#     Copyright (c) 2016+ Type Network, www.typenetwork.com, www.pagebot.io
+#     Licensed under MIT conditions
+#     Made for usage in DrawBot, www.drawbot.com
+# -----------------------------------------------------------------------------
+#
+#     validator.py
+#
+#     (For now) experimental approach. 
+#     A validator contains a set of condition, that may (partially) be in conflict.
+#     The validation takes a theshold value for the minimum “quality level” of all
+#     conditions together, when applied on a container tree of elements.
+#     This was validations can work on a page layout as a whole, but also on the
+#     content of single containers.
+#
+class Validator(object):
+    def __init__(self, conditions):
+        self.conditions = conditions
+
+    def evaluate(self, e):
+        u"""Evaluate the content of element e with the total sum of conditions."""
+        qualityLevel = 0
+        for condition in self.conditions:
+            qualityLevel += e.testCondition(condition)
+        return qualityLevel
+         
