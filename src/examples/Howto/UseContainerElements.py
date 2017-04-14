81729fb4e80e93bc5b1fc7f0e2a0c621bf720829
petrvanblokland <buro@petr.com>
2017-04-14 11:26:26 +0200

    Adjust 2 Howto examples to new position method of elements.

--- src/examples/Howto/UseContainerElements.py
+++ src/examples/Howto/UseContainerElements.py
@@ -7,9 +7,10 @@
 #     Made for usage in DrawBot, www.drawbot.com
 # -----------------------------------------------------------------------------
 #
-#     ScalingAnImage.py
+#     UseContainerElements.py
 #
-#     How to scale an image (without being an element) in plain DrawBot?
+#     Container element hold an ordered list of elements.
+#     Each element knows its own position.
 #
 import os # Import module that communicates with the file system.
 
@@ -17,33 +18,34 @@ import pagebot.elements.container
 reload(pagebot)
 from pagebot.elements.container import Container
 
-c = Container()
+c = Container(name='myContainerElement')
 
 print 'Container we made:', c 
-print 'No elements yet:', c.getElements() # Currently no elements in the container 
+print 'No elements yet:', c.elements # Currently no elements in the container 
 print
-child1 = Container(eId='Child1') # Make child containers with unique Id
+child1 = Container(eId='myId1', name='Child1') # Make child containers with unique Id
 c.append(child1)
 
-child2 = Container(eId='Child2') 
+child2 = Container(eId='myId2', name='Child2')
 c.append(child2)
 
 print '-- Now the container got 2 named child containers.'
-print 'Elements:', c.getElements() # Currently no elements in the container 
+print 'Elements:', c.elements # Currently no elements in the container 
 print
-print '-- None of the children are placed on a fixed position at this time.'
-print c.getPositions()
+print '-- None of the children are placed on default position (0, 0, 0)'
+for e in c.elements:
+    print e.name, e.x, e.y, e.z
 print
-print '-- Place the Child1 element on a fixed position (x,y), z is undefined'
-c.place(child1, (20,30))
-print c.getElementsPosition()
+print '-- Place the Child1 element on a fixed position (x,y), z is undefined/untouched'
+child1.x = 20
+child1.y = 30
+print child1
 print
-print '-- Place the same Child2 element on another fixed position (x,y,z)'
-c.place(child2, (120,30, 20))
-print c.getElementsPosition()
-print
-print '-- Now the children are placed on a fixed position.'
-print c.getPositions()
+print '-- Place the same Child2 element on another fixed position (x,y,z), a point tuple.'
+child2.point = (120, 30, 20)
+print child2
 print
 print '-- The container behaves as a dictionary of child elements with e.eId as key.'
-print c['Child1'], c['Child2']
+print c['myId1']
+print c['myId2']
+
