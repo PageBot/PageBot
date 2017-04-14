b4ca2fb69f5b537aeea4fb0b4716a124688492ec
petrvanblokland <buro@petr.com>
2017-04-14 14:01:58 +0200

    Add text sample and size sliders

---
+++ src/examples/SinglePageExamples/myglobals.py
@@ -0,0 +1,9 @@
+# -----------------------------------------------------------------------------
+#     Copyright (c) 2016+ Type Network, www.typenetwork.com, www.pagebot.io
+#
+#     P A G E B O T
+#
+#     Licensed under MIT conditions
+#     Made for usage in DrawBot, www.drawbot.com
+# -----------------------------------------------------------------------------
+#
