d7dd85d80007eb9723ea8f42fd86c2dbfc2d1046
petrvanblokland <buro@petr.com>
2017-04-30 21:24:42 +0200

    Working on TextLine and TextRun example classes.
    Former-commit-id: 28d1b79872ae0d33afd44821fb65cf46426f8bba [formerly 4f74c0a240308ec0ac3427d68268488c02d755ca]
    Former-commit-id: eb17050888b36141b404e5a578c3ed9362708159
    Former-commit-id: c35a13ed31b6fb670744107d07f546e02f61f834
    Former-commit-id: e04d8efe9286da0db2150fc0f036663b7ac4c165

--- src/pagebot/elements/textbox.py
+++ src/pagebot/elements/textbox.py
@@ -20,6 +20,285 @@ from pagebot.elements.element import Element
 from pagebot.toolbox.transformer import pointOffset
 from pagebot import getFormattedString, setStrokeColor, setFillColor
 
+
+import CoreText
+import Quartz
+
+#for name in installedFonts():
+#    if 'Bitcount' in name:
+#        print name
+        
+class TextRun(object):
+    def __init__(self, ctRun, string):
+        self.lineString = string
+        self._ctRun = ctRun
+        self.glyphCount = gc = CoreText.CTRunGetGlyphCount(ctRun)
+
+        attrs = CoreText.CTRunGetAttributes(ctRun)
+        self.nsFont = attrs['NSFont']
+        #self.fontDescriptor = f.fontDescriptor()
+        self.fill = attrs['NSColor']
+        self.nsParagraphStyle = attrs['NSParagraphStyle']
+
+
+        i1, i2 = CoreText.CTRunGetStringRange(ctRun)
+        self.string = self.lineString[i1, i2]
+
+        self.stringIndices = CoreText.CTRunGetStringIndicesPtr(ctRun)[0:gc]
+        #CoreText.CTRunGetStringIndices(ctRun._ctRun, CoreText.CFRange(0, 5), None)[4]
+        self.advances = CoreText.CTRunGetAdvances(ctRun, CoreText.CFRange(0, 5), None)
+        self.positions = CoreText.CTRunGetPositionsPtr(ctRun)[0:gc]
+        #CoreText.CTRunGetPositions(ctRun, CoreText.CFRange(0, 5), None)[4]
+        #self.glyphFontIndices = CoreText.CTRunGetGlyphsPtr(ctRun)[0:gc]
+        #print CoreText.CTRunGetGlyphs(ctRun, CoreText.CFRange(0, 5), None)[0:5]
+        self.status = CoreText.CTRunGetStatus(ctRun)
+
+    def __len__(self):
+        return self.glyphCount
+
+    # Font stuff
+
+    def _get_displayName(self):
+        return self.nsFont.displayName()
+    displayName = property(_get_displayName)
+
+    def _get_familyName(self):
+        return self.nsFont.familyName()
+    familyName = property(_get_familyName)
+
+    def _get_fontName(self):
+        return self.nsFont.fontName()
+    fontName = property(_get_fontName)
+
+    def _get_isVertical(self):
+        return self.nsFont.isVertical()
+    isVertical = property(_get_isVertical)
+
+    def _get_isFixedPitch(self):
+        return self.nsFont.isFixedPitch()
+    isFixedPitch = property(_get_isFixedPitch)
+
+    def _get_boundingRectForFont(self):
+        (x, y), (w, h) = self.nsFont.boundingRectForFont()
+        return x, y, w, h
+    boundingRectForFont = propert(_get_boundingRectForFont)
+
+    def _get_renderingMode(self):
+        return self.nsFont.renderingMode()
+    renderingMode = property(_get_renderingMode)
+
+    #   Font metrics
+
+    def _get_ascender(self):
+        return self.nsFont.ascender()
+    ascender = property(_get_ascender)
+    
+    def _get_descender(self):
+        return self.nsFont.descender()
+    descender = property(_get_descender)
+    
+    def _get_capHeight(self):
+        return self.nsFont.capHeight()
+    capHeight = property(_get_capHeight)
+    
+    def _get_xHeight(self):
+        return self.nsFont.xHeight()
+    xHeight = property(_get_xHeight)
+
+    def _get_italicAngle(self):
+        return self.nsFont.italicAngle()
+    italicAngle = property(_get_italicAngle)
+
+    def _get_fontSize(self):
+        return self.nsFont._get_fontSize()
+    fontSize = property(_get_fontSize)
+
+    def _get_leading(self):
+        return self.nsFont.leading()
+    leading = property(_get_leading)
+    
+    def _get_fontMatrix(self):
+        return self.nsFont.matrix()
+    fontMatrix = property(_get_fontMatrix)
+
+    def _get_textTransform(self):
+        return self.nsFont.textTransform()
+    textTransform = property(_get_textTransform)
+
+    def _get_underlinePosition(self):
+        return self.nsFont.underlinePosition()
+    underlinePosition = property(_get_underlinePosition)
+
+    def _get_underlineThickness(self):
+        return self.nsFont.underlineThickness()
+    underlineThickness = property(_get_underlineThickness)
+
+    #   Paragraph attributes
+
+    def _get_matrix(self):
+        return CoreText.CTRunGetTextMatrix(self._ctRun)
+    matrix = property(_get_matrix)
+
+    def _get_alignment(self):
+        return self.nsParagraphStyle.alignment()
+    alignment = property(_get_alignment)
+
+    def _get_lineSpacing(self):
+        return self.nsParagraphStyle.lineSpacing()
+    lineSpacing = property(_get_lineSpacing)
+
+    def _get_paragraphSpacing(self):
+        return self.nsParagraphStyle.paragraphSpacing()
+    paragraphSpacing = property(_get_paragraphSpacing)
+
+    def _get_paragraphSpacingBefore(self):
+        return self.nsParagraphStyle.paragraphSpacingBefore()
+    paragraphSpacingBefore = property(_get_paragraphSpacingBefore)
+
+    def _get_headIndent(self):
+        return self.nsParagraphStyle.headIndent()
+    headIndent = property(_get_headIndent)
+
+    def _get_tailIndent(self):
+        return self.nsParagraphStyle.tailIndent()
+    tailIndent = property(_get_tailIndent)
+
+    def _get_firstLineHeadIndent(self):
+        return self.nsParagraphStyle.firstLineHeadIndent()
+    firstLineHeadIndent = property(_get_firstLineHeadIndent)
+
+    def _get_lineHeightMultiple(self):
+        return self.nsParagraphStyle.lineHeightMultiple()
+    lineHeightMultiple = property(_get_lineHeightMultiple)
+
+    def _get_lineSpacing(self):
+        return self.nsParagraphStyle.lineSpacing()
+    lineSpacing = property(_get_lineSpacing)
+
+    def _get_maximumLineHeight(self):
+        return self.nsParagraphStyle.maximumLineHeight()
+    maximumLineHeight = property(_get_maximumLineHeight)
+
+    def _get_minimumLineHeight(self):
+        return self.nsParagraphStyle.minimumLineHeight()
+    minimumLineHeight = property(_get_minimumLineHeight)
+
+        
+class TextLine(object):
+    def __init__(self, ctLine):
+        self._ctLine = ctLine
+        self.glyphCount = CoreText.CTLineGetGlyphCount(ctLine)
+        # Hack for now to find the string.
+        s = []
+        for index, part in enumerate(`self._ctLine`.split('"')[1].split('\\u')):
+            if index == 0:
+                s.append(part)
+            elif len(part) >= 4:
+                s.append(unichr(int(part[0:4], 16)))
+                s.append(part[4:])
+        self.string = ''.join(s)
+
+        self.runs = []
+        for ctRun in CoreText.CTLineGetGlyphRuns(ctLine):
+            self.runs.append(TextRun(ctRun, self.string))
+
+    def __repr__(self):
+        print self._ctLine
+
+    def __len__(self):
+        return self.glyphCount
+
+    def getIndexForPosition(self, (x, y)):
+        return CoreText.CTLineGetStringIndexForPosition(self._ctLine, CoreText.CGPoint(x, y))
+    
+    def getOffsetForStringIndex(self, i):
+        return CoreText.CTLineGetOffsetForStringIndex(self._ctLine, i, None)
+                
+    def _get_stringIndex(self):
+        return CoreText.CTLineGetStringRange(self._ctLine).location
+    stringIndex = property(_get_stringIndex)
+       
+    #def _get_alignment(self):
+    #    return CoreText.CTTextAlignment(self._ctLine)
+    #alignment = property(_get_alignment)
+            
+    def _get_imageBounds(self):
+        (x, y), (w, h) = CoreText.CTLineGetImageBounds(self._ctLine,None)
+        return x, y, w, h
+    imageBounds = property(_get_imageBounds)
+    
+    def _get_bounds(self):
+        return CoreText.CTLineGetTypographicBounds(self._ctLine,None,None,None)
+    bounds = property(_get_bounds)
+    
+    def _get_trailingWhiteSpace(self):
+        return CoreText.CTLineGetTrailingWhitespaceWidth(self._ctLine)
+    trailingWhiteSpace = property(_get_trailingWhiteSpace)
+           
+def getTextLines(fs, w, h):
+    u"""Answer an ordered list of all baseline position, starting at the top."""
+    box = 0, 0, w, h
+    attrString = fs.getNSObject()
+    setter = CoreText.CTFramesetterCreateWithAttributedString(attrString)
+    path = Quartz.CGPathCreateMutable()
+    Quartz.CGPathAddRect(path, None, Quartz.CGRectMake(*box))
+    box = CoreText.CTFramesetterCreateFrame(setter, (0, 0), path, None)
+    ctLines = CoreText.CTFrameGetLines(box)
+    textLines = []
+    for ctLine in ctLines:
+        tl = TextLine(ctLine)
+        textLines.append(tl)
+    origins = CoreText.CTFrameGetLineOrigins(box, (0, len(ctLines)), None)
+    return textLines, origins
+
+fs = FormattedString(u'This åéöøa', font='BitcountMonoDouble-MediumCircleItalic', fontSize=24)
+fs = fs + FormattedString('This an example. ' * 20, font='Verdana', fontSize=16)
+
+textLines, o = getTextLines(fs, 500, 200)
+#print o
+#print
+for tl in textLines:
+    #print 'Bounds:', tl.bounds, tl.imageBounds, tl.trailingWhiteSpace, 'Len:', len(tl), 'Index:', tl.stringIndex, 
+    #tl.alignment
+    #print 'Index at position (100, 10):', tl.getIndexForPosition((100, 10))
+    #print 'Offset at string index (0):', tl.runs
+    print 'Runs:', len(tl.runs)
+    print 'String:', tl.string
+    for textRun in tl.runs:
+        """
+        print CoreText.CTRunGetTextMatrix(ctRun._ctRun)
+        print CoreText.CTRunGetStringRange(ctRun._ctRun)
+        print CoreText.CTRunGetStringIndicesPtr(ctRun._ctRun)[20]
+        print CoreText.CTRunGetStringIndices(ctRun._ctRun, CoreText.CFRange(0, 5), None)[4]
+        print CoreText.CTRunGetAdvances(ctRun._ctRun, CoreText.CFRange(0, 5), None)
+        print CoreText.CTRunGetPositionsPtr(ctRun._ctRun)[20]
+        print CoreText.CTRunGetPositions(ctRun._ctRun, CoreText.CFRange(0, 5), None)[4]
+        print CoreText.CTRunGetGlyphsPtr(ctRun._ctRun)[20]
+        print CoreText.CTRunGetGlyphs(ctRun._ctRun, CoreText.CFRange(0, 5), None)[0:5]
+        print CoreText.CTRunGetStatus(ctRun._ctRun)
+        print CoreText.CTRunGetAttributes(ctRun._ctRun)['NSColor']
+        print CoreText.CTRunGetAttributes(ctRun._ctRun)['NSParagraphStyle']
+        print CoreText.CTRunGetAttributes(ctRun._ctRun)['NSFont']
+        print ctRun._ctRun
+        """
+        print textRun.nsFont, textRun.glyphFontIndices, textRun.positions
+        print '========'
+        print textRun.displayName
+        print textRun.familyName
+        print textRun.fontName
+        print textRun.glyphCount
+        print textRun.fontDescriptor
+        print textRun.isFixedPitch
+        print textRun.renderingMode
+        print textRun.capHeight
+        print textRun.ascender
+        print textRun.descender
+        print textRun.fontBoundingRect
+        print textRun.italicAngle
+        print
+        #print CoreText.CTLineGetGlyphRuns(tl._ctLine)
+    print #CoreText.CTLineGetStringRange(tl._ctLine)
+    #print tl.string       
 class TextBox(Element):
 
     # Initialize the default behavior tags as different from Element.
