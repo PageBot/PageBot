25fa41d0405193bca07929cd9b054eb9d92e74a2
petrvanblokland <buro@petr.com>
2017-05-06 10:13:52 +0200

    Show all alignment positions.

--- src/examples/Howto/AlignElements.py
+++ src/examples/Howto/AlignElements.py
@@ -9,8 +9,7 @@
 #
 #     AlignElements.py
 #
-#     This script generates a page with random color squares, indicating where their position is.
-#     This script is using the style parameters "originTop", making the coordinate system run downwards.
+#     This script generates a page with aligned square, showing how conditional placement works.
 #
 import pagebot # Import to know the path of non-Python resources.
 
@@ -65,9 +64,39 @@ def makeDocument(rs):
     # Make new container for adding elements inside with alignment.
     cnt = newRect(w=W-2*SQ, h=H-2*SQ, fill=(0.8, 0.8, 0.8, 0.4), parent=page, margin=SQ, yAlign=BOTTOM, 
         xAlign=CENTER, stroke=None, conditions=(Center2Center(), Middle2Middle()))
-    
-    newRect(x=100, y=100, w=SQ, h=SQ, stroke=None, parent=page, xAlign=CENTER,
-        conditions=(Center2Center(),Top2Top()), fill=(1, 1, 0))
+
+    newRect(w=SQ, h=SQ, stroke=None, parent=page, xAlign=CENTER,
+        conditions=(Center2Center(), Middle2Middle()), fill=(1, 0, 0))
+ 
+    conditions = [
+         (Center2Center(),Top2Top()),
+         (Center2Center(),Bottom2Bottom()),
+         (Left2Left(),Middle2Middle()),
+         (Right2Right(),Middle2Middle()),
+    ]   
+    for condition in conditions:
+        newRect(w=SQ, h=SQ, stroke=None, parent=page, xAlign=CENTER,
+            conditions=condition, fill=(1, 1, 0))
+     
+    sideConditions = [
+         (Center2Center(),Top2TopSide()),
+         (Center2Center(),Bottom2BottomSide()),
+         (Left2LeftSide(),Middle2Middle()),
+         (Right2RightSide(),Middle2Middle()),
+    ]   
+    for condition in sideConditions:
+        newRect(w=SQ, h=SQ, stroke=None, parent=page, xAlign=CENTER,
+            conditions=condition, fill=(0.5, 1, 0))
+
+    cornerConditions = [
+         (Left2LeftSide(),Top2TopSide()),
+         (Right2RightSide(),Top2TopSide()),
+         (Left2LeftSide(),Bottom2BottomSide()),
+         (Right2RightSide(),Bottom2BottomSide()),
+    ]   
+    for condition in cornerConditions:
+        newRect(w=SQ, h=SQ, stroke=None, parent=page, xAlign=CENTER,
+            conditions=condition, fill=(0, 0, 1))
 
     # Solve the layout placement conditions on the page by moving the
     # elements that are not on the right positions (which is all of them,
