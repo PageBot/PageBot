3f65b0dc5acf389d6b4a0ce7761f63c2c1cb2cd6
petrvanblokland <buro@petr.com>
2017-05-24 11:49:37 -0400

    Debugging error in ctRun glyph positions, thanks Frederik.
    Better HotMetalText example, showing shades on rectangle edges and
    working with ligatures.
    Add testing files.
    
    
    Former-commit-id: 575ee59695e212ae561b7827a764cdd0f9a76d98 [formerly 491afa91fc6b66fd45db28bcd3ed8bb450294cfe]
    Former-commit-id: 6796cbfdd140b0fa3093ee980a538def20f0f3af
    Former-commit-id: 0aa606b2e44178e1afccdd59d5e7f38f916cceb8
    Former-commit-id: c25c23f7f13bdc27e0450307c74d7b40e05cb658

---
+++ src/examples-in-progress/teststuff/testCTRunLigature.py
@@ -0,0 +1,62 @@
+import CoreText
+import Quartz
+
+from fontTools.ttLib import TTFont
+
+
+
+fontPath = u"Proforma-Bold"
+
+
+#fontToolsFont = TTFont(fontPath)
+#glyphOrder = fontToolsFont.getGlyphOrder()
+
+def ctRunThing(fs, (x, y)):
+    w, h = textSize(fs)
+    box = 0, 0, w, h
+    attrString = fs.getNSObject()
+    setter = CoreText.CTFramesetterCreateWithAttributedString(attrString)
+    path = Quartz.CGPathCreateMutable()
+    Quartz.CGPathAddRect(path, None, Quartz.CGRectMake(*box))
+    ctBox = CoreText.CTFramesetterCreateFrame(setter, (0, 0), path, None)
+    ctLines = CoreText.CTFrameGetLines(ctBox)
+    ctRuns = CoreText.CTLineGetGlyphRuns(ctLines[0])
+    
+    
+    # loop over all runs
+    for run in ctRuns:
+        # get all positions
+        pos = CoreText.CTRunGetPositions(run, (0, CoreText.CTRunGetGlyphCount(run)), None)
+        # get all glyphs
+        glyphs = CoreText.CTRunGetGlyphs(run, (0, CoreText.CTRunGetGlyphCount(run)), None)
+        print pos
+        
+        # enumerate over all pos   
+        for i, (gx, gy) in enumerate(pos):
+            # draw a line
+            stroke(0)
+            line((x+gx, y+gy), (x+gx, y+gy + 100))
+            stroke(None)
+            # get the glyph name from the glyph order
+            #glyphName = glyphOrder[glyphs[i]]
+            # get the shift to center the name of the glyph
+            centerShift = 0
+            if i < len(pos)-1:
+                centerShift = (pos[i+1][0] - gx)
+            else:
+                # last one
+                centerShift = w - gx
+            centerShift *= .5
+            #tx, _ = textSize(glyphName)
+            
+            #text(glyphName, (x+gx+centerShift-tx*.5, y+gy-20))
+                        
+
+    
+fs1 = FormattedString('Ligature fifl', font=fontPath, fontSize=100, openTypeFeatures=dict(liga=False))
+text(fs1, (100, 300))
+ctRunThing(fs1, (100, 300))
+
+fs2 = FormattedString('Ligature fifl', font=fontPath, fontSize=100, openTypeFeatures=dict(liga=True))
+text(fs2, (100, 100))
+ctRunThing(fs2, (100, 100))
