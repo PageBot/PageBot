9311a493aa982da3747b72142e3b3b74a9bd85c1
petrvanblokland <buro@petr.com>
2017-06-01 08:07:27 +0200

    Working on Table element.
    Former-commit-id: 6d0c3d95969116a7f5d11398eb8a19da30b4cba4 [formerly 76430d42da5a138a1000fa8ec3e9864871b71283]
    Former-commit-id: 908327627388d1152e7fe433583d8b92df333de6
    Former-commit-id: f16a202490665e884b0fc7b57ddf6fff7a8d2bb7
    Former-commit-id: e3b4d57b6747683ace744ee376c13c1b82894417

--- src/pagebot/elements/pbtable.py
+++ src/pagebot/elements/pbtable.py
@@ -21,31 +21,57 @@ from pagebot.elements.element import Element
 from pagebot.toolbox.transformer import pointOffset
 from pagebot import newFS, setStrokeColor, setFillColor
 from pagebot.fonttoolbox.objects.glyph import Glyph
+from pagebot.conditions import *
 
-          
+class Row(Element):
+    pass
+
+class Header(Row):
+    u"""The Table header is a special kind of row."""
+    pass
+
+class Cell(Element):
+   def __init__(self, fs, colSpan=1, rowSpan=1, **kwargs):
+        Element.__init__(self,  **kwargs)
+        self.colSpan = colSpan
+        self.rowSpan = rowSpan
+   
+class EmptyCell(Element):
+    u"""Placeholder cell to make index of columns match up if there are empty areas,
+    such as colspan and rowspan."""
+    pass
+
 class Table(Element):
+
+    HEADER_CLASS = Header
+    ROW_CLASS = Row
+    CELL_CLASS = Cell
+    EMPTYCELL_CLASS = EmptyCell
 
     def __init__(self, fs, cols=1, rows=1, **kwargs):
         Element.__init__(self,  **kwargs)
         # Make the column/row/cell dictionary
         self.initCells(cols, rows)
 
-    def initCells(self):
-        self.cells = {} # Key is col/row index.
+    def initCells(self, cols, rows):
+        self.HEADER_CLASS(parent=self)
         for col in range(cols):
-            for row in range(rows):
-                self.cells[(col, row)] = Cell(row=row, row=row, conditions=[])
+            for rowIndex in range(rows):
+                row = self.ROW_CLASS(parent=self, conditions=[Fit2Width()])
+                for colIndex in range(cols):
+                    cell = self.CELL_CLASS(parent=row)
+
+    def solve(self):
+        u"""The solve will align all rows and cells to the available space in Table,
+        and taking into account what the volume is that cells need and how they align
+        in columns and rows."""
 
     def draw(self, origin, view):
         p = pointOffset(self.oPoint, origin)
         p = self._applyScale(p)    
         px, py, _ = p = self._applyAlignment(p) # Ignore z-axis for now.
 
-        columnWidths = self._getColumnWidths()
-        rowHeights = self._getRowHeights()
-        for (x, y), cell in self.cells.items():
-            cell.draw((px+columWidth[x], py+rowHeights[y]), view)
-
-
-
+        for e in self.elements:
+            # TODO: Needs updated x/y value
+            e.draw(origin, view)
 
