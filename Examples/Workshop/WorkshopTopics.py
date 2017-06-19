c573d9e29151150d3c7f38d6b733523216c00ab7
petrvanblokland <buro@petr.com>
2017-06-19 12:43:37 -0400

    Adding workshop projects
    Working on SlowFasion example
    Make fonttoolbox.getVariableFont( ) take path or font.

---
+++ Examples/Workshop/WorkshopTopics.py
@@ -0,0 +1,36 @@
+import pagebot
+"""
+# Monday
+Python basics
+
+Drawbot basics
+Drawbot examples
+Drawbot FormattedString
+Drawbot classes
+Programming tools
+Markdown tools
+Pagebot architecture (classes, functions, pattern)
+
+# Tuesday
+Pagebot Howto 
+Pagebot Example
+Pagebot fonts (variable fonts, fonttools, RoboFont)
+Pagebot FormattedString
+Pagebot Element
+Pagebot Grid
+Pagebot Conditions
+Pagebot Style
+
+# Wednesday
+Pagebot builders (exporting other formats that standard drawbot)
+    Export HTML/JS/CSS/SVG (XPyth, Xierpa, Xierpa3)
+    Export JS-InDesign
+Make your design(s) into Pagebot code
+
+# Thursday
+Make your design(s) into Pagebot code
+Design theory, design the design process
+Pagebot 3D
+Pagebot timeline
+Pagebot future
+"""
