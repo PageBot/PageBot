1e6dc10ef10e3939c97257ca4f83d015fff22c90
petrvanblokland <buro@petr.com>
2017-06-21 08:38:24 -0400

    Adding TMP Xierpa3 HTML builder code to be included into PageBot html builders.
    Trying to solve info box text position problem, introduced with the
    latest DrawBot version.

---
+++ Lib/pagebot/builders/TMP_Xierpa3_builders/builderparts/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: UTF-8 -*-
+# -----------------------------------------------------------------------------
+#    xierpa server
+#    Copyright (c) 2014+  buro@petr.com, www.petr.com, www.xierpa.com
+#    
+#    X I E R P A  3
+#    Distribution by the MIT License.
+#
+# -----------------------------------------------------------------------------
