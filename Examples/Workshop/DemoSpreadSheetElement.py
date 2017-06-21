c6564f7fb397428e3644dee302ed22696f1cfa3d
petrvanblokland <buro@petr.com>
2017-06-21 15:24:23 -0400

    Adding new Element class
    Former-commit-id: 546011118fe3c8fb12d44b9bd0a9680fc998b906 [formerly e794a074a642d231b19e1ab1f025b6c3d5856cd3]
    Former-commit-id: 5d819ab99a5d0ec628caaa91278eab57b649f273
    Former-commit-id: 562a5924387c277d46757812e9aad51a7ed841c4
    Former-commit-id: e13fc7e24b651b1706061202ba1dd8e8d54560a2

--- Examples/Workshop/DemoSpreadSheetElement.py
+++ Examples/Workshop/DemoSpreadSheetElement.py
@@ -7,6 +7,9 @@ from pagebot.conditions import *
 from pagebot.style import A5, TOP
 from pagebot.contributions.filibuster.blurb import Blurb
 
+class DemoSpreadSheet(Element):
+    pass
+    
 W, H = A5
 
 doc = Document(w=W, h=H, originTop=False, autoPages=1)
@@ -19,6 +22,9 @@ view.showFlowConnections = True
 page = doc[0]
 page.padding = int(page.h/12), int(page.w/12)
 
+DemoSpreadSheet(parent=page, width=200, height=200, fill=0.8,
+    conditions=[Left2Left(), Fit2Width(), Top2Top()]
+)
 #print he.x, he.y 
 print page.solve()
 #print he.x, he.y 
