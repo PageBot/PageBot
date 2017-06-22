4cc1e7b3c5412ad8a65ed18079b290b86c1b8acd
petrvanblokland <buro@petr.com>
2017-06-22 00:16:31 -0400

    Some sketching on HTML builder.
    Former-commit-id: c1fa9ada72bc4b7e8e456adb05928ac440ba7600 [formerly dd86108c16fac5e04d1ce1211ac23544b08dea7f]
    Former-commit-id: 7cc466b0acce592b48a32145d2e5a90ac93ef352
    Former-commit-id: 855c04e0b36583d32c39c166dd2538c07e8e9426
    Former-commit-id: 9c66ed516cc338b02514c434a59aa20dfae960d4

---
+++ Examples/Howto/BuildMampShadow.py
@@ -0,0 +1,79 @@
+# -----------------------------------------------------------------------------
+#     Copyright (c) 2016+ Type Network, www.typenetwork.com, www.pagebot.io
+#
+#     P A G E B O T
+#
+#     Licensed under MIT conditions
+#     Made for usage in DrawBot, www.drawbot.com
+# -----------------------------------------------------------------------------
+#
+#     UseShadow.py
+#
+from pagebot import newFS
+from pagebot.style import getRootStyle, BOTTOM, CENTER, MIDDLE
+from pagebot.document import Document
+from pagebot.elements import *
+from pagebot.conditions import *
+from pagebot import Gradient, Shadow
+from pagebot.builders.htmlcss.mampbuilder import MampBuilder
+    
+W = 400
+H = 480
+RectSize = 300
+ShadowOffset = 0
+ShadowBlur = 10
+ShadowTextOffset = 0
+ShadowTextBlur = 10
+
+def makeDocument():
+    
+    doc = Document(originTop=False, w=W, h=H, autoPages=1) 
+    
+    page = doc[0] # Get the first/single page of the document.
+    page.padding = 40 # TODO: order if 4 values?
+    
+    # Make rect as page element centered with centered origin.
+    conditions = (Center2Center(), Middle2Middle())
+    shadow = Shadow(offset=(ShadowOffset, -ShadowOffset), 
+        blur=ShadowBlur, 
+        color=(0.2, 0.2, 0.2, 0.5)
+    )
+    textShadow = Shadow(offset=(ShadowTextOffset, -ShadowTextOffset), 
+        blur=ShadowTextBlur, 
+        color=(0.2, 0.2, 0.2, 0.5)
+    )
+    fs = newFS('This is text with a shadow', 
+        style=dict(font='Verdana', fontSize=30, textFill=0, rLeading=1.2))
+    
+    newTextBox(fs, fill=0.8, parent=page, 
+        w=RectSize, h=RectSize, shadow=shadow, textShadow=textShadow,
+        conditions=conditions, xAlign=CENTER, yAlign=MIDDLE)
+    # Solve the layout conditions of the red rectangle.
+    # Show if one of the conditions failed to solve.
+    score = page.solve()
+    if score.fails:
+        print 'Failed conditions', score.fails
+        
+    # Set the view parameters for the required output.
+    view = doc.getView()
+    view.padding = 0 # Make view padding to show crop marks and frame
+    view.showPageFrame = True # Show frame of the page in blue
+    #view.showPagePadding = True
+    view.showPageCropMarks = True # Show crop marks
+    
+    return doc
+   
+
+Variable([
+    dict(name='ShadowOffset', ui='Slider', 
+        args=dict(minValue=-50, value=6, maxValue=50)),
+    dict(name='ShadowBlur', ui='Slider', args=dict(minValue=0, value=10, maxValue=50)),
+    dict(name='ShadowTextOffset', ui='Slider', 
+        args=dict(minValue=0, value=6, maxValue=50)),
+    dict(name='ShadowTextBlur', ui='Slider', args=dict(minValue=0, value=4, maxValue=10)),
+    dict(name='RectSize', ui='Slider', args=dict(minValue=10, value=W/2, maxValue=W)),
+], globals())
+     
+d = makeDocument()
+d.export('_export/DrawRedRectCenterPage.png')   
+MampBuilder().build(d, u"/Applications/MAMP/htdocs/pbshadows")
