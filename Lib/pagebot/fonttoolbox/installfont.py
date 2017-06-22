3f1b3b590467739ab4a602ff50552aaa5085754b
petrvanblokland <buro@petr.com>
2017-06-21 21:52:31 -0400

    Add installfont.py to be used to install a font in user space, outside scope of DrawBot
    This way we can create Variable instances, and then have them available
    in InDesign.

---
+++ Lib/pagebot/fonttoolbox/installfont.py
@@ -0,0 +1,109 @@
+# -*- coding: UTF-8 -*-
+# -----------------------------------------------------------------------------
+#
+#     P A G E B O T
+#
+#     Copyright (c) 2016+ Type Network, www.typenetwork.com, www.pagebot.io
+#     Licensed under MIT conditions
+#     Made for usage in DrawBot, www.drawbot.com
+# -----------------------------------------------------------------------------
+#
+#     installfont.py
+#
+import CoreFoundation
+import CoreText
+
+
+def _makeURL(path):
+    url = CoreFoundation.CFURLCreateWithFileSystemPath(
+        CoreFoundation.kCFAllocatorDefault,
+        path,
+        CoreFoundation.kCFURLPOSIXPathStyle, False)
+    return url
+
+
+_installScopes = dict(
+    none=CoreText.kCTFontManagerScopeNone,
+    process=CoreText.kCTFontManagerScopeProcess,
+    user=CoreText.kCTFontManagerScopeUser,
+    session=CoreText.kCTFontManagerScopeSession,
+)
+
+class FontInstallError(Exception): pass
+
+
+def installFontFile(path, scope="process"):
+    assert scope in _installScopes, ("scope must be one of %s" %
+            sorted(_installScopes.keys()))
+    scope = _installScopes[scope]
+    fontURL = _makeURL(path)
+    res, error = CoreText.CTFontManagerRegisterFontsForURL(
+            fontURL, CoreText.kCTFontManagerScopeProcess, None)
+    if error:
+        raise FontInstallError(error)
+
+
+def uninstallFontFile(path, scope="process"):
+    assert scope in _installScopes, ("scope must be one of %s" %
+            sorted(_installScopes.keys()))
+    scope = _installScopes[scope]
+    fontURL = _makeURL(path)
+    res, error = CoreText.CTFontManagerUnregisterFontsForURL(
+        fontURL, CoreText.kCTFontManagerScopeProcess, None)
+    if error:
+        raise FontInstallError(error)
+
+
+def _test():
+    r"""
+        >>> import AppKit
+        >>> from tnTestFonts import getFontPath
+        >>> path = getFontPath("Condor-Bold.otf")
+        >>> f = AppKit.NSFont.fontWithName_size_("Condor-Bold", 12)
+        >>> f is not None
+        False
+        >>> installFontFile(path)
+        >>> f = AppKit.NSFont.fontWithName_size_("Condor-Bold", 12)
+        >>> f is not None
+        True
+        >>> uninstallFontFile(path)
+        >>> f = AppKit.NSFont.fontWithName_size_("Condor-Bold", 12)
+        >>> #f is not None  # This fails, yet in DrawBot one can see the font no longer works
+        False
+        >>> path = getFontPath("SaunaPro-RegularItalicMerged.ttf")
+        >>> f = AppKit.NSFont.fontWithName_size_(u'-', 12)
+        >>> f is not None
+        False
+        >>> installFontFile(path)
+        >>> # If we *don't* test with NSFont here, we see that the uninstall indeed works
+        >>> #f = AppKit.NSFont.fontWithName_size_(u'-', 12)
+        >>> #f is not None
+        True
+        >>> uninstallFontFile(path)
+        >>> f = AppKit.NSFont.fontWithName_size_(u'-', 12)
+        >>> f is not None
+        False
+    """
+
+
+def _runDocTests():
+    import doctest
+    return doctest.testmod()
+
+
+if __name__ == "__main__":
+    if True:
+        _runDocTests()
+    else:
+        # Test to be run in DrawBot
+        from tnTestFonts import getFontPath
+        path = getFontPath("Condor-Bold.otf")
+        try:
+            installFontFile(path)
+        except FontInstallError, error:
+            print "font probably already installed"
+            #print error
+        font("Condor-Bold")
+        fontSize(200)
+        text("Hallo", (100, 100))
+        # uninstallFontFile(path)
--- Lib/pagebot/fonttoolbox/otlTools.py
+++ Lib/pagebot/fonttoolbox/otlTools.py
@@ -1,3 +1,14 @@
+# -----------------------------------------------------------------------------
+#
+#     P A G E B O T
+#
+#     Copyright (c) 2016+ Type Network, www.typenetwork.com, www.pagebot.io
+#     Licensed under MIT conditions
+#     Made for usage in DrawBot, www.drawbot.com
+# -----------------------------------------------------------------------------
+#
+#   otlTools.py
+#
 """A collection of code to inspect and manipulate OpenType Layout features in a FontTools TTFont."""
 
 #
