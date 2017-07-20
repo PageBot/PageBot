e1bc925a72221c0c619b30cf9601888052a0d7cd
petrvanblokland <buro@petr.com>
2017-07-20 02:15:39 +0200

    Refining the e-delta drawing.
    This is source for new Elements to develop that show the internal of
    TTF, OTF and VF fonts. Then these images can become part of automated
    specimen and article-about-VF-fonts pages.
    
    
    Former-commit-id: 749b6632da15bcc5c83f89c6496f43d4a8983eca [formerly cabb134e740650976bb07a4437c54945beb1b0af]
    Former-commit-id: 8e3e7ab542d8b35780c761564d78f07d82d56bea
    Former-commit-id: f8ee4d63ca169b49a93da02d2efa386140ef674a
    Former-commit-id: f7a5470b7766e9640d7b67af0fcbd7d24080e347

--- Examples/Howto/FindVariableFontDeltas.py
+++ Examples/Howto/FindVariableFontDeltas.py
@@ -38,7 +38,7 @@ for (minValue, defaultValue, maxValue), deltas in axisDeltas.deltas.items():
     print len(deltas), len(g.points4), len(g.points), len(g.coordinates), deltas
     print
 
-if 0:
+if 1:
     # Using PageBot Font wrapper.
     print 'Axis count and names', len(f.axes), f.axes.keys()
     print 'Font.rawDeltas[%s]:' % c, len(f.rawDeltas[c]) 
@@ -58,15 +58,19 @@ drawPath(pen.path)
 fill(None)
 strokeWidth(5)
 R = 30
+COLORS = ((0, 0.2, 1), (1, 0, 1))
 for pIndex, point in enumerate(f[c].points4):
     if point.onCurve:
         stroke(1, 0, 0)
     else:
         stroke(0, 1, 0)
     oval(point.x-R/2, point.y-R/2, R, R)
-    stroke(0.2, 0.2, 0.9)
-    for axisValues, deltas in axisDeltas.deltas.items():
+    color = 0
+    for axisValues, deltas in sorted(axisDeltas.deltas.items()):
         dx, dy = deltas[pIndex]
+        rc, gc, bc = COLORS[color]
+        color += 1
+        stroke(rc, gc, bc)
         line((point.x, point.y), (point.x + dx, point.y + dy))
         
 stroke(0.5)
@@ -84,5 +88,5 @@ if SHOW_DIRECT:
     print 'glyphCount:', f.ttFont['gvar'].glyphCount, len(f)
     print
 
-saveImage('_export/findVariableFontDeltas.py')
+saveImage('_export/findVariableFontDeltas.pdf')
 
