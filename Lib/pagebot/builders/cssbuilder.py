d1fec0e0abe24f0c4c6919a0f8e1cfc53b5a9dbb
petrvanblokland <buro@petr.com>
2017-07-26 21:06:07 +0200

    Add testing on HTML tags for automatic CSS names.
    Add transformer value2Tuple4(v)
    Make automatic class names all lower case, to be used in CSS class
    names.
    Add text box padding for testing.
    
    
    Former-commit-id: 6c9c2f821c19d865c46509225b1701d68c925cc1 [formerly 1a18bd85a756c7fe53863958e627e5d7ac7e261e]
    Former-commit-id: fc33b756406519855c6738efa2427e1d851ab75b
    Former-commit-id: 69ac40e78a0b92b18ae0e4a74df61d2c6b4da70b
    Former-commit-id: c216c3a0cf669d32bb8c00f0f7c97b8c66838485

--- Lib/pagebot/builders/cssbuilder.py
+++ Lib/pagebot/builders/cssbuilder.py
@@ -18,10 +18,28 @@
 #     which will be used by the HtmlBuilder to generate a close representation of the 
 #     document as interactive & responsive website.
 #
+#     TODO: Also make available as SCSS generator.
+#
 import codecs
 import pagebot
 from basebuilder import BaseBuilder
-from pagebot.toolbox.transformer import color2Css
+from pagebot.toolbox.transformer import color2Css, value2Tuple4
+
+HTMLTAGS = set(['h1','h2','h3','h4','h5','h6','p','span','div'])
+
+STYLE2CSS = {
+    'fill': ('background-color: %s;', (1, 1, 1), color2Css),
+    'font': ('font-family: %s;', 'Verdana, Sans', None),
+    'fontSize': ('font-size: %spx;', 12, None),
+    'textFill': ('color: %s;', (0, 0, 0), color2Css),
+    'leading': ('line-height: %spx;', None, None),
+    'rLeading': ('line-height: %sem;', '%0.2f'%1.3, None),
+    'padding': ('padding: %spx %spx %spx %spx;', (0, 0, 0, 0), value2Tuple4),
+    'pl': ('padding-left: %spx;', 0, None),
+    'pt': ('padding-top: %spx;', 0, None),
+    'pb': ('padding-bottom: %spx;', 0, None),
+    'pr': ('padding-right: %spx;', 0, None),
+}
 
 class CssBuilder(BaseBuilder):
 
@@ -41,22 +59,10 @@ class CssBuilder(BaseBuilder):
         self.buildMainStyles(doc, out)
         out.close()
 
-    STYLE2CSS = {
-        'fill': ('background-color: %s;', (1, 1, 1), color2Css),
-        'font': ('font-family: %s;', 'Verdana, Sans', None),
-        'fontSize': ('font-size: %spx;', 12, None),
-        'textFill': ('color: %s;', (0, 0, 0), color2Css),
-        'leading': ('line-height: %spx;', None, None),
-        'rLeading': ('line-height: %sem;', '%0.2f'%1.3, None),
-        'pl': ('padding-left: %spx;', 0, None),
-        'pt': ('padding-top: %spx;', 0, None),
-        'pb': ('padding-bottom: %spx;', 0, None),
-        'pr': ('padding-right: %spx;', 0, None),
-    }
     def _writeStyleValue(self, name, value, out):
-        u"""Write the converted style value as CSS, using self.STYLE2CSS for conversion parameters."""
-        if name in self.STYLE2CSS:
-            cssName, default, f = self.STYLE2CSS[name]
+        u"""Write the converted style value as CSS, using STYLE2CSS for conversion parameters."""
+        if name in STYLE2CSS:
+            cssName, default, f = STYLE2CSS[name]
             cssValue = value or default
             if f is not None:
                 cssValue = f(cssValue)
@@ -65,43 +71,53 @@ class CssBuilder(BaseBuilder):
                 return True # Mark that we found it
         return False
 
+    def _cssId(self, name):
+        if name in HTMLTAGS:
+            return name
+        return 'div.'+name
+
     def _buildStyle(self, doc, out, style):
         u"""Export the style parameters as translated CSS values."""
         # For now write all values as comment as development reference.
         notProcessed = {}
         for parName, value in sorted(style.items()):
+            if parName == 'name':
+                continue
             if not self._writeStyleValue(parName, value, out):
                 notProcessed[parName] = value
 
         # For now write all unprocessed values as comments a development reference.
-        out.write('/*')
-        for parName, value in sorted(notProcessed.items()):
-    		out.write('\t%s: %s;\n' % (parName, value))
-        out.write('*/\n')
+        if notProcessed: # Any style value not processed, then show as comment.
+            out.write('/*')
+            for parName, value in sorted(notProcessed.items()):
+        		out.write('\t%s: %s;\n' % (self._cssId(parName), value))
+            out.write('*/\n')
 
     def buildRootStyle(self, doc, out):
     	u"""Translate the doc.rootStyle to the root body{...} CSS style using doc.rootStyle values."""
     	out.write('body {\n')
         self._buildStyle(doc, out, doc.styles['root'])
-    	out.write('}\n\n')
+    	out.write('} /* body */\n\n')
 
     def buildMainStyles(self, doc, out):
     	u"""Build the styles for text elements, as defined in the doc.styles dictionary."""
     	for styleName, style in sorted(doc.styles.items()):
             if styleName == 'root':
                 continue
-            out.write('%s {\n' % styleName)
+            cssId = self._cssId(styleName)
+            out.write('%s {\n' % cssId)
             self._buildStyle(doc, out, style)
-            out.write('}\n\n')
+            out.write('} /* %s */\n\n' % cssId)
 
     def buildElementStyles(self, doc, out, e=None):
         u"""Recursively build all style values into CSS."""
     	if e is None:
     		self.builfElementStyle(doc, out, self)
     	else:
-            out.write('.%s {\n' % e.name, )
+            cssId = self._cssId(e.name)
+            out.write('%s {\n' % cssId)
             self._buildStyle(doc, out, e.style)
-            out.write('}\n\n')
+            out.write('} /* %s */\n\n' % cssId)
             for child in e.getElements():
                 self.buildElementStyles(doc, out, child)
 
