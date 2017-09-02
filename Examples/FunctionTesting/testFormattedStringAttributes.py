2a29abe5b50fb93edf329cbd3eb7bee48d87f710
petrvanblokland <buro@petr.com>
2017-09-02 21:39:43 +0200

    Trying to find a way to add original style/tag info inside FormattedString runs, so they can convert (back) into HTML

---
+++ Examples/FunctionTesting/testFormattedStringAttributes.py
@@ -0,0 +1,34 @@
+f = FormattedString()
+
+f.fill(1, 0, 0)
+f.fontSize(100)
+f += "hello"
+
+attr = f.getNSObject()
+
+attr.addAttribute_value_range_("com.petr.pageBot.myAttribute", "this is my data", (0, 5)) 
+
+f += " "
+f += "world"
+
+attr = f.getNSObject()
+attr.addAttribute_value_range_("com.petr.pageBot.myOtherAttibute", ["a", "list", "object"], (5, 5)) 
+
+text(f, (96, 172))
+
+
+#print attr
+
+ff = FormattedString('AAAA', font='Verdana', fontSize=100, fill=(0, 0.5, 0))
+text(ff, (96, 440))
+attr = ff.getNSObject()
+attr.addAttribute_value_range_("io.pageBot.class", 'CLASS_NAME', (0, len(ff))) 
+attr.addAttribute_value_range_("io.pageBot.tag", 'TAG_NAME', (0, len(ff))) 
+
+#print attr
+
+fff = ff + f
+attr = fff.getNSObject()
+
+print attr
+
