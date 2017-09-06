bd63057955c77d2d83da6e09230ef1adb394bd0d
petrvanblokland <buro@petr.com>
2017-09-06 17:07:57 -0400

    Automatic more elements in Website class. First example will run as designdesign.space site.
    Make MampView work for local publishing
    
    
    Former-commit-id: 3e627cc77d7eaf928ecaea6b51905cd8fdd730d3 [formerly d6d73c45f9c39f2e1613561d70cb08a697037a33]
    Former-commit-id: e5c4fa44c1631e0983df52c800d7387bf8eebb81
    Former-commit-id: 9e63af6c57186fbe4b0478880de7a4fc597fbef4
    Former-commit-id: 35a16ce1e1439ba01ccc98853edea629b47113ec

--- Lib/pagebot/elements/views/mampview.py
+++ Lib/pagebot/elements/views/mampview.py
@@ -11,40 +11,52 @@
 #
 #     mampview.py
 #
+import os
+import shutil
 from pagebot.elements.views.view import View
 from pagebot.builders import WebBuilder
-import os
 
 class MampView(View):
     viewId = 'Mamp'
 
     # self.build exports in MAMP folder that does not commit in Git. 
-    EXPORT_PATH = '/Applications/MAMP/htdocs/'
-    MAMP_PAGEBOT_PATH = EXPORT_PATH + 'pagebot/'
-    DEFAULT_HTML_FILE = 'index.html'
-    DEFAULT_CSS_FILE = 'pagebot.css'
+    MAMP_PATH = '/Applications/MAMP/htdocs/'
     # If the MAMP server application not installed, a browser is opened on the MAMP website to download it.
     # There is a free demo version can be installed.
     MAMP_SHOP_URL = 'https://www.mamp.info/en/' 
 
+    SITE_PATH = 'docs/'
+    DEFAULT_HTML_FILE = 'index.html'
+    DEFAULT_HTML_PATH = SITE_PATH + DEFAULT_HTML_FILE
+    DEFAULT_CSS_PATH = SITE_PATH + 'css/pagebot.css'
+
     #   B U I L D  H T M L  /  C S S
 
     def build(self, name, pageSelection=None, multiPage=True):
-        path = self.MAMP_PAGEBOT_PATH + name + '/' 
         doc = self.parent
-        b = WebBuilder()
-        print path
-        doc[0].build(self, b)
-        if path.endswith('.html'):
-            b.writeHtml(path) 
-            b.writeCss(os.path.join(os.path.dirname(path), self.DEFAULT_CSS_FILE))
-        else:
-            if path[-1] != "/":
-                path+="/"
-            b.writeHtml(path + self.DEFAULT_HTML_FILE)
-            b.writeCss(path + self.DEFAULT_CSS_FILE)
+        for pn, pages in doc.pages.items():
+            for page in pages:
+                fileName = page.name
+                if not fileName:
+                    fileName = DEFAULT_HTML_FILE
+                if not fileName.lower().endswith('.html'):
+                    fileName += '.html'
+                path = self.SITE_PATH + fileName
+                print pn, page.name, path
+                b = WebBuilder()
+                page.build(self, b)
+                b.writeHtml(path)
+                if pn == 0:
+                    # TODO: Don't need to write the css for every page.
+                    b.writeCss(self.DEFAULT_CSS_PATH)
+
+        mampPath = self.MAMP_PATH + name
+        if os.path.exists(mampPath):
+            shutil.rmtree(mampPath)
+        shutil.copytree(self.SITE_PATH, mampPath)
 
     def getUrl(self, name):
-        return 'http://localhost:8888/pagebot/%s/%s' % (name, self.DEFAULT_HTML_FILE)
+        return 'http://localhost:8888/%s/%s' % (name, self.DEFAULT_HTML_FILE)
+
 
 
