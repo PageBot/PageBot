c0f75f6461d5d35b6105b76784520886fc10700b
petrvanblokland <buro@petr.com>
2017-09-20 18:41:57 -0400

    Working on examples
    Former-commit-id: 21e5f16c04aa2f9fd72a4f9135c9b7a4bb4ea7ce [formerly 7f44f4f09a3a9eb7d366b1352bb42025cd11aaac]
    Former-commit-id: b72cd3c3ccaa5b28d7b2e36145fe4d9a7d36ffa6
    Former-commit-id: 9796a1945417d44eeb331438613176fa8f9c0873
    Former-commit-id: ce8954b96a164f6e1754f42f6fc537d333b9025f

--- Examples/FunctionTesting/testBabelStringsByView.py
+++ Examples/FunctionTesting/testBabelStringsByView.py
@@ -12,22 +12,30 @@
 #
 #     testBabelStrings.py
 #
+import pagebot
 from pagebot.builders.drawbotbuilder import drawBotBuilder as b
-from pagebot.elements.views.strings import newFsString
 if b is None:
-    print 'Platform does not support DrawBot.'
-else:
-    from pagebot.elements.views import DrawBotView
-    view = DrawBotView()
-    # Create a new BabelString, FS-flavor with the DrawBot FormttedString inside.
-    style=dict(font='Verdana', fontSize=50, textFill=(1, 0, 0))
-    bs = view.newString('This is an FsString', style=style)
-    # It prints it content.
-    print bs
-    # Adding or appending other BabelStrings works too,
-    # by adding to the embedded OSX Formatted string.
-    style['textFill'] = 1, 0, 1 # Change style a bit to see diff.
-    bs += view.newString(' and more', style=style)
-    print bs
-    # Usage in DrawBot by addressing the embedded FS for drawing.
-    b.text(bs.s, (100, 100))
+	print 'Example only runs in DrawBot'
+	raise KeyboardInterrupt()
+
+from pagebot.elements.views.strings import newFsString
+from pagebot.elements.views import DrawBotView
+
+view = DrawBotView()
+style=dict(font='Verdana', fontSize=50, textFill=(1, 0, 0))
+# Create a new BabelString, FS-flavor with the DrawBot FormattedString inside.
+# Getting it through the view, automatically makes set the class to FsString
+bs = view.newString('This is an FsString', style=style)
+# It prints it content (same as bs.s)
+print bs
+# Adding or appending other BabelStrings works too,
+# by adding to the embedded OSX Formatted string.
+style['textFill'] = 1, 0, 1 # Change style a bit to see diff.
+bs += view.newString(' and more', style=style)
+print bs
+# Usage in DrawBot by addressing the embedded FS for drawing.
+b.text(bs.s, (100, 100))
+# Other string operations work
+bs = view.newString('.', style=style)
+
+    
