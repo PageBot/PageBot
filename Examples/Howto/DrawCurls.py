c44a2c4536988f851d4409a5adbd281da4db3da6
petrvanblokland <buro@petr.com>
2017-09-27 12:47:23 -0400

    Debugging curls
    Former-commit-id: d4a5b1bc697662bc75a307b2b5ca7e6827b6d7e8 [formerly 9abb8af09a43401231cfcbbbf30e8e2ec3446441]
    Former-commit-id: f5d0787979ceb7214cb87b3daf1644c4cb2f7c4c
    Former-commit-id: fe84b07703b1d7a283cc08d18a1389cb20a245c4
    Former-commit-id: 0445adba2a35c378047467b0093194aedfa9c599

--- Examples/Howto/DrawCurls.py
+++ Examples/Howto/DrawCurls.py
@@ -15,9 +15,9 @@
 from __future__ import division # Make integer division result in float.
 import pagebot # Import to know the path of non-Python resources.
 W = H = 1000
-X = 300
-y = 300
-N = 64
+X = 0
+Y = 0
+N = 12*8
 Sx = 10
 Sy = 10
 Ex = 10
@@ -32,7 +32,7 @@ def drawSpiral():
     my = H/2
     runs = False
     path = BezierPath()
-    for n in range(0, N, 4):
+    for n in range(0, int(N), 4):
         dx1 = n*Sx*D
         dy1 = n*Sy*D
         dx2 = (n+1)*Sx*D
@@ -44,12 +44,12 @@ def drawSpiral():
         dx5 = (n+4)*Sx*D
         dy5 = (n+4)*Sy*D
         if not runs:
-            path.moveTo((mx, my-dy1))
+            path.moveTo((mx, my))
         else:
-            path.curveTo((mx-dx1*Ex, my-dy1), (mx-dx1, my-dy1), (mx-dx1, my))
-            path.curveTo((mx-dx2, my+dy2*Ey), (mx-dx2*Ex, my+dy3), (mx, my+dy3))
-            path.curveTo((mx+dx4*Ex, my+dy3), (mx+dx4, my+dy3*Ey), (mx+dx4, my))
-            path.curveTo((mx+dx4, my-dy5*Ey), (mx+dx4*Ex, my-dy5), (mx, my-dy5))
+            path.curveTo((mx-dx1*Exy, my-dy1), (mx-dx1, my-dy1*Exy), (mx-dx1, my))
+            path.curveTo((mx-dx2, my+dy2*Exy), (mx-dx2*Exy, my+dy2), (mx, my+dy2))
+            path.curveTo((mx+dx3*Exy, my+dy3), (mx+dx3, my+dy3*Exy), (mx+dx3, my))
+            path.curveTo((mx+dx4, my-dy4*Exy), (mx+dx4*Exy, my-dy4), (mx, my-dy4))
         runs = True
     # close the path
     fill(None)
@@ -62,10 +62,10 @@ Variable([
     #dict(name='ElementOrigin', ui='CheckBox', args=dict(value=False)),
     dict(name='X', ui='Slider', args=dict(minValue=100, value=300, maxValue=1000)),
     dict(name='Y', ui='Slider', args=dict(minValue=100, value=300, maxValue=1000)),
+    dict(name='N', ui='Slider', args=dict(minValue=8*2, value=8*8, maxValue=8*32)),
     dict(name='Sx', ui='Slider', args=dict(minValue=2, value=10, maxValue=40)),
     dict(name='Sy', ui='Slider', args=dict(minValue=2, value=10, maxValue=40)),
-    dict(name='Ex', ui='Slider', args=dict(minValue=0.01, value=0.5, maxValue=1)),
-    dict(name='Ey', ui='Slider', args=dict(minValue=0.01, value=0.5, maxValue=1)),
+    dict(name='Exy', ui='Slider', args=dict(minValue=0.01, value=0.58, maxValue=1)),
     dict(name='D', ui='Slider', args=dict(minValue=0.1, value=0.5, maxValue=5)),
 ], globals())
        
