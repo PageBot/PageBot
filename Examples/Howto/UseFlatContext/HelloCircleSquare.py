020e86cc91a603468921beec617e7331b51634f7
petrvanblokland <buro@petr.com>
2017-12-06 09:09:07 +0100

    Make HalloCircleSquare.py work as first "flat" context example.
    Add saveImage( ) identical to saveDocument( ) for DrawBot compatibility.
    Add global iround(value) function to calculate color (0,1) into color
    (0, 255) with constraints. Color API is kept as floats on the outside
    of the contexts.
    
    
    Former-commit-id: 6d848536050c65cb95921f4d7c1f4ff9b51f6f82 [formerly 1d75989d4e50bac76a0d3966bd3e7c15b9813d6f]
    Former-commit-id: 2e0da155bbc20b31db3f77e7e70f3122a8efa4e9
    Former-commit-id: 76f6f61f98467e25d9aba9bc8057a4bd489834f9
    Former-commit-id: ec9e7e4b0866fe04a60a365c1f4c8cb7cf096a5d

--- Examples/Howto/UseFlatContext/HelloCircleSquare.py
+++ Examples/Howto/UseFlatContext/HelloCircleSquare.py
@@ -1,23 +1,51 @@
-from pagebot.contexts import defaultContext as c
-from pagebot.document import Document
+# -----------------------------------------------------------------------------
+#     Copyright (c) 2016+ Buro Petr van Blokland + Claudia Mens & Font Bureau
+#     www.pagebot.io
+#
+#     P A G E B O T
+#
+#     Licensed under MIT conditions
+#     
+#     Supporting usage of DrawBot, www.drawbot.com
+#     Supporting usage of Flat, https://github.com/xxyxyz/flat
+# -----------------------------------------------------------------------------
+#
+#     HelloCircleSquare.py
+#
+#     Example to run in plain Python.
+#     Dependencies: pabebot, flat, fonttools.
+#
+#     http://xxyxyz.org/flat#tutorial
+#
+from random import random
+from pagebot.contexts import defaultContext as context
 
-doc = Document()
+W = H = 500
+PAGES = 3
+RECTS = 150
+R = 20 # Diameter of circle or square
+M = 20 # Page margin
 
-for p in range(10):
-    c.newPage(1000, 1000)
-    for n in range(50):
-        c.fill((random(), 0, random(), 0.5 +random()*0.2))
+context.newDocument(w=W, h=H)
+for p in range(PAGES):
+    context.newPage(W, H)
+    for n in range(RECTS):
+        # Compatible to DrawBot: color values between (0, 1)
+        a = 0.5 +random()*0.5
+        red, green, blue = random(), random(), random()
+        context.fill((red, green, blue, a))
         ch = random()
-        x = 20 + random()*800
-        y = 20 + random()*800
+        x = M + random()*(W-2*M-R) # Only in available space
+        y = M + random()*(H-2*M-R)
         if ch < 0.2:
-            c.oval(x, y, 80, 80 )
+            context.oval(x, y, R, R )
         elif ch < 0.4:
-            c.rect(x, y, 80, 80 )
+            context.rect(x, y, R, R )
         else:
-            c.fontSize(24)
-            c.text('Hello world on %d,%d' % (x, y), (x, y))
-
+            #c.fontSize(24)
+            #c.text('Hello world on %d,%d' % (x, y), (x, y))
+            pass
 
 
 #c.saveImage('_export/HelloCircleSquare.gif')
+context.saveDocument('_export/HelloCircleSquare.png')
