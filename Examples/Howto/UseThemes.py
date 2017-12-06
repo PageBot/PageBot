64d4dd2e770912d24075ab18212a80e7620ea375
petrvanblokland <buro@petr.com>
2017-12-06 10:04:16 +0100

    Cleaning some examples.

--- Examples/Howto/UseThemes.py
+++ Examples/Howto/UseThemes.py
@@ -1,3 +1,17 @@
+# -----------------------------------------------------------------------------
+#     Copyright (c) 2016+ Buro Petr van Blokland + Claudia Mens & Font Bureau
+#     www.pagebot.io
+#
+#     P A G E B O T
+#
+#     Licensed under MIT conditions
+#     
+#     Supporting usage of DrawBot, www.drawbot.com
+#     Supporting usage of Flat, https://github.com/xxyxyz/flat
+# -----------------------------------------------------------------------------
+#
+#     UseThemes.py
+#
 from pagebot.themes import Themes
 
 theme = Themes['Metallic Office']
