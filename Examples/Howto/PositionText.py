edb017607d938ad65907f7b3d13fb3f2d7c43db1
petrvanblokland <buro@petr.com>
2017-12-07 17:41:03 +0100

    Debugging FlatContext. Working on PositionText.py example

--- Examples/Howto/PositionText.py
+++ Examples/Howto/PositionText.py
@@ -14,19 +14,13 @@
 #
 #     Hard position a text box on an (x, y) position.
 #
-USE_FLAT = True
-
 import os
 import pagebot # Import to know the path of non-Python resources.
-from pagebot.contexts import defaultContext, FlatContext
-if USE_FLAT:
-    EXPORT_PATH = '_export/PositionText_Flat.pdf'
-    defaultContext = FlatContext()
-else:
-    EXPORT_PATH = '_export/PositionText.pdf'
 
-# Creation of the RootStyle (dictionary) with all available default style parameters filled.
-from pagebot.style import getRootStyle, B4, CENTER, LEFT, TOP, BOTTOM, RIGHT
+from pagebot.contexts import defaultContext as context
+
+EXPORT_PATH = '_export/PositionText.pdf'
+
 # Document is the main instance holding all information about the document togethers (pages, styles, etc.)
 from pagebot.document import Document
 from pagebot.elements import *
@@ -38,7 +32,7 @@ def makeDocument():
     Make number of pages with default document size.
     Initially make all pages default with template."""
     
-    doc = Document(originTop=False, w=W, h=H, autoPages=1, context=defaultContext) 
+    doc = Document(originTop=False, w=W, h=H, autoPages=1, context=context) 
 
     page = doc[0] # Get the first/single page of the document.
     page.size = W, H
@@ -50,8 +44,7 @@ def makeDocument():
     view.showPageCropMarks = True
     view.showElementOrigin = True # Show the alignment position of elements as plus-mark.
     
-    title = 'Book Cover' # Using plain string, style values from from text box element.
-    fontSize = 40
+    title = context.newString('Book Cover', style=dict(fontSize = 40, textFill=(0, 0, 1)))
     newTextBox(title, # Text inside the text box
         name='Other element', # Optinal (unique) name of element. Otherwise e.eId is used.
         x=100, y=100, # Position from left-bottom of page side (not page padding)
@@ -59,12 +52,6 @@ def makeDocument():
         w=400, h=200, # Size of the element. Since x = 100 and W = 500, this "manually" fits.
         padding=5, # Padding inside text box for all 4 sides equal.
         fill=(0.4, 0.6, 1), stroke=(1, 0, 0), strokeWidth=10, # Rectangle fill, stroke colors 
-        # Below style values that apply to the content
-        font='Verdana', 
-        fontSize=fontSize, 
-        leading=0, rLeading=1, # Absolute and relative leading.
-        xAlign=LEFT, yAlign=BOTTOM, # Set origin of element to left-bottom
-        textFill=(0, 0, 1) # Color of the text.
     )
     # Return the generated document to the caller.
     return doc
@@ -72,8 +59,7 @@ def makeDocument():
 doc = makeDocument()
 doc.export(EXPORT_PATH)
 
-if USE_FLAT:
-    os.system(u'open "%s"' % EXPORT_PATH)
+os.system(u'open "%s"' % EXPORT_PATH)
     
 print 'Done'
         
