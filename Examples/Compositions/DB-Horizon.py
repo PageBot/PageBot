33bd28d3e8f0681dd9b1a6cabd253f542e129a53
petrvanblokland <buro@petr.com>
2017-12-09 10:39:04 +0100

    Rename and context changes

--- Examples/Compositions/DB-Horizon.py
+++ Examples/Compositions/DB-Horizon.py
@@ -1,62 +1,69 @@
+# -----------------------------------------------------------------------------
+#     Copyright (c) 2016+ Buro Petr van Blokland + Claudia Mens & Font Bureau
+#     www.pagebot.io
+#
+#     P A G E B O T
+#
+#     Licensed under MIT conditions
+#     
+#     Supporting usage of DrawBot, www.drawbot.com
+#     Supporting usage of Flat, https://github.com/xxyxyz/flat
+# -----------------------------------------------------------------------------
+#
+#     Horizon.py
+#
 from random import choice
-from pagebot.builders import drawBotBuilder as b
-if b is None:
-    print ('Example runs only in DrawBot')
+from pagebot.contexts import defaultContect as context
+
+LETTERS = 'ABCEFGHIJKLMNOPQTSTIVWXUZ'
+
+Frames = 100
+
+W = H = 500
+IMAGE_PATH = '_export/HorizonWorld.gif'
+
+if not 'BitcountMonoDouble-RegularCircle' in b.installedFonts():
+    fontNames = ['Georgia-Bold', 'Georgia']
 else:
-    LETTERS = 'ABCEFGHIJKLMNOPQTSTIVWXUZ'
+    fontNames = []
+    for fontName in installedFonts():
+        if 'BitcountMono' in fontName and not 'Italic' in fontName:
+            fontNames.append(fontName)
+            
+letters = []
+for n in range(10):
+    c = choice(LETTERS)
+    x = 0    
+    y = 15    
+    z = 20+int(random()*500)
 
-    Frames = 100
+    x = 1/z + random()*100-100
+    cc = random()*0.8+0.1, random()*0.1, random()*0.8*0.1
+    f = choice(fontNames)
+    letters.append((c, f, x, y, z, cc))
+    
+for n in range(Frames):
+    context.newPage(W, H)
+    context.fill(0.8)
+    context.rect(0, 0, W, H)
+    for c, f, x, y, z, (r, g, b) in letters:
+        #y = y/z
+        bs = context.newString(c, style=dict(font=f, fontSize=fSize, textFill=(r, g, b)))
+        context.text(bs, (x+250, y+250-fSize/2))
 
-    W = H = 500
-    IMAGE_PATH = '_export/HorizonWorld.gif'
-    if not 'BitcountMonoDouble-RegularCircle' in b.installedFonts():
-        fontNames = ['Georgia-Bold', 'Georgia']
-    else:
-        fontNames = []
-        for fontName in installedFonts():
-            if 'BitcountMono' in fontName and not 'Italic' in fontName:
-                fontNames.append(fontName)
-                
-    letters = []
-    for n in range(10):
-        c = choice(LETTERS)
-        x = 0    
-        y = 15    
-        z = 20+int(random()*500)
-
-        x = 1/z + random()*100-100
-        cc = random()*0.8+0.1, random()*0.1, random()*0.8*0.1
-        f = choice(fontNames)
-        letters.append((c, f, x, y, z, cc))
-        
-    for n in range(Frames):
-        b.newPage(W, H)
-        b.fill(0.8)
-        b.rect(0, 0, W, H)
-        for c, f, x, y, z, (r, g, b) in letters:
-            #y = y/z
-            b.fill(r, g, b)
-            b.font(f)
-            b.stroke(None)
-            fSize = min(200, 40000/z)
-            b.fontSize(fSize)
-            b.text(c, (x+250, y+250-fSize/2))
-
-            b.fill(None)
-            b.strokeWidth(0.5)
-            b.stroke(0.5)
-            b.line((0, 250), (500, 250))
-        
-            b.fill(1, 1, 1, 0.4)
-            b.rect(0, 0, W, H/2-1)
-        
-            for n in range(0, 500, 10):
-                b.fill(None)
-                b.stroke(1)
-                y = W/2-2 - n*0.4
-                lineThickness = (random()*3+0.5) * (H/2 - y)/10
-                b.strokeWidth(lineThickness)
-                b.line((0, y-lineThickness/2), (W, y-lineThickness/2))
-        
-    b.saveImage(IMAGE_PATH)
+        context.fill(None)
+        context.stroke(0.5, 0.5)
+        context.line((0, 250), (500, 250))
+    
+        context.fill(1, 1, 1, 0.4)
+        context.rect(0, 0, W, H/2-1)
+    
+        for n in range(0, 500, 10):
+            context.fill(None)
+            y = W/2-2 - n*0.4
+            lineThickness = (random()*3+0.5) * (H/2 - y)/10
+            context.stroke(1, lineThickness)
+            stroke.line((0, y-lineThickness/2), (W, y-lineThickness/2))
+    
+context.saveImage(IMAGE_PATH)
 
