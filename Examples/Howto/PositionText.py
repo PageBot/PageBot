232fc1aa42e500519a720ff51489c085cfe494a5
petrvanblokland <buro@petr.com>
2017-12-25 16:04:40 +0100

    PositionText
    Former-commit-id: 955fa1f0b711e9b7315bcfe50fabf3010b5953d6 [formerly f6e2bd7322cf398352e958254e5af1e18b536bd0]
    Former-commit-id: 6ffe857862f328903d99aa57055ac544b5037e35
    Former-commit-id: 5b323e0b1a765703c5e3971b2845dabc19ab796a
    Former-commit-id: 18599515615e0654134d81d7c8c3552afcf68c07

--- Examples/Howto/PositionText.py
+++ Examples/Howto/PositionText.py
@@ -16,23 +16,35 @@
 #     Hard position a text box on an (x, y) position.
 #
 import os
-from pagebot.elements import newTextBox
+from random import choice
+from pagebot.elements import newText
 from pagebot.contexts import defaultContext
 from pagebot.style import LEFT, BOTTOM
 # Document is the main instance holding all information
 # about the document togethers (pages, styles, etc.)
 from pagebot.document import Document
+from pagebot.toolbox.hyphenation import words
 
 USE_FLAT = True
-EXPORT_PATH = '_export/'
+EXPORT_PATH = '_export/PositionText.png'
 W, H = 500, 500
+C = 40 # Column
+G = 10
+M = 20 # Margin
 
 def makeDocument():
     u"""Create new document with (w,h) and fixed amount of pages.
     Make number of pages with default document size.
     Initially make all pages default with template."""
 
-    doc = Document(originTop=False,
+    smallWords = []
+    for word in words(): # Get all English hyphenating words.
+      if len(word) in (4,5,6):
+        smallWords.append(word)
+
+    title = 'Position Text Box Example' # Using plain string, style values
+
+    doc = Document(originTop=False, title=title,
                    w=W, h=H,
                    autoPages=1,
                    context=defaultContext)
@@ -47,28 +59,25 @@ def makeDocument():
     view.showPageCropMarks = True
     view.showElementOrigin = True # Show the alignment position
                                   # of elements as plus-mark.
-
-    title = 'Book Cover' # Using plain string, style values
-                         # from text box element.
-    fontSize = 40
-    newTextBox(title, # Text inside the text box
-               name='Other element', # Optinal (unique) name of element.
-                                     # Otherwise e.eId is used.
-               x=100, y=100, # Position from left-bottom of page side
-                             # (not page padding)
-               parent=page,
-               w=400, h=200, # Size of the element.
-                             # Since x = 100 and W = 500, this "manually" fits.
-               padding=5, # Padding inside text box for all 4 sides equal.
-               fill=(0.4, 0.6, 1), # Rectangle fill color
-               stroke=(1, 0, 0), # Rectangle stroke color
-               strokeWidth=10,
-               # Below style values that apply to the content
-               font='Verdana',
-               fontSize=fontSize,
-               leading=0, rLeading=1, # Absolute and relative leading.
-               xAlign=LEFT, yAlign=BOTTOM, # Set origin of element to left-bottom
-               textFill=(0, 0, 1)) # Color of the text.
+    fontSize = 18
+    cellHeight = int(fontSize*4)
+    for cx in range(0, W-2*M, C):
+      for cy in range(0, H-2*M, cellHeight):
+        newText(choice(smallWords), # Random small word
+                   x=cx, y=cy, # Position from left-bottom of page side
+                                 # (not page padding)
+                   parent=page,
+                   w=C-G, h=cellHeight, # Size of the element.
+                   padding=5, # Padding inside text box for all 4 sides equal.
+                   fill=(0.4, 0.6, 1), # Rectangle fill color
+                   stroke=(1, 0, 0), # Rectangle stroke color
+                   strokeWidth=10,
+                   # Below style values that apply to the content
+                   font='Verdana',
+                   fontSize=fontSize,
+                   leading=0, rLeading=1, # Absolute and relative leading.
+                   xAlign=LEFT, yAlign=BOTTOM, # Set origin of element to left-bottom
+                   textFill=(0, 0, 1)) # Color of the text.
     # Return the generated document to the caller.
     return doc
 
