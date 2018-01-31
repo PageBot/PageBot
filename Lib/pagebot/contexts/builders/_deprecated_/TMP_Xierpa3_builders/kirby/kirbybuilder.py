a33f919d747eeffea9eb986a9671c3b5146c15db
Felipe Corrêa da Silva Sanches <juca@members.fsf.org>
2018-01-31 02:59:47 -0200

    fix licensing (issue #79)

--- Lib/pagebot/contexts/builders/_deprecated_/TMP_Xierpa3_builders/kirby/kirbybuilder.py
+++ Lib/pagebot/contexts/builders/_deprecated_/TMP_Xierpa3_builders/kirby/kirbybuilder.py
@@ -1,14 +1,15 @@
 # -*- coding: UTF-8 -*-
 # -----------------------------------------------------------------------------
-# 	xierpa server
-# 	(c) 2014+  buro@petr.com, www.petr.com, www.xierpa.com
 #
-# 	X I E R P A  3
-# 	No distribution without permission.
+#     P A G E B O T
 #
+#     Copyright (c) 2016+ Buro Petr van Blokland + Claudia Mens & Font Bureau
+#     www.pagebot.io
+#     Licensed under MIT conditions
+#
+#     Supporting usage of DrawBot, www.drawbot.com
+#     Supporting usage of Flat, https://github.com/xxyxyz/flat
 # -----------------------------------------------------------------------------
-#
-#   phpbuilder.py
 #
 from xierpa3.builders.cssbuilder import CssBuilder
 from xierpa3.builders.htmlbuilder import HtmlBuilder
