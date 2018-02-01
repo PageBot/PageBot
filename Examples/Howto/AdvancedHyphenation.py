b81f5a5fc549faa8cbedd7a095909b3bce4177f1
Felipe Corrêa da Silva Sanches <juca@members.fsf.org>
2018-02-01 17:54:32 -0200

    refactor Thom's contribs updating it to current codebase

--- Examples/Howto/AdvancedHyphenation.py
+++ Examples/Howto/AdvancedHyphenation.py
@@ -1,17 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# -----------------------------------------------------------------------------
+#
+#     P A G E B O T
+#
+#     Copyright (c) 2017 Thom Janssen <https://github.com/thomgb>
+#     www.pagebot.io
+#     Licensed under MIT conditions
+#
+#     Supporting usage of DrawBot, www.drawbot.com
+#     Supporting usage of Flat, https://github.com/xxyxyz/flat
+# -----------------------------------------------------------------------------
+#
 ## This works with the current (3.97) version of DrawBot
+from pagebot.contexts import defaultContext as c
 """
 Hyphenation with head and tail
-TODO: 
+TODO:
     - show hyphen if hyphenated...
     > make it work in drawBot, extra aguments in FormattedString (headHyphenation=4, tailHyphenation=3)
     >> then drawBot.context.baseContext.BaseContext.hyphenateAttributedString will take care if it (eventually)
-
 """
 t = "programmatic"
-
-fs = FormattedString(fontSize=70, language='en',)
-fs.append(t)
-
+fs = c.newString(t, style=dict(fontSize=70, language='en'))
 softHyphen = unichr(0x00AD)
 
 # set head and tail
@@ -38,16 +49,18 @@ n=0
 for p in hyphenPositions:
     a=a[:p+n] + "-" + a[p+n:] # hyphen, show hyphenation
     b=b[:p+n] + softHyphen + b[p+n:] # softHyphen here can the word hyphenate 
-    
-    n+=1 # hyphenationPositions are 0+n, where n = number of hyphenation positions that are already defined, because we add (soft)hyphens...
+
+    n+=1 # hyphenationPositions are 0+n, where n = number of hyphenation positions
+         # that are already defined, because we add (soft)hyphens...
+
+c.newPage(1000, 2000)
 
 # the hard hyphens:
-textBox(a,(20,500,900,500)) 
+c.textBox(a,(20,500,900,500))
 
-# the soft hyphens:    
+# the soft hyphens:
 w = 472 # change width to see hyphenations
-textBox(b,(20,50,w,500))  
-fill(None)
-stroke(0)
-rect(20,50,w,500)
-
+c.textBox(b, (20,50,w,500))
+c.fill(None)
+c.stroke(0)
+c.rect(20,50,w,500)
--- Examples/Howto/AdvancedHyphenation_futureDrawBot.py
+++ Examples/Howto/AdvancedHyphenation_futureDrawBot.py
@@ -1,12 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# -----------------------------------------------------------------------------
+#
+#     P A G E B O T
+#
+#     Copyright (c) 2017 Thom Janssen <https://github.com/thomgb>
+#     www.pagebot.io
+#     Licensed under MIT conditions
+#
+#     Supporting usage of DrawBot, www.drawbot.com
+#     Supporting usage of Flat, https://github.com/xxyxyz/flat
+# -----------------------------------------------------------------------------
+#
 ## Works with this version of DrawBot:
 ## https://github.com/thomgb/drawbot
 ## download my DrawBot: https://www.dropbox.com/s/xsu1mz89ipo5x3y/DrawBot.dmg?dl=0
+from pagebot.contexts import defaultContext as c
 
-t = FormattedString("programmatic", fontSize=30, hyphenationHead=4, hyphenationTail=3)
-hyphenation(True)
+t = c.newString("programmatic",
+                style=dict(fontSize=30,
+                           hyphenationHead=4,
+                           hyphenationTail=3))
+c.hyphenation(True)
 
 w=200 # change width to see other hyphenations
 
-textBox(t, (100,100,w,600)) fill(None)
-stroke(0)
-rect(100,100,w,600)
+c.newPage(1000, 2000)
+c.textBox(t, (100,100,w,600))
+c.fill(None)
+c.stroke(0)
+c.rect(100,100,w,600)
