323b6d18bd464c537a05142f97ae3a9ee5f4c95d
petrvanblokland <buro@petr.com>
2018-02-01 17:17:20 +0100

    More examples to work.
    Make Hyphenation demo work as docTest, so we can check if library
    changed.

--- Examples/Howto/UseText/Hyphenation.py
+++ Examples/Howto/UseText/Hyphenation.py
@@ -14,30 +14,51 @@
 #
 from pagebot.toolbox.hyphenation import hyphenate, hyphenatedWords, words
 
-print 'English hyphenated words:', len(hyphenatedWords('en'))
-print 'Dutch hyphenated words:', len(hyphenatedWords('nl'))
-print
-# Single English words
-print hyphenate('housing', 'en')
-print hyphenate('Tutankhamun') # English is default
-print hyphenate('Tutankhamun', 'en')
-# Single Dutch words
-print hyphenate('marmerplaatjes', 'nl')
-# Hyphenates as plaat-staal (sheet of steel) where plaats-taal (regional language) also would have been valid
-print hyphenate('plaatstaal', 'nl') 
+def hyphenDemo(s, language='en'):
+    u"""Show demo of hyphenation.
 
-# Combined dutch words, hyphenating between valid words.
-print hyphenate('marmer', 'nl', True)
-print hyphenate('plaats', 'nl', True)
-print hyphenate('marmerplaats', 'nl', True)
-print hyphenate('marmerplaatsbepaling', 'nl', True)
-print hyphenate('ochtendjaskledinghangerschroefdraad', 'nl', True)
-print hyphenate('hagelslagroomboterbloemkoolstofzuigerveerpont', 'nl', True)
-# Longestin practice. Don't go longer than this, as calculation time exponentioally increases.
-print hyphenate('kernenergieadviesbureaugebouwtoegangsdeurknopbedieningspaneeltjes', 'nl', True)
+    >>> len(hyphenatedWords('en')) # English hyphenated words in the library
+    171942
+    >>> len(hyphenatedWords('nl')) # Dutch hyphenated words in the library
+    235916
+    >>> hyphenate('housing', 'en')
+    u'hous-ing'
+    >>> # Single English words
+    >>> hyphenate('Tutankhamun'), hyphenate('Tutankhamun', 'en') # English is default
+    (u'Tut-ankh-a-mun', u'Tut-ankh-a-mun')
+    >>> # Combined Dutch words
+    >>> hyphenate('marmerplaatjes', 'nl')
+    u'mar-mer-plaat-jes'
+    >>> # Hyphenates as plaat-staal (sheet of steel) where plaats-taal (regional language) also would have been valid
+    >>> hyphenate('plaatstaal', 'nl') 
+    u'plaat-staal'
+    >>> # Combined dutch words, hyphenating between valid words.
+    >>> hyphenate('marmer', 'nl', True)
+    u'mar-mer'
+    >>> hyphenate('plaats', 'nl', True)
+    u'plaats'
+    >>> hyphenate('marmerplaats', 'nl', True) # Find combination, although the word itself is not part of the library.
+    u'mar-mer-plaats'
+    >>> hyphenate('marmerplaatsbepaling', 'nl', True)
+    u'mar-mer-plaats-be-pa-ling'
+    >>> hyphenate('ochtendjaskledinghangerschroefdraad', 'nl', True)
+    u'och-tend-jas-kle-ding-han-ger-schroef-draad'
+    >>> hyphenate('hagelslagroomboterbloemkoolstofzuigerveerpont', 'nl', True)
+    u'ha-gel-slag-room-bo-ter-bloem-kool-stof-zui-ger-veer-pont'
+    >>> # Longest in practice. Don't go longer than this, as calculation time exponentioally increases.
+    >>> hyphenate('kernenergieadviesbureaugebouwtoegangsdeurknopbedieningspaneeltjes', 'nl', True)
+    u'kern-ener-gie-ad-vies-bu-reau-ge-bouw-toe-gangs-deur-knop-be-die-nings-pa-neel-tjes'
+    >>> hyphenate('housewarmingpartyinvitation', 'nl', True) is None # --> None: no matching in another language.
+    True
+    >>> hyphenate('housewarmingpartyinvitation', 'en', True) # --> Still works: house-warm-ing-par-ty-in-vi-ta-tion
+    u'house-warm-ing-par-ty-in-vi-ta-tion'
+    >>> # First [100:105] words of the sorted list of all language words in the dictionary.
+    >>> words('nl')[100:105] 
+    [u'aanaardt', u'aanademing', u'aanbad', u'aanbak', u'aanbakken']
+    """
+    return hyphenate(s)
 
-print hyphenate('housewarmingpartyinvitation', 'nl', True) # --> None: no matching in another language.
-print hyphenate('housewarmingpartyinvitation', 'en', True) # --> Still works: house-warm-ing-par-ty-in-vi-ta-tion
 
-# First [100:120] words of the sorted list of all language words in the dictionary.
-print words('nl')[100:120] 
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
