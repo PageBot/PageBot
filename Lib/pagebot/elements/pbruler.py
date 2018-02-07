d36441a40813dd26929e9c53ffb0f96fa7bc9e11
Felipe Corrêa da Silva Sanches <juca@members.fsf.org>
2018-02-07 17:09:00 -0200

    add a few more docTests to Travis

--- Lib/pagebot/elements/pbruler.py
+++ Lib/pagebot/elements/pbruler.py
@@ -45,21 +45,7 @@ class Ruler(Element):
 
         >>> from pagebot.contexts import defaultContext as c
         >>> from pagebot.document import Document
-        >>> w, h = 300, 400
-        >>> doc = Document(w=w, h=h, autoPages=1, padding=30, originTop=False, context=c)
-        >>> page = doc[0]
-        >>> e = Ruler(parent=page, x=0, y=20, w=page.w, h=3)
-        >>> e.build(doc.getView(), (0, 0))
-        >>> e.xy
-        (0, 20)
-        >>> e.size
-        (300, 3, 1)
-        >>> view = doc.getView()
-        >>> e.build(view, (0, 0))
-
-        >>> from pagebot.contexts.flatcontext import FlatContext 
-        >>> from pagebot.document import Document
-        >>> c = FlatContext()
+        >>> w, h = (300, 300)
         >>> doc = Document(w=w, h=h, autoPages=1, padding=30, originTop=False, context=c)
         >>> page = doc[0]
         >>> e = Ruler(parent=page, x=0, y=20, w=page.w, h=3)
@@ -68,6 +54,8 @@ class Ruler(Element):
         >>> e.build(doc.getView(), (0, 0))
         >>> e.xy
         (0, 20)
+        >>> e.size
+        (300, 3, 1)
         """
         context = self.context # Get current context and builder.
 
