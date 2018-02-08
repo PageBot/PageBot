081036950de8f11b408f159fec0aed5414616092
petrvanblokland <buro@petr.com>
2018-02-08 19:56:37 +0100

    Adding more docTests of all Contexts to elements and add method to NoneDrwaBotContext
    Found some bugs of old naming in element builders, good to add docTests.
    Enable     #import ForceErrorHere # Uncheck in case of forcing
    noneDrawBotBuilder testing
    for testing non-DrawBot behaviour if DrawBot is available as context.

---
+++ Lib/pagebot/contexts/platforms/__init__.py
@@ -0,0 +1,28 @@
+# -*- coding: UTF-8 -*-
+# -----------------------------------------------------------------------------
+#
+#     P A G E B O T
+#
+#     Copyright (c) 2016+ Buro Petr van Blokland + Claudia Mens & Font Bureau
+#     www.pagebot.io
+#     Licensed under MIT conditions
+#
+#     Supporting usage of DrawBot, www.drawbot.com
+#     Supporting usage of Flat, https://github.com/xxyxyz/flat
+# -----------------------------------------------------------------------------
+#
+#     pagebot/contexts/__init__.py
+#
+from htmlcontext import HtmlContext
+
+try:
+    import AppKit # Force exception on non-OSX platforms
+    from pagebot.contexts.builders.drawbotbuilder import drawBotBuilder
+    from drawbotcontext import DrawBotContext
+    defaultContext = DrawBotContext() # Test if platform is supporing DrawBot:
+    FlatContext = None
+except ImportError:
+    from flatcontext import FlatContext
+    DrawBotContext = None
+    defaultContext = FlatContext()
+
