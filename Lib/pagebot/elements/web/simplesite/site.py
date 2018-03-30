fcbf192e6c66de81d4b8e93f7d0087c67b05ac8f
petrvanblokland <buro@petr.com>
2018-03-31 00:00:44 +0200

    Solving exceptions in VarFamily.

---
+++ Lib/pagebot/elements/web/simplesite/site.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# -----------------------------------------------------------------------------
+#
+#     P A G E B O T
+#
+#     Copyright (c) 2016+ Buro Petr van Blokland + Claudia Mens & Font Bureau
+#     www.pagebot.io
+#     Licensed under MIT conditions
+#
+#     Supporting usage of DrawBot, www.drawbot.com
+#     Supporting usage of Flat, https://github.com/xxyxyz/flat
+# -----------------------------------------------------------------------------
+#
+#     site.py
+#
+from __future__ import division # Make integer division result in float.
+
+from pagebot.elements.pbgroup import Group
+
+class Site(Group):
+    u"""Build a page, similar to the original template.
+
+    """
+    def build_html(self, view, origin=None, drawElements=True):
+        u"""Build the HTML/CSS navigation, depending on the pages in the root document.
+
+        >>> from pagebot.document import Document
+        >>> from pagebot.elements import newTextBox
+        >>> doc = Document(viewId='Site')
+        >>> page = doc[1]
+        >>> page.title = 'Header Test'
+        >>> page.name = 'index'
+        >>> header = Header(parent=page)
+        >>> tb = newTextBox('This is a header.', parent=header)
+        >>> doc.export('_export/HeaderTest')
+
+        """
+        b = self.context.b
+        self.build_css(view)
+        b.header(cssClass=self.cssClass or self.__class__.__name__.lower())
+
+        if drawElements:
+            # If there are child elements, recursively draw them over the pixel image.
+            self.buildChildElements(view, origin)
+
+        b._header()
+
+
+if __name__ == '__main__':
+    import doctest
+    import sys
+    sys.exit(doctest.testmod()[0])
