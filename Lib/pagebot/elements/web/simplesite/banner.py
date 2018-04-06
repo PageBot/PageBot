49f160ebb05b442feb9ece2248bc71b8dd046372
petrvanblokland <buro@petr.com>
2018-04-06 08:18:38 +0200

    Working on banner doctest

--- Lib/pagebot/elements/web/simplesite/banner.py
+++ Lib/pagebot/elements/web/simplesite/banner.py
@@ -17,11 +17,45 @@
 from __future__ import division # Make integer division result in float.
 
 from pagebot.elements.pbgroup import Group
+from pagebot.toolbox.transformer import point3D, pointOffset, uniqueID
 
 class Banner(Group):
     u"""Draw rectangle, default identical to Element itself.
 
     """
+    def build(self, view, origin, drawElements=True):
+        u"""Default drawing method just drawing the frame.
+        Probably will be redefined by inheriting element classes.
+
+        >>> from pagebot.document import Document
+        >>> from pagebot.elements import newTextBox
+        >>> doc = Document(viewId='Page')
+        >>> page = doc[1]
+        >>> page.title = 'Banner Test'
+        >>> page.name = 'index'
+        >>> banner = Banner(parent=page, cssId='ThisBannerId', w=500, h=100, fill=0.2)
+        >>> tb = newTextBox('This is a banner.', parent=banner)
+        >>> doc.export('_export/BannerTest.pdf')
+        """
+        p = pointOffset(self.oPoint, origin)
+        p = self._applyScale(view, p)
+        px, py, _ = p = self._applyAlignment(p) # Ignore z-axis for now.
+
+        self.buildFrame(view, p) # Draw optional frame or borders.
+
+        if self.drawBefore is not None: # Call if defined
+            self.drawBefore(self, view, p)
+
+        if drawElements:
+            # If there are child elements, recursively draw them over the pixel image.
+            self.buildChildElements(view, p)
+
+        if self.drawAfter is not None: # Call if defined
+            self.drawAfter(self, view, p)
+
+        self._restoreScale(view)
+        view.drawElementMetaInfo(self, origin) # Depends on flag 'view.showElementInfo'
+
     def build_html(self, view, origin=None, drawElements=True):
         u"""Build the HTML/CSS navigation, depending on the pages in the root document.
 
