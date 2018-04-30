99ce1614a17d38b72918a1cf1cf855c2fbb2d545
petrvanblokland <buro@petr.com>
2018-04-30 18:09:07 +0200

    Solved bug that made Line elements with w=0 or h=0 get the minimum width/height value.

--- Lib/pagebot/elements/pbline.py
+++ Lib/pagebot/elements/pbline.py
@@ -16,9 +16,50 @@
 from pagebot.style import NO_COLOR
 from pagebot.toolbox.transformer import pointOffset
 from pagebot.elements.element import Element
-from pagebot.style import ORIGIN
+from pagebot.style import ORIGIN, DEFAULT_WIDTH
+from pagebot.toolbox.units import getUnits, fr, perc, em
 
 class Line(Element):
+
+    def _get_w(self):
+        u"""Answer the width of the element.
+
+        >>> e = Line(w=100, maxW=1000)
+        >>> e.w
+        100
+        >>> e = Line(w=0, maxW=1000)
+        >>> e.w
+        0
+        """
+        w = self.uw # Get uninterpreted unit instance if it exists.
+        if isinstance(w, (fr, perc)):
+            w = w.asPt(self.parent.w) # In case percentage or fraction, answer value in relation to self.parent
+        elif isinstance(w, em):
+            w = w.asPt(self.css('fontSize'))
+        return min(self.maxW, w) # Ingnore self.minW
+    def _set_w(self, w):
+        self.style['w'] = getUnits(w) # Overwrite element local style from here, parent css becomes inaccessable.
+    w = property(_get_w, _set_w)
+
+    def _get_h(self):
+        u"""Answer the height of the element.
+
+        >>> e = Line(h=100, maxH=1000)
+        >>> e.h
+        100
+        >>> e = Line(h=0, maxH=1000)
+        >>> e.h
+        0
+        """
+        h = self.uh # Get uninterpreted unit instance if it exists.
+        if isinstance(h, (fr, perc)):
+            h = h.asPt(self.parent.h) # In case percentage or fraction, answer value in relation to self.parent
+        elif isinstance(h, em):
+            h = h.asPt(self.css('fontSize'))
+        return min(self.maxH, h) # Ingnore self.minH
+    def _set_h(self, h):
+        self.style['h'] = getUnits(h) # Overwrite element local style from here, parent css becomes inaccessable.
+    h = property(_get_h, _set_h)
 
     #   D R A W B O T / F L A T  S U P P O R T
 
@@ -31,12 +72,14 @@ class Line(Element):
         >>> w, h = 300, 400
         >>> doc = Document(w=w, h=h, autoPages=1, padding=30, originTop=False, context=c)
         >>> page = doc[1]
-        >>> e = Line(parent=page, x=0, y=20, w=page.w, h=3)
+        >>> e = Line(parent=page, x=0, y=20, w=page.w, h=0)
+        >>> e.x, e.y, e.w, e.h
+        (0, 20, 300, 0)
         >>> e.build(doc.getView(), (0, 0))
         >>> e.xy
         (0, 20)
         >>> e.size
-        (300, 3, 1)
+        (300, 0, 1)
         >>> view = doc.getView()
         >>> e.build(view, (0, 0))
 
