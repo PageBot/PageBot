690915881daf6563fa4a3c14eae5b7fef3d27d50
Michiel Kauw-A-Tjoe <michiel@concretejungle.nl>
2018-05-30 14:50:33 +0200

    Removing unused pyobjc decorator function from future.
    Former-commit-id: 58398c9c40780e0abd5e399ca67e6f42a493ef32 [formerly 70fe87d5e6ebe9b41eb93fe928087f69b2b25731]
    Former-commit-id: 23c446e776897ad197b197353b4a2ec17834cd8b
    Former-commit-id: 3c78d0fb2f37fd9fed92d80ad0629adc6173f973
    Former-commit-id: 79977fcc72d0c260102fb06f7426734f5185fcc8

--- Lib/pagebot/toolbox/future.py
+++ Lib/pagebot/toolbox/future.py
@@ -14,27 +14,12 @@
 #    future.py
 #
 
-import objc
 import platform
 
 try:
     import builtins
 except:
     import __builtin__ as builtins
-
-PY3 = False
-
-if platform.python_version()[0] == '3':
-    PY3 = True
-
-def decorated(func):
-    """Optional decorated python method for py3 compatibility with PyObjC."""
-    return objc.python_method(func)
-
-def same(func):
-    return func
-
-python_method = decorated if PY3 else same
 
 def chr(char):
     return builtins.chr(char)
