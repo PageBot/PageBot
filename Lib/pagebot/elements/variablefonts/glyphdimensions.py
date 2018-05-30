a5a4e4597aaa78a063b0acd5dadb30ba7d07fe40
petrvanblokland <buro@petr.com>
2018-05-30 06:52:05 -0400

    Some testing changes to the specimen elements.
    There is a problem with the CENTER and RIGHT alignment of TextBox still.
    And the VF functions need to be made global

---
+++ Lib/pagebot/elements/variablefonts/glyphdimensions.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# -----------------------------------------------------------------------------
+#
+#     P A G E B O T
+#
+#     Copyright (c) 2016+ Buro Petr van Blokland + Claudia Mens & Font Bureau
+#     www.pagebot.io
+#     Licensed under MIT conditions
+#
+#     Supporting usage of DrawBot, www.drawbot.com
+#     Supporting usage of Flat, https://github.com/xxyxyz/flat
+# -----------------------------------------------------------------------------
+#
+#     glyphdimensions.py
+#
+from pagebot.toolbox.transformer import pointOffset
+from pagebot.elements.variablefonts.basefontshow import BaseFontShow
+
+class GlyphDimensions(BaseFontShow): 
+    u"""Showing the specified (variable) font large glyphs with a variety
+    of optional measures and indicator.
+
+    """
+    def __init__(self, f, **kwargs):
+        u"""   
+        >>> from pagebot.fonttoolbox.objects.font import findFont
+        >>> from pagebot.document import Document
+        >>> from pagebot.constants import Letter
+        >>> from pagebot.contexts.drawbotcontext import DrawBotContext
+        >>> from pagebot.conditions import *
+        >>> c = DrawBotContext()
+        >>> w, h = Letter
+        >>> doc = Document(w=w, h=h, padding=80, originTop=False, autoPages=2, context=c)
+        >>> style = dict(gh=16, fill=0.95, rLeading=1.4, fontSize=24)  
+        >>> conditions = [Fit()]
+        >>> page = doc[1]
+        >>> font1 = findFont('AmstelvarAlpha-VF')
+        >>> gs = GlyphDimensions(font1, parent=page, conditions=conditions, padding=40, style=style, context=c)
+        >>> style = dict(stroke=0, strokeWidth=0.25, gh=8, rLeading=1.4, fontSize=18) # Smaller fontSize and grid.
+        >>> page = doc[2]
+        >>> font2 = findFont('RobotoDelta-VF')
+        >>> #font2 = findFont('Upgrade-Regular')
+        >>> #font2 = findFont('Escrow-Bold')
+        >>> gs = GlyphDimensions(font2, parent=page, conditions=conditions, style=style, padding=40, context=c)
+        >>> score = doc.solve()
+        >>> doc.export('_export/%sGlyphDimensions.pdf' % font1.info.familyName)
+        """
+        BaseFontShow.__init__(self, **kwargs)
+        self.f = f # Font instance
+
+
+    def build(self, view, origin, drawElements=True):
+        u"""Default drawing method just drawing the frame.
+        Probably will be redefined by inheriting element classes."""
+        p = pointOffset(self.oPoint, origin)
+        p = self._applyScale(view, p)
+        px, py, _ = p = self._applyAlignment(p) # Ignore z-axis for now.
+
+        self.buildFrame(view, p) # Draw optional frame or borders.
+
+        # Let the view draw frame info for debugging, in case view.showElementFrame == True
+        view.drawElementFrame(self, p) 
+
+        if self.drawBefore is not None: # Call if defined
+            self.drawBefore(self, view, p)
+
+        self.drawGlyphDimensions(view, p)
+
+        if self.drawAfter is not None: # Call if defined
+            self.drawAfter(self, view, p)
+
+        self._restoreScale(view)
+        view.drawElementMetaInfo(self, origin) # Depends on flag 'view.showElementInfo'
+
+
+    def drawGlyphDimensions(self, view, origin):
+        u"""Draw the the indicated glyph(s), with dimensions and other indicator.
+        """
+        c = self.context
+        ox, oy, _ = origin
+        glyph = self.f['H']
+        c.save()
+        c.stroke(0, 0.25)
+        c.fill((0.7, 0.7, 0.7, 0.6))
+        s = (self.pw/glyph.width)
+        c.scale(s)
+        c.drawPath(glyph.path, ((ox+self.pl)/s, (oy+self.ph/3)/s))
+        c.restore()
+
+if __name__ == '__main__':
+    import doctest
+    import sys
+    sys.exit(doctest.testmod()[0])
