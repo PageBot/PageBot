e2764edcb03b8c6349f1a7592454c8524688aebe
petrvanblokland <buro@petr.com>
2018-06-08 08:35:53 -0400

    Solving path stuff for Flat.
    Working on Banners
    
    
    Former-commit-id: 16bd3458076916f889eb3a0c9cab33aaf0a2ea54 [formerly 27342cc7fe06000a935f33571c520494f59d422e]
    Former-commit-id: 16af0adcb751b63bf09ad22d907bc114a4ef4f99
    Former-commit-id: 1a5a5c05b9bc89144f21e26202c72b527df64257
    Former-commit-id: e5b0cf5908995e539662ff54d7620ba6125534e3

--- Examples/TypeSpecimens/GimletBanner.py
+++ Examples/TypeSpecimens/GimletBanner.py
@@ -31,40 +31,27 @@ class AnimatedBannerFrame(AnimationFrame):
             """
             ox, oy, _ = origin
             c = self.context
-            # Make the instance for the style holding the location
             style = self.style.copy()
-            instance = self.f.getInstance(self.style['location'])#instance.path
+            
+            # Now make instance and draw over regular and add to new copy of the style
+            style = self.style.copy()
+            instance = self.f.getInstance(self.style['location'])
             style['font'] = instance.path
-
-            # Draw the growing text on the right in the real font size enlarged by fontSizeFactor
-            bs = c.newString(self.sampleText, style=style)
-            tw1, th1 = bs.textSize()
-            x1, y1 = self.w*2/3, self.h/2-th1/4
-            c.text(bs, (x1, y1))
-                  
-            # Magnifying glass 
-            R = 320
-            L = 40
-            c.stroke((0.6, 0.8, 1, 0.9), L)
-            c.fill(None)
-            x = x1-R/2+R/8
-            y = self.h/2-R/2
-            c.oval(x, y, R, R)
-            c.line((x-L/2+2, y+R/2), (x-R+2*L, y+R/2))
-       
-            # Large "Opt" on the left
-            style['fontSize'] = self.f.axes['opsz'][2]*magnifySizeFactor
-            bs = c.newString(self.sampleText[:3], style=style)
-            tw2, th2 = bs.textSize()
-            x2, y2 = self.w/2-tw2, self.h/2-th2/5
-            c.text(bs, (x2, y2)) # Make tekst right aligned
+            #print(self.frameIndex, style['font'])
+            #style['fontSize'] = self.h/3
+            bs = c.newString(self.sampleText, style=style, w=self.pw)
+            tw, th = bs.textSize()
+            c.text(bs, (self.w/2 - tw/2, self.h/2-th/2))
      
 c = DrawBotContext()
 W, H = 2040, 1020 # Type Network banners
-font = findFont('AmstelvarAlpha-VF')
-# Amstelvar axes to select from: here we are showing the optical size.
+
+# Claire: for now, add your Fit-Variable_1.ttf to your /Library/Fonts and it can be found.
+#Gimlet_Italics-VF.ttf#Gimlet_Romans-VF.ttf
+font = findFont('Gimlet_Italics-VF')
+# Fit axes to select from: here we are showing the optical size.
 # Define tag list for axes to be part of the animation as sequence
-sequenceAxes = ['opsz']
+sequenceAxes = ['wdth']
 sequenceLength = 3 # Seconds per sequence
 sequences = len(sequenceAxes) # Amount of sequences, one per axis
 duration = sequenceLength * len(sequenceAxes) # Total duration of the animation in seconds
@@ -72,14 +59,11 @@ framesPerSecond = 10
 frameCnt = duration * framesPerSecond # Total number of frames
 axisFrames = sequenceLength * framesPerSecond # Number of frames per axis sequence.
 
-fontSizeFactor = 3 # Enlarge the [opsz] font size by this factor.
-magnifySizeFactor = 8 # Enlarge magnified word by this factor, compared to the max value of [opsz]
-
 # Create a new doc, with the right amount of frames/pages.
 doc = Document(w=W, h=H, originTop=False, frameDuration=1.0/framesPerSecond, 
     autoPages=frameCnt, context=c)
 # Sample text to show in the animation
-sample = 'Optical size' #font.info.familyName #'Decovar'
+sample = 'Fitting' 
 
 frameIndex = 1 # Same as page index in the document
 for axisTag in sequenceAxes:
@@ -89,14 +73,13 @@ for axisTag in sequenceAxes:
         page = doc[frameIndex] # Get the current frame-page
         page.w = W
 
+        axisRange = maxValue - minValue
         phisin = sin(radians(axisFrameIndex/axisFrames * 360+3/4*360))*0.5+0.5
-        fontSize = phisin*(maxValue - minValue) + minValue
-
+        
         # Variable Font location for this frame sample
-        location = {axisTag: fontSize}
+        location = {axisTag: phisin*axisRange+minValue}
         # Overall style for the frame
-        style = dict(rLeading=1.4, fontSize=fontSize*fontSizeFactor, xTextAlign=RIGHT, textFill=1, 
-            testStroke=None,
+        style = dict(rLeading=1.4, fontSize=H-40, xTextAlign=RIGHT, textFill=1, 
             fill=0, location=location)
         
         af = AnimatedBannerFrame(sample, font, frameCnt, frameIndex, parent=page, style=style, 
