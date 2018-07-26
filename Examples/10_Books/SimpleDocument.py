5170f50bb3f8b31c579c5acc7f0f77806c280d65
petrvanblokland <buro@petr.com>
2018-07-26 23:38:35 +0200

    Debugging example

--- Examples/10_Books/SimpleDocument.py
+++ Examples/10_Books/SimpleDocument.py
@@ -25,12 +25,12 @@ from pagebot.toolbox.units import pt
 
 if 1:
     from pagebot.contexts.platform import getContext
-    context = getContext()
+    context = getContext() # DrawBotContext or FlatContext if available.
 else:
     from pagebot.contexts.svgcontext import SvgContext
     context = SvgContext()
 
-W, H = A4 # Standard page size
+W, H = 498, 770#A4 # Standard page size
 # To see how the "responsive" layout works in landscape, swap W and H
 #H, W = A4
 
@@ -42,20 +42,21 @@ doc = Document(w=W, h=H, title="Demo pages", originTop=False, context=context)
 view = doc.view
 view.padding = pt(40) # Space around the page to accommodate registratio and crop marks.
 view.showPageCropMarks = True # Set the view flags what to show.
-view.showPageRegistrationMarks = False
-view.showPagePadding = False
-view.showPageFrame = False
-view.showPageNameInfo = False
+view.showPageRegistrationMarks = True
+view.showPagePadding = True
+view.showPageFrame = True
+view.showPageNameInfo = True
 
 # Get the page of the document. Since right pages are uneven, the list of pages
 # startes at #1.
 page = doc[1]
 page.padding = PAD, PAD, 2*PAD, PAD # Set the padding of the page, as we don't use a template here.
 # Create rectangles. Default position and size is (0, 0, 100, 100). The conditions define the layout.
-newRect(fill=color(1, 0, 0), parent=page, h=pt(50), conditions=(Left2Left(), Float2Top(),Fit2Right()))
+newRect(fill=color(1, 0, 0), parent=page, h=pt(62), conditions=(Left2Left(), Float2Top(),Fit2Right()))
 newRect(fill=color(1, 0, 1), parent=page, conditions=(Left2LeftSide(), Float2Top()))
-newRect(fill=color(0, 1, 1), parent=page, conditions=(Right2Right(), Float2Top(), Float2Left(), Fit2Right()))
+r = newRect(fill=color(0, 1, 1), parent=page, conditions=(Right2Right(), Float2Top(), Float2Left(), Fit2Right()))
 # Solve the conditions of the layout in defined order.
+print(r.style['fill'])
 doc.solve()
 # Export to PDF.
 doc.export('_export/SimpleDocument.pdf')
