6ba731261793645362443382b7ae554486f41611
Michiel Kauw-A-Tjoe <michiel@concretejungle.nl>
2018-08-13 15:58:24 +0200

    Using context to draw.
    Former-commit-id: c08d2c397ea06301de74e5dc317592c0c7bdbd6e [formerly e411cfc46fe5a8de6f1a75047f25a01e4160447e]
    Former-commit-id: 192923cadb8446b76d7d07b6f90882f9ef261f33
    Former-commit-id: 9a297c31661058a0d199eaa224fde6813fabfd46
    Former-commit-id: 34430e089e4c1cc3829fc4229442aa0dfc41e62e

--- Examples/140_Infographics/classHierarchy.py
+++ Examples/140_Infographics/classHierarchy.py
@@ -12,24 +12,39 @@
 #
 #     classHierarchy.py
 #
-import pagebot
-import pagebot.contexts.drawbotcontext
+
 import sys, inspect
+import pagebot.contexts.drawbotcontext
+from pagebot.contexts.platform import getContext
+from pagebot.toolbox.units import *
+from pagebot.toolbox.color import Color, blackColor
+
+context = getContext()
+
+HBOX = 30
+WBOX = 160
 
 def drawClassHierarchy(obj):
     x = 100
     y = 100
-    for c in obj.__mro__:
+    for c in list(obj.__mro__)[::-1]:
         drawClass(c, x, y)
         y += 100
     print('...')
 
 def drawClass(c, x, y):
-    print(c)
+    color = Color(0.6, 1, 0.6)
+    context.fill(color)
+    context.rect(pt(x), pt(y), pt (WBOX), pt(HBOX))
+    context.fill(blackColor)
+    context.fontSize(pt(20))
+    context.text(c.__name__, (x+5, y+5))
+    
+context.newPage(pt(1000), pt(1000))
+#size('A1')
     
 classes = inspect.getmembers(sys.modules['pagebot.contexts.drawbotcontext'])
 
 for name, obj in classes:
     if inspect.isclass(obj):
         drawClassHierarchy(obj)
-        
