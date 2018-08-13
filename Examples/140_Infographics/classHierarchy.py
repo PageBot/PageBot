b8b5872e21341965a2d749b63f68ae79d881997d
Michiel Kauw-A-Tjoe <michiel@concretejungle.nl>
2018-08-13 13:58:39 +0200

    Starting example.
    Former-commit-id: c9e5a852dc6fa3c948a9dbd3546ff09d98a2a46a [formerly 9a6a6297b64957cae7639118c3d62b40abfd5f2a]
    Former-commit-id: 16292ade581bfbf74c487a2463e95a1f611cfb77
    Former-commit-id: f4c616cdb579ea7711c4811f26a2e5c3eb602c9c
    Former-commit-id: df457eb01058fdf054b1f5b76951d8d1059c7d8c

--- Examples/140_Infographics/classHierarchy.py
+++ Examples/140_Infographics/classHierarchy.py
@@ -12,3 +12,24 @@
 #
 #     classHierarchy.py
 #
+import pagebot
+import pagebot.contexts.drawbotcontext
+import sys, inspect
+
+def drawClassHierarchy(obj):
+    x = 100
+    y = 100
+    for c in obj.__mro__:
+        drawClass(c, x, y)
+        y += 100
+    print('...')
+
+def drawClass(c, x, y):
+    print(c)
+    
+classes = inspect.getmembers(sys.modules['pagebot.contexts.drawbotcontext'])
+
+for name, obj in classes:
+    if inspect.isclass(obj):
+        drawClassHierarchy(obj)
+        
