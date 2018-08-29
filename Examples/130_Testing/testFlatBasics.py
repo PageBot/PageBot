00936ff23c4d99e67db1e5ba9f9b224ab99e50c1
Michiel Kauw-A-Tjoe <michiel@concretejungle.nl>
2018-08-29 11:04:36 +0200

    Restoring flat test.

--- Examples/130_Testing/testFlatBasics.py
+++ Examples/130_Testing/testFlatBasics.py
@@ -1,23 +1,25 @@
 
 # Only runs under Flat
 from flat import rgb, font, shape, strike, document
+from pagebot.fonttoolbox.objects.font import findFont
 
-font = findFont('Roboto-Bold')
-
-#c = rgb(255, 0, 0)
-c = rgb(0, 0, 0)
-#lato = font.open('/Library/Fonts/Verdana.ttf')
-lato = font.open('/Library/Fonts/Upgrade-Middle.ttf')
+f = findFont('Roboto-Bold')
+print(f)
+print(f.path)
+c = rgb(255, 0, 0)
+ff = font.open(f.path)
+print(ff)
 figure = shape().stroke(c).width(2.5)
-headline = strike(lato).color(c).size(80, 96)
-
+headline = strike(ff).color(c).size(80, 96)
 d = document(400, 200, 'mm')
 p = d.addpage()
 p.place(figure.circle(50, 50, 20))
 t = headline.text(u'Hello world! AVT.TeYAYeYé')
-p.place(t).frame(10, 10, 380, 80)
+#p.place(t).frame(10, 10, 380, 80)
+'''
 im = p.image(kind='rgb')
 im.png('_export/hello.png')
 im.jpeg('_export/hello.jpg')
 p.svg('_export/hello.svg')
 d.pdf('_export/hello.pdf')
+'''
