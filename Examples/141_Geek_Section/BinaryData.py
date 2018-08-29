90edb70eb10b1302a24c02482aafe3ec3850073b
Michiel Kauw-A-Tjoe <michiel@concretejungle.nl>
2018-08-29 13:14:33 +0200

    Reading PNG as binary.

---
+++ Examples/141_Geek_Section/BinaryData.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -----------------------------------------------------------------------------
+#     Copyright (c) 2016+ Buro Petr van Blokland + Claudia Mens
+#     www.pagebot.io
+#
+#     P A G E B O T
+#
+#     Licensed under MIT conditions
+#
+#     Supporting DrawBot, www.drawbot.com
+#     Supporting Flat, xxyxyz.org/flat
+# -----------------------------------------------------------------------------
+#
+#     BinaryData.py
+#
+#     Shows how to read and write binary data.
+#
+
+# Creates empty bytes.
+empty_bytes = bytes(4)
+print(type(empty_bytes))
+print(empty_bytes)
+
+# Casts bytes to bytearray.
+mutable_bytes = bytearray(empty_bytes)
+print(mutable_bytes)
+mutable_bytes = bytearray(b'\x00\x0F')
+
+# Bytearray allows modification:
+mutable_bytes[0] = 255
+mutable_bytes.append(255)
+print(mutable_bytes)
+
+# Cast bytearray back to bytes
+immutable_bytes = bytes(mutable_bytes)
+print(immutable_bytes)
+
+from pagebot import getResourcesPath
+imagePath = getResourcesPath() + "/images/peppertom_lowres_398x530.png"
+
+with open(imagePath, "rb") as binary_file:
+    
+    # Seek position and read N bytes
+    binary_file.seek(0)  # Go to beginning
+    couple_bytes = binary_file.read(2)
+    print(couple_bytes)
+    
+    # Read the whole file at once
+    data = binary_file.read()
+    mutable_bytes = bytearray(data)
+    print(len(mutable_bytes))
