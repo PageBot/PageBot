3f756bba7d809275beff7c5ab7b3a3b3bd117ad0
Michiel Kauw-A-Tjoe <michiel@concretejungle.nl>
2018-09-04 10:51:24 +0200

    Testing PDF binary.
    Former-commit-id: ecae3d97493bf29c213b465d37329ac022e74702 [formerly 2b80f62fe01b386d557a6a9e61aac9db4fec6998]
    Former-commit-id: 5e386aee27a4f7282fe976c5e4b16106966123f3
    Former-commit-id: e20d7a11157b38318a68bcf7af576042bef8636b
    Former-commit-id: a576c9b722afc853229ee727726e643e6f70eae5

--- Examples/141_Geek_Section/BinaryPDF.py
+++ Examples/141_Geek_Section/BinaryPDF.py
@@ -15,3 +15,38 @@
 #
 #     Shows how to read and write a PDF file.
 #
+from pagebot.contexts.platform import getContext
+import os, os.path
+
+context = getContext()
+pdfPath = 'test.pdf'
+if not os.path.exists('_export'):
+    os.mkdir('_export')
+
+with open(pdfPath, "rb") as binary_file:
+
+    
+    # Read the whole file at once
+    data = binary_file.read()
+
+    # Copies data.
+    newFile = open("_export/testcopy.pdf", "wb")
+    newFile.write(data)
+
+    # Shows byte values for first 100 blocks.
+    context.newDocument(1100, 1100)
+    context.newPage(1100, 1100)
+    
+    print(len(data))
+    x = 100
+    y = context.b.height() - 100
+
+    for i in range(101):
+        context.text(str(data[i]), (x, y))
+        
+        x += 100
+        if i > 0 and i % 10 == 0:
+            x = 100
+            y -= 100
+
+    context.saveDocument("_export/bytes.pdf")
