47a3bf783f98516c63b8eece2999551826ca8c36
Michiel Kauw-A-Tjoe <michiel@concretejungle.nl>
2018-09-04 10:35:15 +0200

    Splitting up examples, starting PDF.
    Former-commit-id: 32048fb148c753fa1e5bc8655e4afccf3779cda8 [formerly 5a3d9f653929c2a3206074ec00d51a4e1d620012]
    Former-commit-id: 7243b3a3c4c062470ba17ee36912b50ad7b88234
    Former-commit-id: 6f13343c43fdf87065ed441b33b7120eb85cb12f
    Former-commit-id: b616f766b6ea300a3c9c4f65d9c4191cc73eb9fa

---
+++ Examples/141_Geek_Section/BinaryPDF.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -----------------------------------------------------------------------------
+#     Copyright (c) 2016+ Buro Petr van Blokland + Claudia Mens
+#     www.pagebot.io
+#
+#     P A G E B O T
+#
+#     Licensed under MIT conditions
+#
+#     Supporting DrawBot, www.drawbot.com
+#     Supporting Flat, xxyxyz.org/flat
+# -----------------------------------------------------------------------------
+#
+#     BinaryPDF.py
+#
+#     Shows how to read and write a PDF file.
+#
