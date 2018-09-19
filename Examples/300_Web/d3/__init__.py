b43445485f91294502f5b445a82edaf391b738f5
petrvanblokland <buro@petr.com>
2018-09-19 12:10:05 +0200

    Cleaning D3-init file
    Former-commit-id: 612bb92c255607d7bf690223883201d05db17193 [formerly 604c1b6a99b0a77eb6a225a398eba97eb534929d]
    Former-commit-id: 957c84116d8356132814d7a69a09193686d24d02
    Former-commit-id: 05cae4da2e1ece0570624863b8d75cdff724379e
    Former-commit-id: edd08cef23f4154a2d6f8147641843815d7e8539

--- Examples/300_Web/d3/__init__.py
+++ Examples/300_Web/d3/__init__.py
@@ -16,13 +16,10 @@
 #     Elements with behavior that works best as website output (e.g. because
 #     they are animated or interactive). Yet, these elements can still be used
 #     building in static output media, such as PDF or PNG.
+#   
+#     The elements in this folder use the d3.js library as base: https://d3js.org
 #
-from pagebot.elements.web.simplesite.header import Header
-from pagebot.elements.web.simplesite.navigation import Navigation
-from pagebot.elements.web.simplesite.banner import Banner
-from pagebot.elements.web.simplesite.logo import Logo
-from pagebot.elements.web.simplesite.introduction import Introduction
-from pagebot.elements.web.simplesite.hero import Hero
-from pagebot.elements.web.simplesite.featured import Featured
-from pagebot.elements.web.simplesite.content import WideContent
-from pagebot.elements.web.simplesite.footer import Footer
+#     Future resources
+#     https://www.jasondavies.com/animated-bezier/ = Bezier/Quadratics editor?
+#
+from pagebot.elements.web.d3.barchart import BarChart
