0f663d178657a0bcffc1233c06cc7d39dd247475
petrvanblokland <buro@petr.com>
2018-09-24 14:39:49 +0200

    Working on element sides positions test
    Former-commit-id: 45977449a103d68347d1bf044959609a75a90792 [formerly 782da0e1f8ba59d6e41fe881d74d6b62d3657c90]
    Former-commit-id: e5e4e959eb420238971a0f13400ba9a815e465f0
    Former-commit-id: dc87a006f1afa6f72e637c52b8cd1737192a1c38
    Former-commit-id: 77a2efc097aebd34f56df41f2ecbc9518adc991c

--- Examples/060_Typography/110_OriginTopBottom.py
+++ Examples/060_Typography/110_OriginTopBottom.py
@@ -19,6 +19,9 @@
 #     that are used by conditions.
 #     Test on variations of origin alignments and top/bottom of element origin.
 #
+#     TODO: Seems to be a problem when using picas p( ) as measurements for page size
+#     TODO: color circles have wrong offset for originTop=False
+#
 from pagebot.document import Document # Get the main Document class
 from pagebot.toolbox.units import pt, inch, p
 from pagebot.toolbox.color import color
@@ -30,27 +33,34 @@ from pagebot.conditions import *
 
 context = getContext() # Get the current context (e.g. DrawBotContext instance)
 
+DO_SOLVE = False
+
 # Example baseline position, showing that start can be different from page side
 # or top of text box.
-BASELINE = pt(15)
-BASELINE_START = 3.5 * BASELINE
-TP = 5*BASELINE
-RP = 3*BASELINE
-BP = 6*BASELINE
-LP = 4*BASELINE
+UNIT = pt(15)
+TP = 4*UNIT
+RP = 3*UNIT
+BP = 8*UNIT
+LP = 6*UNIT
 PADDING = TP, RP, BP, LP # Page padding related to baseline in this example.
-W, H = B5
+W, H = pt(300, 400)
 EW = W - LP - RP
 EH = H - TP - BP
 
 E_FILL = color(0.5, 0.5, 0.5, 0.5)
+GREEN = color(rgb='green')
+RED = color(rgb='red')
+
 R = 10 # Radius of the red/green circles.
 
-CONDS = []#[Fit()]
+if DO_SOLVE:
+    CONDS = [Fit()]
+else:
+    CONDS = []
 
 doc = Document(w=W, h=H, padding=PADDING, 
     autoPages=2*3, # Create multiple pages, to show page origin top/bottom and element alignments.
-    baselineGrid=BASELINE, baselineGridStart=BASELINE_START)
+)
 
 view = doc.view # Get the current view of this document. Defaulse it PageView.
 view.showPadding = True # Show the padding of the page. The size is then (page.pw, page.ph)
@@ -63,22 +73,24 @@ r1.showOrigin = True
 # 'Origin Top, box alignment top-left', 
 r1.xAlign = LEFT
 r1.yAlign = TOP
-#page.solve() # Make the r1 fit the page padding
-r1.x = LP
-r1.y = TP
-r1.size = EW, EH
+if DO_SOLVE:
+    page.solve() # Make the r1 fit the page padding
+else:
+    r1.x = LP
+    r1.y = TP
+    r1.size = EW, EH
 
-newCircle(x=r1.x+r1.w/2-R, y=r1.y, r=R, fill=color(rgb='green'), parent=page) # x,y position
-newCircle(x=r1.center+R, y=r1.top, r=R, fill=color(rgb='red'), parent=page) # sides location
+newCircle(x=r1.x+r1.w/2-R, y=r1.y, r=R, fill=GREEN, parent=page) # x,y position
+newCircle(x=r1.center+R, y=r1.top, r=R, fill=RED, parent=page) # sides location
  
-newCircle(x=r1.x+r1.w-R, y=r1.y+r1.h/2, r=R, fill=color(rgb='green'), parent=page)
-newCircle(x=r1.right+R, y=r1.middle, r=R, fill=color(rgb='red'), parent=page)
+newCircle(x=r1.x+r1.w-R, y=r1.y+r1.h/2, r=R, fill=GREEN, parent=page)
+newCircle(x=r1.right+R, y=r1.middle, r=R, fill=RED, parent=page)
 
-newCircle(x=r1.x+r1.w/2-R, y=r1.y+r1.h, r=R, fill=color(rgb='green'), parent=page)
-newCircle(x=r1.center+R, y=r1.bottom, r=R, fill=color(rgb='red'), parent=page)
+newCircle(x=r1.x+r1.w/2-R, y=r1.y+r1.h, r=R, fill=GREEN, parent=page)
+newCircle(x=r1.center+R, y=r1.bottom, r=R, fill=RED, parent=page)
 
-newCircle(x=r1.x-R, y=r1.y+r1.h/2, r=R, fill=color(rgb='green'), parent=page)
-newCircle(x=r1.left+R, y=r1.middle, r=R, fill=color(rgb='red'), parent=page)
+newCircle(x=r1.x-R, y=r1.y+r1.h/2, r=R, fill=GREEN, parent=page)
+newCircle(x=r1.left+R, y=r1.middle, r=R, fill=RED, parent=page)
 
 
 page = doc[2]
@@ -89,22 +101,24 @@ r1.showOrigin = True
 # 'Origin Top, box alignment middle-center', 
 r1.xAlign = CENTER
 r1.yAlign = MIDDLE
-#page.solve() # Make the r1 fit the page padding
-r1.x = LP + EW/2
-r1.y = TP + EH/2
-r1.size = EW, EH
+if DO_SOLVE:
+    page.solve() # Make the r1 fit the page padding
+else:
+    r1.x = LP + EW/2
+    r1.y = TP + EH/2
+    r1.size = EW, EH
 
-newCircle(x=r1.x-R, y=r1.y-r1.h/2, r=R, fill=color(rgb='green'), parent=page)
-newCircle(x=r1.center+R, y=r1.top, r=R, fill=color(rgb='red'), parent=page)
+newCircle(x=r1.x-R, y=r1.y-r1.h/2, r=R, fill=GREEN, parent=page)
+newCircle(x=r1.center+R, y=r1.top, r=R, fill=RED, parent=page)
  
-newCircle(x=r1.x+r1.w/2-R, y=r1.y, r=R, fill=color(rgb='green'), parent=page)
-newCircle(x=r1.right+R, y=r1.middle, r=R, fill=color(rgb='red'), parent=page)
+newCircle(x=r1.x+r1.w/2-R, y=r1.y, r=R, fill=GREEN, parent=page)
+newCircle(x=r1.right+R, y=r1.middle, r=R, fill=RED, parent=page)
 
-newCircle(x=r1.x-R, y=r1.y+r1.h/2, r=R, fill=color(rgb='green'), parent=page) # By coordinates
-newCircle(x=r1.center+R, y=r1.bottom, r=R, fill=color(rgb='red'), parent=page) # By positioning on sides
+newCircle(x=r1.x-R, y=r1.y+r1.h/2, r=R, fill=GREEN, parent=page) # By coordinates
+newCircle(x=r1.center+R, y=r1.bottom, r=R, fill=RED, parent=page) # By positioning on sides
 
-newCircle(x=r1.x-r1.w/2-R, y=r1.y, r=R, fill=color(rgb='green'), parent=page)
-newCircle(x=r1.left+R, y=r1.middle, r=R, fill=color(rgb='red'), parent=page)
+newCircle(x=r1.x-r1.w/2-R, y=r1.y, r=R, fill=GREEN, parent=page)
+newCircle(x=r1.left+R, y=r1.middle, r=R, fill=RED, parent=page)
 
 
 page = doc[3]
@@ -115,22 +129,24 @@ r1.showOrigin = True
 # 'Origin Top, box alignment bottom-right', 
 r1.xAlign = RIGHT
 r1.yAlign = BOTTOM
-#page.solve() # Make the r1 fit the page padding
-r1.x = LP + EW
-r1.y = TP + EH
-r1.size = EW, EH
+if DO_SOLVE:
+    page.solve() # Make the r1 fit the page padding
+else:
+    r1.x = LP + EW
+    r1.y = TP + EH
+    r1.size = EW, EH
 
-newCircle(x=r1.x-r1.w/2-R, y=r1.y-r1.h, r=R, fill=color(rgb='green'), parent=page)
-newCircle(x=r1.center+R, y=r1.top, r=R, fill=color(rgb='red'), parent=page)
+newCircle(x=r1.x-r1.w/2-R, y=r1.y-r1.h, r=R, fill=GREEN, parent=page)
+newCircle(x=r1.center+R, y=r1.top, r=R, fill=RED, parent=page)
  
-newCircle(x=r1.x-R, y=r1.y-r1.h/2, r=R, fill=color(rgb='green'), parent=page)
-newCircle(x=r1.right+R, y=r1.middle, r=R, fill=color(rgb='red'), parent=page)
+newCircle(x=r1.x-R, y=r1.y-r1.h/2, r=R, fill=GREEN, parent=page)
+newCircle(x=r1.right+R, y=r1.middle, r=R, fill=RED, parent=page)
 
-newCircle(x=r1.x-r1.w/2-R, y=r1.y, r=R, fill=color(rgb='green'), parent=page) # By coordinates
-newCircle(x=r1.center+R, y=r1.bottom, r=R, fill=color(rgb='red'), parent=page) # By positioning on sides
+newCircle(x=r1.x-r1.w/2-R, y=r1.y, r=R, fill=GREEN, parent=page) # By coordinates
+newCircle(x=r1.center+R, y=r1.bottom, r=R, fill=RED, parent=page) # By positioning on sides
 
-newCircle(x=r1.x-r1.w-R, y=r1.y-r1.h/2, r=R, fill=color(rgb='green'), parent=page)
-newCircle(x=r1.left+R, y=r1.middle, r=R, fill=color(rgb='red'), parent=page)
+newCircle(x=r1.x-r1.w-R, y=r1.y-r1.h/2, r=R, fill=GREEN, parent=page)
+newCircle(x=r1.left+R, y=r1.middle, r=R, fill=RED, parent=page)
 
 
 page = doc[4]
@@ -146,17 +162,17 @@ r1.x = LP
 r1.y = BP + EH
 r1.size = EW, EH
 
-newCircle(x=r1.x+r1.w/2-R, y=r1.y, r=R, fill=color(rgb='green'), parent=page)
-newCircle(x=r1.center+R, y=r1.top, r=R, fill=color(rgb='red'), parent=page)
+newCircle(x=r1.x+r1.w/2-R, y=r1.y, r=R, fill=GREEN, parent=page)
+newCircle(x=r1.center+R, y=r1.top, r=R, fill=RED, parent=page)
  
-newCircle(x=r1.x+r1.w-R, y=r1.y-r1.h/2, r=R, fill=color(rgb='green'), parent=page)
-newCircle(x=r1.right+R, y=r1.middle, r=R, fill=color(rgb='red'), parent=page)
+newCircle(x=r1.x+r1.w-R, y=r1.y-r1.h/2, r=R, fill=GREEN, parent=page)
+newCircle(x=r1.right+R, y=r1.middle, r=R, fill=RED, parent=page)
 
-newCircle(x=r1.x+r1.w/2-R, y=r1.y-r1.h, r=R, fill=color(rgb='green'), parent=page)
-newCircle(x=r1.center+R, y=r1.bottom, r=R, fill=color(rgb='red'), parent=page)
+newCircle(x=r1.x+r1.w/2-R, y=r1.y-r1.h, r=R, fill=GREEN, parent=page)
+newCircle(x=r1.center+R, y=r1.bottom, r=R, fill=RED, parent=page)
 
-newCircle(x=r1.x-R, y=r1.y-r1.h/2, r=R, fill=color(rgb='green'), parent=page)
-newCircle(x=r1.left+R, y=r1.middle, r=R, fill=color(rgb='red'), parent=page)
+newCircle(x=r1.x-R, y=r1.y-r1.h/2, r=R, fill=GREEN, parent=page)
+newCircle(x=r1.left+R, y=r1.middle, r=R, fill=RED, parent=page)
 
 
 page = doc[5]
@@ -172,17 +188,17 @@ r1.x = LP + EW/2
 r1.y = BP + EH/2
 r1.size = EW, EH
 
-newCircle(x=r1.x-R, y=r1.y+r1.h/2, r=R, fill=color(rgb='green'), parent=page)
-newCircle(x=r1.center+R, y=r1.top, r=R, fill=color(rgb='red'), parent=page)
+newCircle(x=r1.x-R, y=r1.y+r1.h/2, r=R, fill=GREEN, parent=page)
+newCircle(x=r1.center+R, y=r1.top, r=R, fill=RED, parent=page)
  
-newCircle(x=r1.x+r1.w/2-R, y=r1.y, r=R, fill=color(rgb='green'), parent=page)
-newCircle(x=r1.right+R, y=r1.middle, r=R, fill=color(rgb='red'), parent=page)
+newCircle(x=r1.x+r1.w/2-R, y=r1.y, r=R, fill=GREEN, parent=page)
+newCircle(x=r1.right+R, y=r1.middle, r=R, fill=RED, parent=page)
 
-newCircle(x=r1.x-R, y=r1.y-r1.h/2, r=R, fill=color(rgb='green'), parent=page)
-newCircle(x=r1.center+R, y=r1.bottom, r=R, fill=color(rgb='red'), parent=page)
+newCircle(x=r1.x-R, y=r1.y-r1.h/2, r=R, fill=GREEN, parent=page)
+newCircle(x=r1.center+R, y=r1.bottom, r=R, fill=RED, parent=page)
 
-newCircle(x=r1.x-r1.w/2-R, y=r1.y, r=R, fill=color(rgb='green'), parent=page)
-newCircle(x=r1.left+R, y=r1.middle, r=R, fill=color(rgb='red'), parent=page)
+newCircle(x=r1.x-r1.w/2-R, y=r1.y, r=R, fill=GREEN, parent=page)
+newCircle(x=r1.left+R, y=r1.middle, r=R, fill=RED, parent=page)
 
 
 page = doc[6]
@@ -198,17 +214,17 @@ r1.x = LP + EW
 r1.y = BP
 r1.size = EW, EH
 
-newCircle(x=r1.x-r1.w/2-R, y=r1.y+r1.h, r=R, fill=color(rgb='green'), parent=page)
-newCircle(x=r1.center+R, y=r1.top, r=R, fill=color(rgb='red'), parent=page)
+newCircle(x=r1.x-r1.w/2-R, y=r1.y+r1.h, r=R, fill=GREEN, parent=page)
+newCircle(x=r1.center+R, y=r1.top, r=R, fill=RED, parent=page)
 
-newCircle(x=r1.x-R, y=r1.y+r1.h/2, r=R, fill=color(rgb='green'), parent=page)
-newCircle(x=r1.right+R, y=r1.middle, r=R, fill=color(rgb='red'), parent=page)
+newCircle(x=r1.x-R, y=r1.y+r1.h/2, r=R, fill=GREEN, parent=page)
+newCircle(x=r1.right+R, y=r1.middle, r=R, fill=RED, parent=page)
 
-newCircle(x=r1.x-r1.w/2-R, y=r1.y, r=R, fill=color(rgb='green'), parent=page)
-newCircle(x=r1.center+R, y=r1.bottom, r=R, fill=color(rgb='red'), parent=page)
+newCircle(x=r1.x-r1.w/2-R, y=r1.y, r=R, fill=GREEN, parent=page)
+newCircle(x=r1.center+R, y=r1.bottom, r=R, fill=RED, parent=page)
 
-newCircle(x=r1.x-r1.w-R, y=r1.y+r1.h/2, r=R, fill=color(rgb='green'), parent=page)
-newCircle(x=r1.left+R, y=r1.middle, r=R, fill=color(rgb='red'), parent=page)
+newCircle(x=r1.x-r1.w-R, y=r1.y+r1.h/2, r=R, fill=GREEN, parent=page)
+newCircle(x=r1.left+R, y=r1.middle, r=R, fill=RED, parent=page)
 
 
 # Export the document showing the baselines of the page as horizontal lines and the padding.  
