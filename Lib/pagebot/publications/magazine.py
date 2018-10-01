2c4ef045c40b2385c6567cedd4c8643808ebec56
petrvanblokland <buro@petr.com>
2018-10-01 03:13:46 +0200

    Working on Magazine class
    Former-commit-id: 71c19d5b891745b99f1d66480404b96872537fd1 [formerly d49c687752b5302ed97322a6032522274568b89f]
    Former-commit-id: 9a53e4b8cb38a075dbffa1eb02b313334008ab7b
    Former-commit-id: 90502ed6924ffbb69d95a30c47cf4053d0e956e0
    Former-commit-id: 8bbe7eb8686bbf3ec9683cf332b6c87a52f84b07

--- Lib/pagebot/publications/magazine.py
+++ Lib/pagebot/publications/magazine.py
@@ -14,21 +14,51 @@
 #
 from pagebot.publications.publication import Publication
 from pagebot.document import Document
-from pagebot.elements import newRect, Element
+from pagebot.elements import *
 from pagebot.conditions import *
 from pagebot.toolbox.units import p, pt
+from pagebot.toolbox.color import noColor
+from pagebot.fonttoolbox.objects.font import findFont
+from pagebot.constants import LEFT, RIGHT
+
+class ThumbPage(Element):
+    """Contains a single page thumbnail. Either as page object or a path to a thumbnail file."""
+    def __init__(self, spread, pageNumber, path=None, page=None, w=None, h=None, **kwargs):
+        Element.__init__(self, w=w, h=h, **kwargs)
+        context = self.context
+        self.spread = spread
+        self.pageNumber = pageNumber
+        bookFont = findFont('Upgrade-Book')
+
+        # Name of the page or spread
+        bs = self.context.newString(spread.name, style=dict(font=bookFont, fontSize=pt(7)))
+        tb1 = newTextBox(bs, w=w, y=h-self.gh/2, h=self.gh/2, parent=self, stroke=noColor)
+        # Thumbnail of the page
+        if page is not None:
+            # Show scaled page here.
+            pass
+        elif path is not None:
+            im = newImage(path=path, y=self.h-self.gh/2, w=w, parent=self, borders=pt(0.5))
+        else: # Page has no instance or path
+            im = newImage(w=w, y=self.gh/2, h=h-self.gh, parent=self, borders=pt(0.5))
+        # Pagenumber of the page, alignment left or right
+        if pageNumber % 2:
+            xAlign = RIGHT
+        else:
+            xAlign = LEFT
+        bs = context.newString(pageNumber, style=dict(font=bookFont, fontSize=pt(7), xTextAlign=xAlign))
+        tb2 = newTextBox(bs, w=w, h=self.gh/2, parent=self, stroke=noColor)
+                
 
 class PageSpread(Element):
 
-    def __init__(self, spread, **kwargs):
+    def __init__(self, spread, pageNumber=0, **kwargs):
         Element.__init__(self, **kwargs)
         self.spread = spread
-        if spread[0] is not None:
-            newRect(w=self.w/2, h=self.h, stroke=0, strokeWidth=0.5, padding=0, parent=self, 
-                conditions=(Left2Left(), Top2Top()))
-        if spread[1] is not None:
-            newRect(w=self.w/2, h=self.h, stroke=0, strokeWidth=0.5, padding=0, parent=self, 
-                conditions=(Right2Right(), Top2Top()))
+        if len(spread) and spread[0] is not None:
+            ThumbPage(spread[0], pageNumber, w=self.w/2, h=self.h, padding=0, parent=self)
+        if len(spread) > 1 and spread[1] is not None:
+            ThumbPage(spread[1], pageNumber+1, x=self.w/2, w=self.w/2, h=self.h, padding=0, parent=self)
 
 class Magazine(Publication):
     """Create a default magazine, with cover, front-of-book, articles on different template
@@ -47,7 +77,7 @@ class Magazine(Publication):
         Note that the e.parent of all elements is untouched. If parts run over several
         pages, then the same part instance is there.
         """
-        spreads = [[None]] # Keep left side of cover open.
+        spreads = [[None]] # Skip first to keep left side of cover open.
         for e in self.elements:
             e.getSpreads(spreads)
         return spreads
@@ -68,11 +98,13 @@ class Magazine(Publication):
         doc = Document(w=self.w, h=self.h, originTop=False,  
             gw=self.gw, gh=self.gh, gridX=self.gridX, gridY=self.gridY,
             baseline=self.baselineGrid, baselineStart=self.baselineGridStart)
+        
         view = doc.view
         view.showPadding = True
         view.showGrid = True
 
         page = None
+        pn = 0
 
         spreads = self.spreads
         for index in range(0, len(spreads), maxSpread):
@@ -84,8 +116,9 @@ class Magazine(Publication):
                     page = page.next
                 page.padding = self.padding
                 for pageSpread in pageSpreads:
-                    PageSpread(pageSpread, w=sw, h=self.h/self.w*sw/2, mr=self.gw, mb=self.gh, parent=page, 
+                    PageSpread(pageSpread, pn, w=sw, h=self.h/self.w*sw/2+self.gh, mr=self.gw, mb=self.gh, parent=page, 
                         conditions=(Right2RightSide(), Float2Top(), Float2Left()))
+                    pn += 2
 
         doc.solve()
         doc.export(path)
