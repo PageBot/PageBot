de77cf18473a34fa16497fe5cd953a81e5713d50
Michiel Kauw-A-Tjoe <michiel@concretejungle.nl>
2018-10-02 12:23:20 +0200

    Test read IDML package.
    Former-commit-id: 158894400f7879c9d9c0840933b6de45c09d798a [formerly e1946b6271f093ad2adb7e268c58cc13fa958290]
    Former-commit-id: f766adcbe11acaa1328fc6a4fc644e1e97f92767
    Former-commit-id: c44e647cf48e8a49a5f5ef6a076410580011807e
    Former-commit-id: 363bb625247fac9a81811be0854b280a2cf488b5

---
+++ Examples/18_Testing/testIDML.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -----------------------------------------------------------------------------
+#     Copyright (c) 2016+ Buro Petr van Blokland + Claudia Mens
+#     www.pagebot.io
+#
+#     P A G E B O T
+#
+#     Licensed under MIT conditions
+#
+#     Supporting DrawBot, www.drawbot.com
+# -----------------------------------------------------------------------------
+#
+#     testIDML.py
+#
+# Test InDesign Markup Language.
+#
+import traceback
+
+IDML = False
+
+try:
+    from simple_idml import idml
+    IDML = True
+except:
+    print(traceback.format_exc())
+
+def testIDML():
+    pkg = idml.IDMLPackage("test.idml")
+    print(pkg.font_families)
+    l = [e.get("Name") for e in pkg.font_families]
+    print(l)
+    print(pkg.spreads)
+    print(pkg.stories)
+    xml = pkg.xml_structure
+    from lxml import etree
+    s = etree.tostring(xml, pretty_print=True)
+    print(s)
+
+    #with pkg.prefix("main") as f:
+
+
+if IDML:
+    testIDML()
+
