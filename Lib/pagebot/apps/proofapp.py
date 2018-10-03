2949ed029e0fb28a341244e6cb63a3c28972a732
petrvanblokland <buro@petr.com>
2018-10-03 16:42:21 +0200

    Clean up copyright notice
    Former-commit-id: 4fdd7225d2d2f824fcf40ecd45406de009d310e1 [formerly b4a1eb261ec4b504e8e32ff752b5c17bbaf7d088]
    Former-commit-id: 913f72c4aae07508ccb3ad55775bdd692c1fae75
    Former-commit-id: b00aac6f1b8503413ce2f2b4af0b298698c87691
    Former-commit-id: 68d357f20806499125f62f5e4030e2b99accf2f4

--- Lib/pagebot/apps/proofapp.py
+++ Lib/pagebot/apps/proofapp.py
@@ -2,9 +2,13 @@
 # -----------------------------------------------------------------------------
 #
 #     P A G E B O T
-# #     Copyright (c) 2016+ Type Network, www.typenetwork.com, www.pagebot.io
+#
+#     Copyright (c) 2016+ Buro Petr van Blokland + Claudia Mens
+#     www.pagebot.io
 #     Licensed under MIT conditions
-#     Made for usage in DrawBot, www.drawbot.com
+#
+#     Supporting DrawBot, www.drawbot.com
+#     Supporting Flat, xxyxyz.org/flat
 # -----------------------------------------------------------------------------
 #
 #     scriptrunnerapp.py
