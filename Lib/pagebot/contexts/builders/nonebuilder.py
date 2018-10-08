e28a6e2d5a791b0f671fe56920a3b14819f4c417
Michiel Kauw-A-Tjoe <michiel@concretejungle.nl>
2018-10-08 15:34:11 +0200

    Adding required functions to BaseBuilder.
    Former-commit-id: faeda3ab49a1348b7d878b47560078707e5a7d41 [formerly 9731109c95762dddad6d48cc8abb15cb8fe7269f]
    Former-commit-id: 9dbd5c62ee2059e67593de3be0f0ff8d99a57106
    Former-commit-id: b8c772984ce14f5a35e6a926c7ecd94ada8b4f37
    Former-commit-id: be02da8ded0675348bf9ad5831c0d5fc60e07ba8

--- Lib/pagebot/contexts/builders/nonebuilder.py
+++ Lib/pagebot/contexts/builders/nonebuilder.py
@@ -16,6 +16,7 @@
 #
 import os
 from pagebot.toolbox.transformer import path2Name
+from pagebot.contexts.builders.basebuilder import BaseBuilder
 
 class NoneBezierPath:
     """Make NoneBezierPath with the same API for NoneDrawBotBuilder drawing
@@ -43,7 +44,7 @@ class NoneImageObject:
     def __init__(self, path):
         self.path = path
 
-class NoneBuilder:
+class NoneBuilder(BaseBuilder):
     """Make NoneBuilder with a general builder API for docTesting. Used for
     example when the platform does not support DrawBot. Methods need to be
     added here when DrawBotContext docTests fail with non-DrawBot platforms.
@@ -51,145 +52,7 @@ class NoneBuilder:
     NOTE: when completed this set of methods should match with DrawBot
     itself."""
 
-    def __init__(self):
-        self._installedFonts = []
-
-    def newDrawing(self, path=None):
-        pass
-
-    def frameDuration(self, v):
-        pass
-
-    restore = save = drawPath = newDrawing # Methods without attributes
-
-    BezierPath = NoneBezierPath
-
-    def newPath(self):
-        return NoneBezierPath()
-
-    def scale(self, sx, sy):
-        pass
-
-    def translate(self, x, y):
-        pass
-
-    def newPage(self, w, h):
-        pass
-
-    def moveTo(self, p):
-        pass
-
-    lineTo = moveTo
-
-    def curveTo(self, bcp1, bcp2, p):
-        pass
-
-    def openTypeFeatures(self, **openTypeFeatures):
-        pass
-
-    def closePath(self):
-        pass
-
-    def line(self, p1, p2):
-        pass
-
-    def oval(self, x, y, w, h):
-        pass
-
-    rect = oval
-
-    def line(self, p1, p2):
-        pass
-
-    def fill(self, r, g=None, b=None, a=None, alpha=None): # Covering API inconsistencies in DrawBot
-        pass
-
-    setFillColor = setStrokeColor = stroke = fill
-
-    def cmykFill(self, c, m=None, y=None, k=None, a=None, alpha=None): # Covering API inconsistencies in DrawBot
-        pass
-
-    cmykStroke = cmykFill
-
-    def strokeWidth(self, w):
-        pass
-
-    def sizes(self):
-        return dict(screen=(800, 600))
-
-    def installedFonts(self, pattern=None):
-        return []
-
-    def font(self, font):
-        pass
-
-    def fontSize(self, fontSize):
-        pass
-
-    def textSize(self, s):
-        return 10, 10
-
-    def hyphenation(self, language):
-        pass
-
-    def image(self, path, p, pageNumber=0, alpha=None):
-        pass
-
-    def imageSize(self, path):
-        """Answers the image size of our test image
-
-        path = getResourcesPath() + '/images/peppertom_lowres_398x530.png'
-        """
-        return 398, 530
-
-    def clipPath(self, clipPath):
-        pass
-
-    def numberOfImages(self, path):
-        return 1
-
-    def transform(self, t):
-        pass
-
-    def rotate(self, angle, center=None):
-        pass
-
-    def FormattedString(self, s):
-        class FS:
-            def __init__(self, s):
-                self.s = s
-        return FS(s)
-
-    def text(self, s, p):
-        pass
-
-    def textBox(self, s, r):
-        pass
-
-    def saveImage(self, path, multipage=True):
-        pass
-
-    def installFont(self, fontPath):
-        self._installedFonts.append(fontPath)
-        if os.path.exists(fontPath):
-            return path2Name(fontPath)
-        return None
-
-    def save(self):
-        pass
-
-    def restore(self):
-        pass
-
-    def fontName2FontPath(self, fontName):
-        """We cannot tell the relation of the font name and the font path for
-        DrawBot without OS X unless it is a path."""
-        if os.path.exists(fontName):
-            return fontName
-        return None
-
-    def ImageObject(self, path):
-        return NoneImageObject(path)
+    #BezierPath = NoneBezierPath
 
 class NoneDrawBotBuilder(NoneBuilder):
 
