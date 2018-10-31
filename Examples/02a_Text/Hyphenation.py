c35bd6a9918f694b14f7b231cd3712543938b178
Michiel Kauw-A-Tjoe <michiel@concretejungle.nl>
2018-10-31 14:00:08 +0100

    Testing hyphenation, always the same apparently.
    Former-commit-id: 72274e10ae78efdceadd072cbd11f25edb5ff2b5 [formerly 39085178f727a5c72d6933e1516d6276a90dd78a]
    Former-commit-id: 242ed0efc3a286f381547604b0d3d33b75bc8083
    Former-commit-id: 7af2618b95d14a66177c191edb504b8dd77e133a
    Former-commit-id: 47dc0af732eadfb604c1095b1f71aa9eb0a0ffbc

---
+++ Examples/02a_Text/Hyphenation.py
@@ -0,0 +1,45 @@
+# -----------------------------------------------------------------------------
+#
+#     P A G E B O T
+#
+#     Copyright (c) 2017 Thom Janssen <https://github.com/thomgb>
+#     www.pagebot.io
+#     Licensed under MIT conditions
+#
+#     Supporting DrawBot, www.drawbot.com
+#     Supporting Flat, xxyxyz.org/flat
+# -----------------------------------------------------------------------------
+#
+#     Hyphenation.
+
+from pagebot.document import Document
+from pagebot import getContext
+from pagebot.elements import *
+from pagebot.contributions.filibuster.blurb import Blurb
+from pagebot.toolbox.color import color
+
+context = getContext()
+
+txt = Blurb().getBlurb('article_ankeiler', noTags=True)
+
+w = 400 # change width to see other hyphenations
+h = 400
+
+W = 1200
+H = 1500
+doc = Document(w=W, h=H, autoPages=1)
+page = doc[1]
+
+style = dict(fontSize=24, hyphenationHead=4, hyphenationTail=3, hyphenation=True)
+t = context.newString(txt, style=style)
+newTextBox(t, x=100, y=H-100, w=w, h=h, parent=page, border=1, fill=color(0.3, 0.2, 0.1, 0.5))
+
+style['hyphenationTail'] = 400
+t = context.newString(txt, style=style)
+newTextBox(t, x=100, y=H-100-h, w=w, h=h, parent=page, border=1, fill=color(1, 1, 0))
+
+style['hyphenation'] = False
+t = context.newString(txt, style=style)
+newTextBox(t, x=100, y=H-100-2*h, w=w, h=h, parent=page, border=1, fill=color(1, 0, 1))
+doc.export('_export/Hyphenation.pdf')
+
