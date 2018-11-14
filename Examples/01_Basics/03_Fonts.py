2aa8bbb1d7d2cddcacccef6e285ab90803ff9e28
Michiel Kauw-A-Tjoe <michiel@concretejungle.nl>
2018-11-14 10:44:44 +0100

    Adding to groups.
    Former-commit-id: 9d16afb8cfddb6c88e45a01ee69d5289d8eae8f9 [formerly 9ac3c44842de2c7c7995d1676579866c4b2396f2]
    Former-commit-id: 0c3de7075d2afb27f57838bb06c003f106e42b28
    Former-commit-id: 4187c6258ddeaf9d0cd082f066aea9d27c7b48e7
    Former-commit-id: 495ada6ada5eedd556c752914f2cdf183216c22c

--- Examples/01_Basics/03_Fonts.py
+++ Examples/01_Basics/03_Fonts.py
@@ -16,45 +16,48 @@
 #     Shows how to get fonts.
 #
 from pagebot import *
-from pagebot.fonttoolbox.objects.font import findFont
-from pagebot.fonttoolbox.fontpaths import *
-from pagebot.fonttoolbox.objects.family import getFamilyPaths
 from pagebot.constants import A3
 from pagebot.document import Document
 from pagebot.elements import *
 from pagebot.toolbox.units import *
-
 from pagebot.conditions import *
+from pagebot.fonttoolbox.objects.font import findFont
+from pagebot.fonttoolbox.fontpaths import *
+from pagebot.fonttoolbox.objects.family import getFamilyPaths
 
-H, W = A3
-W = W * 1.5
+W, H = A3
+
 def showAll():
     context = getContext()
     doc = Document(w=W, h=H, originTop=False, autoPages=1, context=context)
     page = doc[1]
-    c = (Right2Right(), Top2Top(), Float2Left())
+    c1 = (Left2Left(), Float2Top())
+    c2 = (Left2Left(), Top2Top())
+    c3 = (Right2Right(), Top2Top())
 
     families = getFamilyPaths()
-    print(families['Roboto'])
+    #print(families['Roboto'])
     fontPaths = getFontPaths()
     print('Number of fonts found: %d' % len(fontPaths))
     import glob
     tfp = getTestFontsPath()
     pbFonts = getPageBotFontPaths()
     print('Number of fonts shipped with PageBot: %d' % len(pbFonts))
-    print(sorted(pbFonts.keys()))
+    #print(sorted(pbFonts.keys()))
     font = findFont('Roboto-Black')
     print('The Font object from the pagebot.fonttoolbox.objects module: %s' % font)
     print('Number of glyphs: %d' % len(font))
+    
     for pbFont in list(pbFonts.keys()):
-        if 'Bungee' in pbFont:
-            newText('%s\n' % pbFont, parent=page, conditions=c, fontSize=30)
+        if 'Bungee' in pbFont or 'Roboto' in pbFont:
+            g = newGroup(parent=page, conditions=c1)
+            newText('%s\n' % pbFont, parent=g, conditions=c2, fontSize=30)
             f = findFont(pbFont)
             path = PageBotPath(context=context)
             path.text('CDE', style=dict(font=f, fontSize=pt(30)))
             path = path.removeOverlap()
-            newPaths(path, parent=page, fill=0, conditions=c)
-    
+            newPaths(path, parent=g, fill=0, conditions=c3)
+
     page.solve()
     doc.build()
 
