53d3ba10f51ff230b8552b4f5064786f07ea7c5b
Michiel Kauw-A-Tjoe <michiel@kauwatjoe.com>
2019-03-25 10:36:47 +0100

    Tests PageBot app layer in DrawBot.
    Former-commit-id: 1b1d756cb7123d9a88db477ec4e965e35405c753 [formerly 940e4e61e56cdc825633868480c086615e3d5a34]
    Former-commit-id: 830be79f79cb58f657d85a59445b63bf6adedeef
    Former-commit-id: e4c4d1579f8f7d158db7fe88f66ea26762d431ce
    Former-commit-id: 023bc0d0e4469387430b5a1b90c43759d3ea6d6c

---
+++ Examples/23_PageBotApps/01_SpecimenApp.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -----------------------------------------------------------------------------
+#     Copyright (c) 2016+ Buro Petr van Blokland + Claudia Mens
+#     www.pagebot.io
+#
+#     P A G E B O T
+#
+#     Licensed under MIT conditions
+#
+#     Supporting DrawBot, www.drawbot.com
+#     Supporting Flat, xxyxyz.org/flat
+# -----------------------------------------------------------------------------
+#
+#     01_SpecimenApp.py
+#
+from pagebot.apps.specimenapp import SpecimenApp
+
+app = SpecimenApp('/Users/michiel/Code/PublicationsApp/specimenapp')
