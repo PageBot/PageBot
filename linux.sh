2749388ed009ef1c1d6abd750301fe957d471dc3
Michiel Kauw-A-Tjoe <michiel@kauwatjoe.com>
2019-10-16 16:23:20 +0200

    Separate install scripts.

---
+++ linux.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# https://docs.travis-ci.com/user/multi-os/
+
+#if [ $TRAVIS_OS_NAME = 'linux' ]; then
+    #sudo apt-get python3 python3-setuptools python3-pip
+#fi
+python --version
+pyenv global system 3.6
+pip install --upgrade pip
+pip install pylint
+pip install -r requirements.txt
+python setup.py install
+
