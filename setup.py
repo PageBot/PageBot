d8ebe0ffaaeb0bd3cdfb439798353e3f01f3f9e1
Michiel Kauw-A-Tjoe <michiel@kauwatjoe.com>
2019-10-16 10:51:01 +0200

    Shorter link names.

--- setup.py
+++ setup.py
@@ -17,7 +17,6 @@ from os import path
 
 this_directory = path.abspath(path.dirname(__file__))
 
-# read the contents of your README file
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
