f84aafeb7a18a3ae429327d6b557baa7a3bd9380
Michiel Kauw-A-Tjoe <michiel@kauwatjoe.com>
2019-10-16 11:56:33 +0200

    Trying matrix include.

--- .travis/install.sh
+++ .travis/install.sh
@@ -2,6 +2,6 @@
 # https://docs.travis-ci.com/user/multi-os/
 
 if [ $TRAVIS_OS_NAME = 'linux' ]; then
-    sudo apt-get python3 python3-setuptools python3-pip
+    #sudo apt-get python3 python3-setuptools python3-pip
 fi
 
