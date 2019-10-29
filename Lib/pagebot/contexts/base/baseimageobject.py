408781f9f09fdfcd034777c836a75539af2968a8
Michiel Kauw-A-Tjoe <michiel@kauwatjoe.com>
2019-10-29 09:49:18 +0100

    ImageObject template to inherit from.

---
+++ Lib/pagebot/contexts/base/baseimageobject.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+# -----------------------------------------------------------------------------
+#
+#     P A G E B O T
+#
+#     Copyright (c) 2016+ Buro Petr van Blokland + Claudia Mens
+#     www.pagebot.io
+#     Licensed under MIT conditions
+#
+#     Supporting DrawBot, www.drawbot.com
+#     Supporting Flat, xxyxyz.org/flat
+# -----------------------------------------------------------------------------
+#
+#     baseimageobject.py
+#
+
+class BaseImageObject:
+
+    def __init__(self, path=None):
+        pass
