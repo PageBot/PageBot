d3b99f104b100c013e23e2831ae6d977caac593b
Michiel Kauw-A-Tjoe <michiel@kauwatjoe.com>
2019-10-29 11:01:23 +0100

    Removing OSX-specific libraries.

--- Lib/pagebot/contexts/base/basebezierpath.py
+++ Lib/pagebot/contexts/base/basebezierpath.py
@@ -15,6 +15,8 @@
 #     basebezierpath.py
 #
 
+import math
+
 class BaseBezierPath:
     """A Bézier path object, if you want to draw the same over and over
     again."""
@@ -30,57 +32,44 @@ class BaseBezierPath:
     # pen support
 
     def moveTo(self, point):
-        """
-        Move to a point `x`, `y`.
-        """
+        """Move to a point `x`, `y`."""
 
     def lineTo(self, point):
-        """
-        Line to a point `x`, `y`.
-        """
+        """Line to a point `x`, `y`."""
 
     def curveTo(self, *points):
-        """
-        Draw a cubic bezier with an arbitrary number of control points.
-
-        The last point specified is on-curve, all others are off-curve
-        (control) points.
+        """Draws a cubic Bézier with an arbitrary number of control points. The
+        last point specified is on-curve, all others are off-curve (control)
+        points.
         """
 
     def qCurveTo(self, *points):
-        """
-        Draw a whole string of quadratic curve segments.
-
-        The last point specified is on-curve, all others are off-curve
-        (control) points.
+        """Draws an entire string of quadratic curve segments. The last point
+        specified is on-curve, all others are off-curve (control) points.
         """
 
     def closePath(self):
-        """
-        Close the path.
-        """
+        """Close the path."""
 
     def beginPath(self, identifier=None):
-        """
-        Begin using the path as a so called point pen and start a new subpath.
-        """
+        """Begin using the path as a so called point pen and start a new
+        subpath."""
 
     def addPoint(self, point, segmentType=None, smooth=False, name=None,
             identifier=None, **kwargs):
-        """
-        Use the path as a point pen and add a point to the current subpath. `beginPath` must
-        have been called prior to adding points with `addPoint` calls.
-        """
+        """Use the path as a point pen and add a point to the current subpath.
+        `beginPath` must have been called prior to adding points with
+        `addPoint` calls."""
 
     def endPath(self):
         """End the current subpath. Calling this method has two distinct
         meanings depending on the context:
 
-        When the bezier path is used as a segment pen (using `moveTo`,
+        When the Bézier path is used as a segment pen (using `moveTo`,
         `lineTo`, etc.), the current subpath will be finished as an open
         contour.
 
-        When the bezier path is used as a point pen (using `beginPath`,
+        When the Bézier path is used as a point pen (using `beginPath`,
         `addPoint` and `endPath`), the path will process all the points added
         with `addPoint`, finishing the current subpath."""
 
@@ -91,18 +80,15 @@ class BaseBezierPath:
         fontTools.misc.transform module. More precisely: it should be a
         sequence containing 6 numbers.
 
-        A `glyphSet` is required during initialization of the BezierPath object.
+        A `glyphSet` is required during initialization of the BezierPath
+        object.
         """
 
     def drawToPen(self, pen):
-        """
-        Draw the bezier path into a pen
-        """
+        """Draws the Bézier path into a pen"""
 
     def drawToPointPen(self, pointPen):
-        """
-        Draw the bezier path into a point pen.
-        """
+        """Draws the Bézier path into a point pen."""
 
     def arc(self, center, radius, startAngle, endAngle, clockwise):
         """Arc with `center` and a given `radius`, from `startAngle` to
@@ -116,23 +102,22 @@ class BaseBezierPath:
         legs of the angle."""
 
     def rect(self, x, y, w, h):
-        """Add a rectangle at possition `x`, `y` with a size of `w`, `h`."""
+        """Adds a rectangle at possition `x`, `y` with a size of `w`, `h`."""
 
     def oval(self, x, y, w, h):
-        """Add a oval at possition `x`, `y` with a size of `w`, `h`"""
+        """Adds a oval at possition `x`, `y` with a size of `w`, `h`"""
 
     def line(self, point1, point2):
-        """Add a line between two given points."""
+        """Adds a line between two given points."""
 
     def polygon(self, *points, **kwargs):
-        """Draws a polygon with n-amount of points.
-        Optionally a `close` argument can be provided to open or close the path.
-        As default a `polygon` is a closed path.
-        """
+        """Draws a polygon with n-amount of points. Optionally a `close`
+        argument can be provided to open or close the path.  As default a
+        `polygon` is a closed path."""
 
     def text(self, txt, offset=None, font=None, fontSize=10, align=None):
         """Draws a `txt` with a `font` and `fontSize` at an `offset` in the
-        bezier path.  If a font path is given the font will be installed and
+        Bézier path. If a font path is given the font will be installed and
         used directly.
 
         - Optionally an alignment can be set. Possible `align` values are:
@@ -144,25 +129,21 @@ class BaseBezierPath:
     def textBox(self, txt, box, font=None, fontSize=10, align=None,
             hyphenation=None):
         """
-        Draws a `txt` with a `font` and `fontSize` in a `box` in the bezier path.
+        Draws a `txt` with a `font` and `fontSize` in a `box` in the Bézier path.
         If a font path is given the font will be installed and used directly.
 
         Optionally an alignment can be set.
-        Possible `align` values are: `"left"`, `"center"` and `"right"`.
-
-        The default alignment is `left`.
-
-        Optionally `hyphenation` can be provided.
-
-        Optionally `txt` can be a `FormattedString`.
-        Optionally `box` can be a `BezierPath`.
+        * Possible `align` values are: `"left"`, `"center"` and `"right"`.
+        * The default alignment is `left`.
+        * Optionally `hyphenation` can be provided.
+        * Optionally `txt` can be a `FormattedString`.
+        * Optionally `box` can be a `BezierPath`.
         """
 
-    def traceImage(self, path, threshold=.2, blur=None, invert=False, turd=2, tolerance=0.2, offset=None):
-        """
-        Convert a given image to a vector outline.
-
-        Optionally some tracing options can be provide:
+    def traceImage(self, path, threshold=.2, blur=None, invert=False, turd=2,
+            tolerance=0.2, offset=None):
+        """Converts a given image to a vector outline. Optionally some tracing
+        options can be provided:
 
         * `threshold`: the threshold used to bitmap an image
         * `blur`: the image can be blurred
@@ -173,30 +154,26 @@ class BaseBezierPath:
         """
 
     def getBezierPath(self):
-        """
-        Return the BezierPath.
-        """
+        """Returns the BezierPath."""
 
     def setBezierPath(self, path):
-        """
-        Set a nsBezierPath.
-        """
+        """Sets a BezierPath."""
 
     def pointInside(self, xy):
-        """Check if a point `x`, `y` is inside a path."""
+        """Checks if a point `x`, `y` is inside a path."""
 
     def bounds(self):
-        """Return the bounding box of the path."""
+        """Returns the bounding box of the path."""
 
     def controlPointBounds(self):
-        """Return the bounding box of the path including the offcurve
+        """Returns the bounding box of the path including the offcurve
         points."""
 
     def optimizePath(self):
         pass
 
     def copy(self):
-        """Copy the bezier path."""
+        """Copy the Bézier path."""
 
     def reverse(self):
         """Reverse the path direction."""
@@ -217,13 +194,11 @@ class BaseBezierPath:
     # NOTE: currently handled within context.
 
     def translate(self, x=0, y=0):
-        """
-        Translate the path with a given offset.
-        """
+        """Translates the path with a given offset."""
         self.transform((1, 0, 0, 1, x, y))
 
     def rotate(self, angle, center=(0, 0)):
-        """Rotate the path around the `center` point (which is the origin by
+        """Rotates the path around the `center` point (which is the origin by
         default) with a given angle in degrees."""
         angle = math.radians(angle)
         c = math.cos(angle)
@@ -231,8 +206,8 @@ class BaseBezierPath:
         self.transform((c, s, -s, c, 0, 0), center)
 
     def scale(self, x=1, y=None, center=(0, 0)):
-        """Scale the path with a given `x` (horizontal scale) and `y` (vertical
-        scale).
+        """Scales the path with a given `x` (horizontal scale) and `y`
+        (vertical scale).
 
         If only 1 argument is provided a proportional scale is applied.
 
@@ -243,7 +218,7 @@ class BaseBezierPath:
         self.transform((x, 0, 0, y, 0, 0), center)
 
     def skew(self, angle1, angle2=0, center=(0, 0)):
-        """Skew the path with given `angle1` and `angle2`. If only one argument
+        """Skews the path with given `angle1` and `angle2`. If only one argument
         is provided a proportional skew is applied. The center of skewing can
         optionally be set via the `center` keyword argument. By default this is
         the origin."""
@@ -252,7 +227,7 @@ class BaseBezierPath:
         self.transform((1, math.tan(angle2), math.tan(angle1), 1, 0, 0), center)
 
     def transform(self, transformMatrix, center=(0, 0)):
-        """Transform a path with a transform matrix (xy, xx, yy, yx, x, y)."""
+        """Transforms a path with a transform matrix (xy, xx, yy, yx, x, y)."""
 
     # boolean operations
 
@@ -267,79 +242,30 @@ class BaseBezierPath:
         return contours
 
     def union(self, other):
-        """
-        Return the union between two bezier paths.
-        """
-        assert isinstance(other, self.__class__)
-        import booleanOperations
-        contours = self._contoursForBooleanOperations() + other._contoursForBooleanOperations()
-        result = self.__class__()
-        booleanOperations.union(contours, result)
-        return result
+        """Returns the union between two Bézier paths."""
 
     def removeOverlap(self):
-        """
-        Remove all overlaps in a bezier path.
-        """
-        import booleanOperations
-        contours = self._contoursForBooleanOperations()
-        result = self.__class__()
-        booleanOperations.union(contours, result)
-        self.setBezierPath(result.getBezierPath())
-        return self
+        """Remove all overlaps in a Bézier path."""
 
     def difference(self, other):
-        """
-        Return the difference between two bezier paths.
-        """
-        assert isinstance(other, self.__class__)
-        import booleanOperations
-        subjectContours = self._contoursForBooleanOperations()
-        clipContours = other._contoursForBooleanOperations()
-        result = self.__class__()
-        booleanOperations.difference(subjectContours, clipContours, result)
-        return result
+        """Returns the difference between two Bézier paths."""
 
     def intersection(self, other):
-        """
-        Return the intersection between two bezier paths.
-        """
-        assert isinstance(other, self.__class__)
-        import booleanOperations
-        subjectContours = self._contoursForBooleanOperations()
-        clipContours = other._contoursForBooleanOperations()
-        result = self.__class__()
-        booleanOperations.intersection(subjectContours, clipContours, result)
-        return result
+        """Returns the intersection between two Bézier paths."""
 
     def xor(self, other):
-        """
-        Return the xor between two bezier paths.
-        """
-        assert isinstance(other, self.__class__)
-        import booleanOperations
-        subjectContours = self._contoursForBooleanOperations()
-        clipContours = other._contoursForBooleanOperations()
-        result = self.__class__()
-        booleanOperations.xor(subjectContours, clipContours, result)
-        return result
+        """Returns the xor between two Bézier paths."""
 
     def intersectionPoints(self, other=None):
         """
-        Return a list of intersection points as `x`, `y` tuples.
+        Returns a list of intersection points as `x`, `y` tuples.
 
         Optionaly provide an other path object to find intersection points.
         """
-        import booleanOperations
-        contours = self._contoursForBooleanOperations()
-        if other is not None:
-            assert isinstance(other, self.__class__)
-            contours += other._contoursForBooleanOperations()
-        return booleanOperations.getIntersections(contours)
 
     def expandStroke(self, width, lineCap="round", lineJoin="round", miterLimit=10):
         """
-        Returns a new bezier path with an expanded stroke around the original path,
+        Returns a new Bézier path with an expanded stroke around the original path,
         with a given `width`. Note: the new path will not contain the original path.
 
         The following optional arguments are available with respect to line caps and joins:
@@ -347,15 +273,6 @@ class BaseBezierPath:
         * `lineJoin`: Possible values are `"bevel"`, `"miter"` or `"round"`
         * `miterLimit`: The miter limit to use for `"miter"` lineJoin option
         """
-        if lineJoin not in _LINEJOINSTYLESMAP:
-            raise DrawBotError("lineJoin must be 'bevel', 'miter' or 'round'")
-        if lineCap not in _LINECAPSTYLESMAP:
-            raise DrawBotError("lineCap must be 'butt', 'square' or 'round'")
-
-        strokedCGPath = Quartz.CGPathCreateCopyByStrokingPath(self._getCGPath(), None, width, _LINECAPSTYLESMAP[lineCap], _LINEJOINSTYLESMAP[lineJoin], miterLimit)
-        result = self.__class__()
-        result._setCGPath(strokedCGPath)
-        return result
 
     def __mod__(self, other):
         return self.difference(other)
@@ -394,7 +311,7 @@ class BaseBezierPath:
 
     def __ixor__(self, other):
         result = self.xor(other)
-        #self.setBezierPath(result.getBezierPath())
+        self.setBezierPath(result.getBezierPath())
         return self
 
     def _points(self, onCurve=True, offCurve=True):
