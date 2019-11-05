5946cc8878a17f40f28e4314442ce8145e823166
Michiel Kauw-A-Tjoe <michiel@kauwatjoe.com>
2019-11-05 12:51:43 +0100

    Installing fontconfig with homebrew.

--- scripts/osx-uninstall.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-pip uninstall vanilla
-pip uninstall defconAppKit
-pip uninstall compositor 
-pip uninstall pagebotcocoa
-pip uninstall drawbot
--- scripts/osx.sh
+++ scripts/osx.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -ev
 #pyenv global system 3.6
-pyenv versions
+#pyenv versions
+brew install fontconfig
 pip3 install --upgrade pip
 pip3 install pylint
 git clone https://github.com/PageBot/PageBotCocoa.git
