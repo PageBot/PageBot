23b5e91ec45ff51cc2220f3e676c21f9aecdc61c
petrvanblokland <buro@petr.com>
2020-04-16 22:26:29 +0200

    Change headers for Python

--- Lib/pagebot/contexts/basecontext/pagebotstring.py
+++ Lib/pagebot/contexts/basecontext/pagebotstring.py
@@ -1,4 +1,5 @@
-#!/usr/bin/env python3 # -*- coding: UTF-8 -*-
+#!/usr/bin/env python3 
+# -*- coding: UTF-8 -*-
 # -----------------------------------------------------------------------------
 #
 #     P A G E B O T
