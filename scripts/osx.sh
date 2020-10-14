37049312698b0a4ea88d9e7c041374ed9478eb1b
Michiel <michiel@kauwatjoe.com>
2020-10-14 10:32:13 +0200

    Comment to trigger new build.

--- scripts/osx.sh
+++ scripts/osx.sh
@@ -2,6 +2,7 @@
 set -ev
 pip3 install --upgrade pip
 pip3 install pylint
+# Installs PageBotOSX from git so we develop against bleeding edge commits.
 git clone https://github.com/PageBot/PageBotOSX.git
 cd PageBotOSX
 pip3 install -r requirements.txt
