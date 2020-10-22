e9608cc13ccedabed9d8da9a1e63bac6e0bbb9b3
Michiel <michiel@kauwatjoe.com>
2020-10-22 10:48:20 +0200

    Frame should be drawn before scaling etc.

--- Lib/pagebot/elements/glyphpath.py
+++ Lib/pagebot/elements/glyphpath.py
@@ -21,7 +21,7 @@ from pagebot.toolbox.color import noColor
 from pagebot.constants import DEFAULT_HEIGHT, DEFAULT_WIDTH, ORIGIN
 
 class GlyphPath(Paths):
-    """GlyphPath is an element to show show the path of a glyph with additional features.
+    """GlyphPath is an element to show the path of a glyph with additional features.
 
     >>> from pagebot.fonttoolbox.fontpaths import getTestFontsPath
     >>> from pagebot.fonttoolbox.objects.font import Font
@@ -42,49 +42,75 @@ class GlyphPath(Paths):
     """
     def __init__(self, glyph, w=None, h=None, pathFilter=None, **kwargs):
         Paths.__init__(self, **kwargs)
-        self.font = glyph.font # Store separately, to avoid disappearing weakref.
+        # Store separately, to avoid disappearing weakref.
+        self.font = glyph.font
         self.glyph = glyph
+
         # One of the two needs to be defined, the other should be None to scale
         # proportionally. If both are set, then the image scales
         # disproportionally.
         self.w = w
         self.h = h
-        self.iw = max(1, glyph.width) # Image width of the element equals the glyph width.
-        self.ih = self.font.info.unitsPerEm # Image height is the font em size.
-        self.pathFilter = pathFilter # Optional pathFilter method, called with self as param.
+        # Image width of the element equals the glyph width.
+        self.iw = max(1, glyph.width)
+        # Image height is the font em size.
+        self.ih = self.font.info.unitsPerEm
+        # Optional pathFilter method, called with self as param.
+        self.pathFilter = pathFilter
 
-    # Set the intended width and calculate the new proportional height."""
     def _get_w(self):
-        if not self._w: # Width is undefined
+        # Sets the intended width and calculates the new proportional height."""
+        # Width is undefined.
+        if not self._w:
             if self._h and self.ih:
-                return self.iw * self._h / self.ih  # Height is lead, calculate width.
-            return DEFAULT_WIDTH # Undefined and without parent, answer default width.
-        return self._w # Width is lead and defined as not 0 or None.
+                # Proportion is based on the height, calculate width.
+                return self.iw * self._h / self.ih
+            # Undefined and without parent, answer default width.
+            return DEFAULT_WIDTH
+
+        # Proportion is based on width, defined as not 0 or None.
+        return self._w
+
     def _set_w(self, w):
-        self._w = units(w or DEFAULT_WIDTH) # If self._h is set too, do disproportioan sizing. Otherwise set to 0 or None.
+        # If self._h is set too, do disproportioan sizing. Otherwise set to 0 or None.
+        self._w = units(w or DEFAULT_WIDTH)
     w = property(_get_w, _set_w)
 
     def _get_h(self):
-        if not self._h: # Width is undefined
+        # Width is undefined.
+        if not self._h:
             if self._w and self.iw:
-                return self.ih * self._w / self.iw  # Width is lead, calculate height.
-            return DEFAULT_HEIGHT # Undefined and without parent, answer default width.
-        return self._h # Height is lead and defined as not 0 or None.
+                # Width is lead, calculate height.
+                return self.ih * self._w / self.iw
+            # Undefined and without parent, answer default width.
+            return DEFAULT_HEIGHT
+        # Height is lead and defined as not 0 or None.
+        return self._h
+
     def _set_h(self, h):
-        self._h = units(h or DEFAULT_HEIGHT) # If self._w is set too, do disproportional sizing. Otherwise set to 0 or None.
+        # If self._w is set too, do disproportional sizing. Otherwise set to 0
+        # or None.
+        self._h = units(h or DEFAULT_HEIGHT)
     h = property(_get_h, _set_h)
 
     def build(self, view, origin=ORIGIN, **kwargs):
-        context = self.context # Get current context
+        # Get current context.
+        context = self.context
         p = pointOffset(self.origin, origin)
+        # Draws optional bounding box if view.showFrame = True.
+        self.buildFrame(view, p) # Draw optional frame, shadow or borders.
+
         p = self._applyScale(view, p)
-        px, py, _ = p = self._applyAlignment(p) # Ignore z-axis for now.
+        # Ignore z-axis for now.
+        px, py, _ = p = self._applyAlignment(p)
 
         context.save()
         context.translate(px, py)
-        sx, sy = (self.w/self.iw).rv, (self.h/self.ih).rv # We need the scale as ration value, not as unit.
+        # We need the scale as ration value, not as unit.
+        sx, sy = (self.w/self.iw).rv, (self.h/self.ih).rv
         context.scale(sx, sy)
-        # If there is a path filter defined, then call that the draw and ignore regular drawing.
+
+        # If a path filter is defined, call it and ignore regular drawing.
         if self.pathFilter is not None:
             self.pathFilter(self, self.glyph, view)
         else: # Not path filter defined, draw by regular stroke/fill.
@@ -96,8 +122,6 @@ class GlyphPath(Paths):
         for e in self.elements:
             e.build(view, p, **kwargs)
 
-        # Draw optional bounding box if view.showFrame = True
-        self.buildFrame(view, p) # Draw optional frame, shadow or borders.
 
         self._restoreScale(view)
         view.drawElementInfo(self, origin) # Depends on css flag 'showElementInfo'
