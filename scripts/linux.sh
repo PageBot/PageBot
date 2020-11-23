4a9e0496611ccf079f0a7b592519dae89ff11358
Michiel <michiel@kauwatjoe.com>
2020-11-23 14:24:20 +0100

    Upgrading to Ubuntu Bionic, also try Windows.

--- scripts/linux.sh
+++ scripts/linux.sh
@@ -2,12 +2,4 @@
 set -ev
 # https://docs.travis-ci.com/user/multi-os/
 
-#if [ $TRAVIS_OS_NAME = 'linux' ]; then
-    #sudo apt-get python3 python3-setuptools python3-pip
-#fi
-python --version
-#pyenv versions
-#pyenv global system 3.6
-pip3 install --upgrade pip
-pip3 install pylint
 pip3 install -r requirements.txt
--- scripts/osx.sh
+++ scripts/osx.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 set -ev
-pip3 install --upgrade pip
-pip3 install pylint
-# Installs PageBotOSX from git so we develop against bleeding edge commits.
+
+# Installs PageBotOSX from git first so we develop against bleeding edge commits.
 git clone https://github.com/PageBot/PageBotOSX.git
 cd PageBotOSX
 pip3 install -r requirements.txt
