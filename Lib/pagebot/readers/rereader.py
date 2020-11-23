e20658d12da27d68b12f6a342b4e51d47e578dfb
Michiel <michiel@kauwatjoe.com>
2020-11-23 15:21:16 +0100

    Didn't detect Windows, trying a different config.

--- Lib/pagebot/readers/rereader.py
+++ Lib/pagebot/readers/rereader.py
@@ -27,7 +27,8 @@ def readRE(path, pattern):
     >>> testHtml = resourcesPath + '/templates/test.html'
     >>> testString = '<html><body><p>test paragraph.</p></body></html>'
     >>> import re
-    >>> reNoTags = re.compile(r'\<[^\>]*|([^\<\>]*)')
+    >>> #reNoTags = re.compile(r'\<[^\>]*|([^\<\>]*)')
+    >>> reNoTags = re.compile(r'<[^>]*|([^<>]*)')
     >>> result = readRE(testHtml, reNoTags)
     """
     f = codecs.open(path, mode="r", encoding="utf-8")
