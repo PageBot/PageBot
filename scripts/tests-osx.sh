a5a67d2412f497633773e87f32b5ede51348d7bf
Michiel <michiel@kauwatjoe.com>
2020-11-24 19:11:52 +0100

    No dash.

--- scripts/tests-osx.sh
+++ scripts/tests-osx.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ev
 export PY="python3"
-$PY tests/baseline-osx.py
+$PY tests/baselineosx.py
 #$PY -m doctest tests/context-osx.txt
 #$PY -m doctest tests/composer-osx.txt
 #$PY -m doctest tests/babelstring-osx.txt
